b0d2153db77c833cef80d0a0c5444a1efc5bf7f9
Gauthier <bosson.gauthier27@gmail.com>
2019-05-07 10:51:49 +0200

    Added support support ssr for next with styled-components

---
+++ pages/_app.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+import Head from 'next/head';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import JssProvider from 'react-jss/lib/JssProvider';
+import getPageContext from '../lib/getPageContext';
+
+class MyApp extends App {
+  constructor() {
+    super();
+    this.pageContext = getPageContext();
+  }
+
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <Head>
+          <title>My page</title>
+        </Head>
+        {/* Wrap every page in Jss and Theme providers */}
+        <JssProvider
+          registry={this.pageContext.sheetsRegistry}
+          generateClassName={this.pageContext.generateClassName}
+        >
+          {/* MuiThemeProvider makes the theme available down the React
+              tree thanks to React context. */}
+          <MuiThemeProvider
+            theme={this.pageContext.theme}
+            sheetsManager={this.pageContext.sheetsManager}
+          >
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            {/* Pass pageContext to the _document though the renderPage enhancer
+                to render collected styles on server-side. */}
+            <Component pageContext={this.pageContext} {...pageProps} />
+          </MuiThemeProvider>
+        </JssProvider>
+      </Container>
+    );
+  }
+}
+
+export default MyApp;
---
+++ pages/_document.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Document, { Head, Main, NextScript } from 'next/document';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import flush from 'styled-jsx/server';
+import { ServerStyleSheet } from 'styled-components';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    let pageContext;
+    const page = ctx.renderPage((Component) => {
+      const WrappedComponent = (props) => {
+        // eslint-disable-next-line prefer-destructuring
+        pageContext = props.pageContext;
+        return <Component {...props} />;
+      };
+
+      WrappedComponent.propTypes = {
+        // eslint-disable-next-line react/forbid-prop-types
+        pageContext: PropTypes.object.isRequired,
+      };
+
+      return WrappedComponent;
+    });
+
+    let css;
+    if (pageContext) {
+      css = pageContext.sheetsRegistry.toString();
+    }
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () => originalRenderPage({
+        enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+      });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        ...page,
+        pageContext,
+        styles: (
+          <React.Fragment>
+            <style
+              id="jss-server-side"
+                // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{ __html: css }}
+            />
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+            {flush() || null}
+          </React.Fragment>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    const { pageContext } = this.props;
+
+    return (
+      <html lang="en" dir="ltr">
+        <Head>
+          <meta charSet="utf-8" />
+          {/* Use minimum-scale=1 to enable GPU rasterization */}
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+          />
+          {/* PWA primary color */}
+          <meta
+            name="theme-color"
+            content={pageContext ? pageContext.theme.palette.primary.main : null}
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
+
+export default MyDocument;
