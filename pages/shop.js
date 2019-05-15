47ca1e28478b6eb4b75a76c990a5c8dc7057b5d5
Gauthier <bosson.gauthier27@gmail.com>
2019-05-15 17:18:42 +0200

    added banner for Alfred shop (WIP)

---
+++ pages/shop.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import AlfredBanner from '../components/shop/AlfredBanner/AlfredBanner';
+
+const shop = () => {
+  return (
+    <Fragment>
+      <AlfredBanner />
+    </Fragment>
+  );
+};
+
+export default shop;
