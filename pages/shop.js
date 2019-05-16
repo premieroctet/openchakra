d3c32c665e874680217e9b0259314a9ed3b7566c
Gauthier <bosson.gauthier27@gmail.com>
2019-05-16 21:26:18 +0200

    added navbar

--- pages/shop.js
+++ pages/shop.js
@@ -4,10 +4,12 @@ import CanDo from '../components/shop/CanDo/CanDo';
 import MyBestSellers from '../components/shop/MyBestSellers/myBestSellers';
 import Bio from '../components/shop/Bio/Bio';
 import Review from '../components/shop/Review/Review';
+import NavBar from '../components/NavBar/NavBar';
 
 const shop = () => {
   return (
     <Fragment>
+      <NavBar />
       <AlfredBanner />
       <CanDo />
       <MyBestSellers />
