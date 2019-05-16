978fdbe3e38dd1a0fd57787a33fd2e0803a2723d
Gauthier <bosson.gauthier27@gmail.com>
2019-05-16 10:22:51 +0200

    added Can Do and My Best Sellers sections

--- pages/shop.js
+++ pages/shop.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react';
 import AlfredBanner from '../components/shop/AlfredBanner/AlfredBanner';
+import CanDo from '../components/shop/CanDo/CanDo';
+import MyBestSellers from '../components/shop/MyBestSellers/myBestSellers';
 
 const shop = () => {
   return (
     <Fragment>
       <AlfredBanner />
+      <CanDo />
+      <MyBestSellers />
     </Fragment>
   );
 };
