265364b04ab8a9d48007d8dfbaa2173e46764667
joffrey <joffrey-lhermitte@hotmail.fr>
2019-05-23 11:21:26 +0200

    backend for booking added

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const cors = require('cors');
 const users = require('./routes/api/users');
 const category = require('./routes/api/category');
 const billing = require('./routes/api/billing');
+const booking = require('./routes/api/booking');
 
 nextApp.prepare().then(() => {
     const app = express();
@@ -38,6 +39,7 @@ nextApp.prepare().then(() => {
     app.use('/myAlfred/api/users',users);
     app.use('/myAlfred/api/category',category);
     app.use('/myAlfred/api/billing',billing);
+    app.use('/myAlfred/api/booking',booking);
 
     const port = process.env.PORT || 5000;
 
