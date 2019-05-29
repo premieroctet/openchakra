7b24138d8b790552318ea7463acf098ac20e4300
joffrey <joffrey-lhermitte@hotmail.fr>
2019-05-29 14:38:08 +0200

    add logout navbar

--- components/home/Navbar/Navbar.js
+++ components/home/Navbar/Navbar.js
@@ -5,6 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
+import setAuthToken from "../../../utils/setAuthToken";
 
 const styles = {
   grow: {
@@ -15,21 +16,52 @@ const styles = {
   },
 };
 
-const navbar = (props) => {
-  const { classes } = props;
+class navbar extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      logged: false
+    }
+  }
 
-  return (
-    <AppBar color="default" position="static" className={classes.grow}>
-      <Toolbar>
-        <Typography variant="h6" className={classes.grow}>
-          My Alfred
-        </Typography>
-        <Button href={'/login'} variant="outlined" color="primary" className={classes.buttonSpace}>Connexion</Button>
-        <Button href={'/signup'} variant="contained" color="primary">Inscription</Button>
-      </Toolbar>
-    </AppBar>
-  );
-};
+  componentDidMount() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.setState({logged:true})
+    }
+  }
+
+  logout2() {
+    localStorage.removeItem('token');
+    // Remove auth header for future requests
+    setAuthToken(false);
+    window.location.reload();
+  };
+
+
+  render() {
+    const {classes} = this.props;
+    const test = this.state.logged;
+    const logout = <Button variant="outlined" color="primary" className={classes.buttonSpace}
+                           onClick={this.logout2}>Déconnexion</Button>;
+
+
+
+    return (
+        <AppBar color="default" position="static" className={classes.grow}>
+          <Toolbar>
+            <Typography variant="h6" className={classes.grow}>
+              My Alfred
+            </Typography>
+            {test ? logout :
+                <React.Fragment><Button href={'/login'} variant="outlined" color="primary"
+                                        className={classes.buttonSpace}>Connexion</Button>
+                  <Button href={'/signup'} variant="contained" color="primary">Inscription</Button></React.Fragment>}
+          </Toolbar>
+        </AppBar>
+    );
+  }
+}
 
 // eslint-disable-next-line react/no-typos
 navbar.propTypes = {
