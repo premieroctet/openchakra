e16bcb9e7bbeef2e9c473ff47e8297cf53fb1ed5
joffrey <joffrey-lhermitte@hotmail.fr>
2019-07-09 11:30:21 +0200

    update list category

--- pages/dashboard/category/all.js
+++ pages/dashboard/category/all.js
@@ -1,15 +1,26 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
 import { Typography } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Router from 'next/router';
 import Layout from '../../../hoc/Layout/Layout';
 import axios from "axios";
 import Link from "next/link";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import TablePagination from '@material-ui/core/TablePagination';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from "@material-ui/icons/FirstPage";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import LastPageIcon from "@material-ui/icons/LastPage";
 
 const {config} = require('../../../config/config');
 const url = config.apiUrl;
@@ -17,14 +28,12 @@ const url = config.apiUrl;
 const styles = theme => ({
     signupContainer: {
         alignItems: 'center',
-        height: '170vh',
         justifyContent: 'top',
         flexDirection: 'column',
 
     },
     card: {
         padding: '1.5rem 3rem',
-        width: 400,
         marginTop: '100px',
     },
     cardContant: {
@@ -38,12 +47,71 @@ const styles = theme => ({
     },
 });
 
+const actionsStyles = theme => ({
+    root: {
+        flexShrink: 0,
+        color: theme.palette.text.secondary,
+        marginLeft: theme.spacing.unit * 2.5
+    }
+});
+
+class TablePaginationActions extends React.Component {
+    handleFirstPageButtonClick = event => {
+        this.props.onChangePage(event, 0);
+    };
+
+    handleBackButtonClick = event => {
+        this.props.onChangePage(event, this.props.page - 1);
+    };
+
+    handleNextButtonClick = event => {
+        this.props.onChangePage(event, this.props.page + 1);
+    };
+
+    handleLastPageButtonClick = event => {
+        this.props.onChangePage(event, Math.max(0, Math.ceil(this.props.count / this.props.rowsPerPage) - 1));
+    };
+
+    render() {
+        const { classes, count, page, rowsPerPage, theme } = this.props;
+
+        return <div className={classes.root}>
+            <IconButton onClick={this.handleFirstPageButtonClick} disabled={page === 0} aria-label="First Page">
+                {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+            </IconButton>
+            <IconButton onClick={this.handleBackButtonClick} disabled={page === 0} aria-label="Previous Page">
+                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            </IconButton>
+            <IconButton onClick={this.handleNextButtonClick} disabled={page >= Math.ceil(count / rowsPerPage) - 1} aria-label="Next Page">
+                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+            </IconButton>
+            <IconButton onClick={this.handleLastPageButtonClick} disabled={page >= Math.ceil(count / rowsPerPage) - 1} aria-label="Last Page">
+                {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+            </IconButton>
+        </div>;
+    }
+}
+TablePaginationActions.propTypes = {
+    classes: PropTypes.object.isRequired,
+    count: PropTypes.number.isRequired,
+    onChangePage: PropTypes.func.isRequired,
+    page: PropTypes.number.isRequired,
+    rowsPerPage: PropTypes.number.isRequired,
+    theme: PropTypes.object.isRequired,
+
+};
+const TablePaginationActionsWrapped = withStyles(actionsStyles, { withTheme: true })(TablePaginationActions);
+
 class all extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            category: []
-        }
+            category: [],
+            page: 0,
+            rowsPerPage: 10,
+        };
+        this.handleChangePage = this.handleChangePage.bind(this);
+        this.handleChangeRowsPerPage = this.handleChangeRowsPerPage.bind(this);
 
     }
 
@@ -65,46 +133,82 @@ class all extends React.Component {
         });
     }
 
+    handleChangePage(event, page) {
+        this.setState({page});
+    }
+
+    handleChangeRowsPerPage(event) {
+        this.setState({ page: 0, rowsPerPage: event.target.value });
+    }
+
 
     render() {
         const { classes } = this.props;
         const {category} = this.state;
 
-        const row = category.map(e => (
-           <tr key={e._id}>
-               <td>{e.label}</td>
-               <td><img src={`../../../${e.picture}`} alt='image' width={80}/> </td>
-               <td><a href={`/dashboard/category/view?id=${e._id}`}>Modifier</a> </td>
-           </tr>
-        ));
-
-
         return (
             <Layout>
                 <Grid container className={classes.signupContainer}>
                     <Card className={classes.card}>
-                        <Grid>
-                            <Grid item style={{ display: 'flex', justifyContent: 'center' }}>
-                                <Typography style={{ fontSize: 30 }}>Liste catégorie</Typography>
-                            </Grid>
-                            <table>
-                                <thead>
-                                <tr>
-                                    <th>Label</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                {row}
-                                </tbody>
-                                <Grid item>
-                                    <Link href={"/dashboard/category/add"}>
-                                    <Button type="submit" variant="contained" color="primary" style={{ width: '100%' }}>
-                                        Ajouter
-                                    </Button>
-                                    </Link>
-                                </Grid>
-                            </table>
+
+                        <Grid item style={{ display: 'flex', justifyContent: 'center' }}>
+                            <Typography style={{ fontSize: 30 }}>Liste catégories</Typography>
                         </Grid>
+                        <Paper style={{width: '100%'}}>
+                            <div>
+                                <Table className={classes.table}>
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell>Label</TableCell>
+                                            <TableCell>Image</TableCell>
+                                            <TableCell>Action</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {category.slice(this.state.page * this.state.rowsPerPage, this.state.page * this.state.rowsPerPage + this.state.rowsPerPage)
+                                            .map((e,index) =>
+                                                <TableRow key={index}>
+                                                    <TableCell component="th" scope="row">
+                                                        {e.label}
+                                                    </TableCell>
+                                                    <TableCell component="th" scope="row">
+                                                        <img src={`../../../${e.picture}`} alt='image' width={80}/>
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        <Link href={`/dashboard/category/view?id=${e._id}`}><a>Modifier</a></Link>
+                                                    </TableCell>
+
+                                                </TableRow>
+                                            )}
+
+                                    </TableBody>
+                                </Table>
+                            </div>
+                            <TablePagination
+                                rowsPerPageOptions={[10, 25]}
+                                component="div"
+                                count={category.length}
+                                rowsPerPage={this.state.rowsPerPage}
+                                page={this.state.page}
+                                backIconButtonProps={{
+                                    'aria-label': 'Previous Page',
+                                }}
+                                nextIconButtonProps={{
+                                    'aria-label': 'Next Page',
+                                }}
+                                onChangePage={this.handleChangePage}
+                                onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                                ActionsComponent={TablePaginationActionsWrapped }
+                            />
+                        </Paper>
+
+                        <Link href={"/dashboard/category/add"}>
+                            <Button type="submit" variant="contained" color="primary" style={{ width: '100%' }}>
+                                Ajouter
+                            </Button>
+                        </Link>
+
+
                     </Card>
                 </Grid>
             </Layout>
