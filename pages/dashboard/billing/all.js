d7f3a5ab9b32b624189bd89b9e116f701274ff71
joffrey <joffrey-lhermitte@hotmail.fr>
2019-07-09 10:43:26 +0200

    new table with pagination billing

--- pages/dashboard/billing/all.js
+++ pages/dashboard/billing/all.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
@@ -15,7 +16,12 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-
+import TablePagination from '@material-ui/core/TablePagination';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from "@material-ui/icons/FirstPage";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import LastPageIcon from "@material-ui/icons/LastPage";
 
 const {config} = require('../../../config/config');
 const url = config.apiUrl;
@@ -46,12 +52,73 @@ const styles = theme => ({
     },
 });
 
+const actionsStyles = theme => ({
+    root: {
+        flexShrink: 0,
+        color: theme.palette.text.secondary,
+        marginLeft: theme.spacing.unit * 2.5
+    }
+});
+
+class TablePaginationActions extends React.Component {
+    handleFirstPageButtonClick = event => {
+        this.props.onChangePage(event, 0);
+    };
+
+    handleBackButtonClick = event => {
+        this.props.onChangePage(event, this.props.page - 1);
+    };
+
+    handleNextButtonClick = event => {
+        this.props.onChangePage(event, this.props.page + 1);
+    };
+
+    handleLastPageButtonClick = event => {
+        this.props.onChangePage(event, Math.max(0, Math.ceil(this.props.count / this.props.rowsPerPage) - 1));
+    };
+
+    render() {
+        const { classes, count, page, rowsPerPage, theme } = this.props;
+
+        return <div className={classes.root}>
+            <IconButton onClick={this.handleFirstPageButtonClick} disabled={page === 0} aria-label="First Page">
+                {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+            </IconButton>
+            <IconButton onClick={this.handleBackButtonClick} disabled={page === 0} aria-label="Previous Page">
+                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            </IconButton>
+            <IconButton onClick={this.handleNextButtonClick} disabled={page >= Math.ceil(count / rowsPerPage) - 1} aria-label="Next Page">
+                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+            </IconButton>
+            <IconButton onClick={this.handleLastPageButtonClick} disabled={page >= Math.ceil(count / rowsPerPage) - 1} aria-label="Last Page">
+                {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+            </IconButton>
+        </div>;
+    }
+}
+TablePaginationActions.propTypes = {
+    classes: PropTypes.object.isRequired,
+    count: PropTypes.number.isRequired,
+    onChangePage: PropTypes.func.isRequired,
+    page: PropTypes.number.isRequired,
+    rowsPerPage: PropTypes.number.isRequired,
+    theme: PropTypes.object.isRequired,
+
+};
+const TablePaginationActionsWrapped = withStyles(actionsStyles, { withTheme: true })(TablePaginationActions);
+
 class all extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            billing: []
-        }
+            billing: [],
+            page: 0,
+            rowsPerPage: 10,
+
+        };
+
+        this.handleChangePage = this.handleChangePage.bind(this);
+        this.handleChangeRowsPerPage = this.handleChangeRowsPerPage.bind(this);
 
     }
 
@@ -72,24 +139,25 @@ class all extends React.Component {
         });
     }
 
+    handleChangePage(event, page) {
+        this.setState({page});
+    }
+
+    handleChangeRowsPerPage(event) {
+        this.setState({ page: 0, rowsPerPage: event.target.value });
+    }
+
 
     render() {
         const { classes } = this.props;
         const {billing} = this.state;
-
-        const row = billing.map(e => (
-            <tr key={e._id}>
-                <td>{e.label}</td>
-                <td><a href={`/dashboard/billing/view?id=${e._id}`}>Modifier</a> </td>
-            </tr>
-        ));
-
 
         return (
             <Layout>
                 <Grid container className={classes.signupContainer}>
                     <Card className={classes.card}>
                         <Paper style={{width: '100%'}}>
+                            <div>
                             <Table className={classes.table}>
                                 <TableHead>
                                     <TableRow>
@@ -98,7 +166,8 @@ class all extends React.Component {
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
-                                    {billing.map((e,index) => (
+                                    {billing.slice(this.state.page * this.state.rowsPerPage, this.state.page * this.state.rowsPerPage + this.state.rowsPerPage)
+                                        .map((e,index) =>
                                         <TableRow key={index}>
                                             <TableCell component="th" scope="row">
                                                 {e.label}
@@ -108,9 +177,27 @@ class all extends React.Component {
                                             </TableCell>
 
                                         </TableRow>
-                                    ))}
+                                    )}
+
                                 </TableBody>
                             </Table>
+                            </div>
+                            <TablePagination
+                                rowsPerPageOptions={[10, 25]}
+                                component="div"
+                                count={billing.length}
+                                rowsPerPage={this.state.rowsPerPage}
+                                page={this.state.page}
+                                backIconButtonProps={{
+                                    'aria-label': 'Previous Page',
+                                }}
+                                nextIconButtonProps={{
+                                    'aria-label': 'Next Page',
+                                }}
+                                onChangePage={this.handleChangePage}
+                                onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                                ActionsComponent={TablePaginationActionsWrapped }
+                            />
                         </Paper>
                         <Link href={"/dashboard/billing/add"}>
                             <Button type="submit" variant="contained" color="primary" style={{ width: '100%' }}>
@@ -123,5 +210,7 @@ class all extends React.Component {
         );
     };
 }
+
+
 
 export default withStyles(styles)(all);
