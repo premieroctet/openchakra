9f8b472a89da3b93cedf345c6fff2b089f99b186
joffrey <joffrey-lhermitte@hotmail.fr>
2019-07-15 08:40:55 +0200

    old dashboard deleted

--- admin_dashboard/src/billing/index.js
+++
@@ -1,9 +0,0 @@
-import BillingList from './BillingList';
-import BillingEdit from './BillingEdit';
-import BillingCreate from './BillingCreate'
-
-export default {
-    list: BillingList,
-    edit: BillingEdit,
-    create: BillingCreate
-};
