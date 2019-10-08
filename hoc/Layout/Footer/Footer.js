41084a5d1a7a0c1a531025b46cd34077b3b6452a
ratelhugo <ratelhugo@gmail.com>
2019-10-08 13:26:41 +0200

    ajout de la page cgu et donc modification du lien CGU dans le footer

--- hoc/Layout/Footer/Footer.js
+++ hoc/Layout/Footer/Footer.js
@@ -192,7 +192,7 @@ class Footer extends Component {
                                         <Link href="#"><a style={{textDecoration: 'none'}}><Typography style={{color: 'white'}}>Mentions Légales</Typography></a></Link>
                                     </Grid>    
                                     <Grid item xs={12} style={{textAlign: 'left'}}>
-                                        <Link href="#"><a style={{textDecoration: 'none'}}><Typography style={{color: 'white'}}>Conditions générales d'utilisation</Typography></a></Link>
+                                        <Link href="/footer/cgu"><a style={{textDecoration: 'none'}}><Typography style={{color: 'white'}}>Conditions générales d'utilisation</Typography></a></Link>
                                     </Grid>    
                                     <Grid item xs={12} style={{textAlign: 'left'}}>
                                         <Link href="/footer/privacypolicy"><a style={{textDecoration: 'none'}}><Typography style={{color: 'white'}}>Politique de confidentialité</Typography></a></Link>
@@ -208,7 +208,7 @@ class Footer extends Component {
                                         <Link href="#"><a style={{textDecoration: 'none'}}><Typography style={{color: 'white'}}>Mentions Légales</Typography></a></Link>
                                     </Grid>    
                                     <Grid item xs={12}>
-                                        <Link href="#"><a style={{textDecoration: 'none'}}><Typography style={{color: 'white'}}>Conditions générales d'utilisation</Typography></a></Link>
+                                        <Link href="/footer/cgu"><a style={{textDecoration: 'none'}}><Typography style={{color: 'white'}}>Conditions générales d'utilisation</Typography></a></Link>
                                     </Grid>    
                                     <Grid item xs={12}>
                                         <Link href="/footer/privacypolicy"><a style={{textDecoration: 'none'}}><Typography style={{color: 'white'}}>Politique de confidentialité</Typography></a></Link>
