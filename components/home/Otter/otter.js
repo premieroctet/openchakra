72886e32e4612d041499e2dc14ac793020e0d3b5
Lienard <edwin.lienard@outlook.fr>
2019-10-31 14:02:52 +0100

    REFACTO otter

--- components/home/Otter/otter.js
+++ components/home/Otter/otter.js
@@ -7,67 +7,13 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import axios from 'axios';
-import Link from 'next/link';
 const { config } = require('../../../config/config');
 const url = config.apiUrl;
 
 
 const styles = theme => ({
     container: {
-        paddingRight: 15,
-        paddingLeft: 15,
-        marginRight: 'auto',
-        marginLeft: 'auto',
-
-        // Full width for (xs, extra-small: 0px or larger) and (sm, small: 600px or larger)
-        [theme.breakpoints.up('md')]: { // medium: 960px or larger
-            width: 920,
-        },
-        [theme.breakpoints.up('lg')]: { // large: 1280px or larger
-            width: 1170,
-        },
-        [theme.breakpoints.up('xl')]: { // extra-large: 1920px or larger
-            width: 1366,
-        },
-    },
-    container1: {
-        paddingRight: 15,
-        paddingLeft: 15,
-        marginRight: 'auto',
-        marginLeft: 'auto',
-
-        // Full width for (xs, extra-small: 0px or larger) and (sm, small: 600px or larger)
-        [theme.breakpoints.up('xs')]: {
-            width: 350,
-        },
-        [theme.breakpoints.up('sm')]: {
-            width: 500,
-        },
-        [theme.breakpoints.up('md')]: { // medium: 960px or larger
-            width: 920,
-        },
-        [theme.breakpoints.up('lg')]: { // large: 1280px or larger
-            width: 1170,
-        },
-        [theme.breakpoints.up('xl')]: { // extra-large: 1920px or larger
-            width: 1366,
-        },
-    },
-    media: {
-        height: 400,
-        borderRadius: '20px',
-        paddingTop: '30.25%', // 16:9
-    },
-    mediaLittleCard: {
-        height: 0,
-        borderRadius: '20px',
-        paddingTop: '32.25%', // 16:9
-    },
-    textBox: {
-        paddingRight: 15,
-        paddingLeft: 15,
-        marginBottom: 30,
-        marginTop: 35,
+        margin: 'auto',
 
         // Full width for (xs, extra-small: 0px or larger) and (sm, small: 600px or larger)
         [theme.breakpoints.up('md')]: { // medium: 960px or larger
@@ -102,30 +48,6 @@ const styles = theme => ({
             display: 'flex',
         },
     },
-    card22: {
-        marginTop: '10%',
-        display: 'flex',
-        height: 'auto',
-        boxShadow: '1px 3px 1px transparent',
-
-        [theme.breakpoints.up('xs')]: { // medium: 960px or larger
-            display: 'flex',
-            width: '100%',
-        },
-        [theme.breakpoints.up('sm')]: { // medium: 960px or larger
-            display: 'flex',
-            width: '100%'
-        },
-        [theme.breakpoints.up('md')]: { // medium: 960px or larger
-            display: 'none',
-        },
-        [theme.breakpoints.up('lg')]: { // medium: 960px or larger
-            display: 'none',
-        },
-        [theme.breakpoints.up('xl')]: { // medium: 960px or larger
-            display: 'none',
-        },
-    },
     details: {
         display: 'flex',
         flex: '1',
@@ -157,10 +79,6 @@ const styles = theme => ({
     margin: {
         margin: '0.7rem',
     },
-    margin2: {
-        margin: '0.7rem',
-    },
-
     card: {
         display: 'flex',
         margin: '5px!important',
@@ -168,62 +86,6 @@ const styles = theme => ({
         marginRight: '10px!important',
         marginLeft: '10px!important',
         boxShadow: '1px 3px 1px transparent'
-
-    },
-    personName: {
-        alignSelf: 'center',
-        fontWeight: 'bold',
-        padding: '.5rem',
-        textAlign: 'right!important',
-        color: '#33558B',
-    },
-    personName2: {
-        alignSelf: 'center',
-        padding: '.5rem',
-        textAlign: 'right!important',
-        fontSize: '0.8rem',
-        color: '#33558B',
-    },
-    card11: {
-        display: 'flex',
-        margin: '5px!important',
-        minWidth: '300px!important',
-        marginRight: '10px!important',
-        marginLeft: '10px!important',
-        marginTop: '30px!important',
-        marginBottom: '40px!important',
-        boxShadow: '1px 3px 1px transparent',
-    },
-    imgavat: {
-        marginTop: '60%',
-    },
-    petitpaddingpers: {
-        marginLeft: '-20%',
-    },
-    textBox1: {
-        color: 'rgba(84,89,95,0.95)',
-        letterSpacing: -2,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        paddingRight: 15,
-        paddingLeft: 15,
-        marginBottom: 15,
-        marginTop: 80,
-    },
-    textBox2: {
-        color: 'rgba(84,89,95,0.95)',
-        paddingRight: 15,
-        paddingLeft: 15,
-        marginBottom: 15,
-        fontSize: 28,
-        fontWeight: 570,
-        marginTop: 10,
-    },
-    textdesc: {
-        [theme.breakpoints.down('sm')]: {
-            marginTop: '10%!important',
-        },
-        marginTop: '2%',
     },
 });
 
@@ -247,13 +109,8 @@ class Otter extends React.Component{
 
     render() {
         const {classes} = this.props;
-        const {alfred} = this.state;
-
-
-
         return (
             <Fragment>
-
                 {/*Partager vos passions et vos compétences*/}
                 <Grid container className={classes.container}>
                 </Grid>
@@ -286,16 +143,6 @@ class Otter extends React.Component{
                         </Card>
                     </Grid>
                 </Grid>
-                {/*
-          <div className="thewrap">
-            <section className="card2">
-
-              {cards}
-
-            </section>
-          </div>
-          */}
-
             </Fragment>
         );
     }
