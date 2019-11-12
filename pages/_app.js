84378f3a0531de8a4253412d725081262312a28f
Lienard <edwin.lienard@outlook.fr>
2019-11-12 17:05:58 +0100

    Add talk.to

--- pages/_app.js
+++ pages/_app.js
@@ -31,6 +31,15 @@ class MyApp extends App {
   }
 
   componentDidMount() {
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+      s1.async=true;
+      s1.src='https://embed.tawk.to/5d7a092c9f6b7a4457e14b95/default';
+      s1.charset='UTF-8';
+      s1.setAttribute('crossorigin','*');
+      s0.parentNode.insertBefore(s1,s0);
+    })();
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles && jssStyles.parentNode) {
