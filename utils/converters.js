d93ee91abcf39c884176991e0e6dd8e70e64d40a
Sébastien Auvray <sebastien.auvray@wisper.io>
2019-12-23 16:30:37 +0100

    Modif id event

--- utils/converters.js
+++ utils/converters.js
@@ -44,7 +44,7 @@ const avail2event = availab => {
     evts.forEach(e => {
       let title = e.all_services ? "Tous services" : e.services.map( s => s.label).join('\n');
       let res= {
-        id: e._id+new Date().getTime(),
+        id: e._id+"-"+new Date().getTime(),
         title: title,
         start: new Date(e.begin),
         end: new Date(e.end),
