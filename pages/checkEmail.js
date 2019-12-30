d3cee33da792e0057d7ea5a852fe382fdeefb8b8
Lienard <edwin.lienard@outlook.fr>
2019-12-30 16:56:01 +0100

    UPDATE ui + responsive

--- pages/checkEmail.js
+++ pages/checkEmail.js
@@ -1,18 +1,11 @@
 import React from 'react';
-
 import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
-import { Typography } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import Router from 'next/router';
 import Layout from '../hoc/Layout/Layout';
-import axios from "axios";
 import Link from "next/link";
 
-const { config } = require('../config/config');
-const url = config.apiUrl;
 const styles = theme => ({
     signupContainer: {
         alignItems: 'center',
@@ -23,23 +16,16 @@ const styles = theme => ({
     },
     card: {
         fontFamily: 'Helvetica',
-        width: 800,
+        width: 600,
         marginTop: '100px',
-    },
-    cardContant: {
-        flexDirection: 'column',
-    },
-    linkText: {
-        textDecoration: 'none',
-        color: 'black',
-        fontSize: 12,
-        lineHeight: 4.15,
+        [theme.breakpoints.down('xs')]:{
+            width:'90%'
+        }
     },
     banner: {
         marginBottom: 25,
         backgroundColor: '#2FBCD3',
         height: 80,
-
     },
     newContainer: {
         padding: 20,
@@ -53,18 +39,30 @@ const styles = theme => ({
         paddingTop: 22,
         letterSpacing: 1,
     },
+    responsiveButton:{
+        display:'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10,
+        [theme.breakpoints.down('xs')]:{
+            flexDirection: 'column',
+            justifyContent: 'center',
+        }
+    },
+    responsiveSecondaryButton:{
+        [theme.breakpoints.down('xs')]:{
+           marginTop: '2%'
+        }
+    }
 });
 
 class checkEmail extends React.Component {
     constructor(props) {
         super(props);
-
     }
-
 
     render() {
         const { classes } = this.props;
-
 
         return (
             <Layout>
@@ -72,27 +70,31 @@ class checkEmail extends React.Component {
                     <Card className={classes.card}>
                         <div className={classes.banner}>
                             <h2 className={classes.title}>Inscription terminée</h2>
-
                         </div>
                         <div className={classes.newContainer}>
                             <Grid container style={{display: 'flex', justifyContent: 'center', marginTop: 20}}>
-                                <img src='../static/success-signup.svg' style={{width: 100}} alt={'success'}/>
+                                <img src='../static/happy_castor.svg' style={{width: 100}} alt={'success'}/>
                             </Grid>
-                            <Grid container style={{display: 'flex', justifyContent: 'center', marginTop: 20}}>
-
+                            <Grid item style={{display: 'flex', justifyContent: 'center', marginTop: 10, textAlign: 'justify'}}>
+                                <p>Inscription réussie ! Vous pouvez maintenant proposer ou rechercher vos services sur My Alfred</p>
                             </Grid>
-                            <Grid item style={{display: 'flex', justifyContent: 'center', marginTop: 10}}>
-                                <p>Vous pouvez maintenant proposer ou rechercher vos services sur My Alfred</p>
-                            </Grid>
-                            <Grid item style={{display: 'flex', justifyContent: 'center', marginTop: 10}}>
-                                <Link href={'/'}>
-                                    <a style={{textDecoration:'none'}}>
-                                        <Button variant={"contained"} color={"primary"} style={{color:"white"}}>Commencez à explorer</Button>
-                                    </a>
-                                </Link>
+                            <Grid item className={classes.responsiveButton}>
+                                <Grid item style={{marginRight:'1%' }}>
+                                    <Link href={'/'}>
+                                        <a style={{textDecoration:'none'}}>
+                                            <Button variant={"contained"} color={"primary"} style={{color:"white"}}>Commencez à explorer</Button>
+                                        </a>
+                                    </Link>
+                                </Grid>
+                                <Grid item className={classes.responsiveSecondaryButton}>
+                                    <Link href={'/becomeAlfredForm'}>
+                                        <a style={{textDecoration:'none'}}>
+                                            <Button variant={"contained"} color={"secondary"} style={{color:"white"}}>Créer ma boutique</Button>
+                                        </a>
+                                    </Link>
+                                </Grid>
                             </Grid>
                         </div>
-
                     </Card>
                 </Grid>
             </Layout>
