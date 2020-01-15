c758296752377258892d3b05a8f47b7d608aea76
Sébastien Auvray <sebastien.auvray@free.fr>
2020-01-15 14:36:04 +0000

    Renommage titre de pages My-Alfred=> My Alfred

--- pages/_app.js
+++ pages/_app.js
@@ -51,7 +51,7 @@ class MyApp extends App {
     return (
       <Container>
         <Head>
-          <title>MyAlfred</title>
+          <title>My Alfred</title>
           <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
                 integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
                 crossOrigin=""/>
