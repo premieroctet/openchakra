8b20e4f0e3d2593ac12efb1226de01ccf9afc363
Thibault <thibault@premieroctet.com>
2020-02-03 14:15:21 +0100

    Fix redux persist with undo behavior

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,11 @@ import App from "./App";
 import { AppStateWithUndo } from "./core/models/app";
 import { ThemeProvider, CSSReset, theme } from "@chakra-ui/core";
 import { init } from "@rematch/core";
-import createRematchPersist from "@rematch/persist";
 import { Provider } from "react-redux";
 import { combineReducers } from "redux";
 import undoable from "redux-undo";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import models from "./core/models";
 import filterUndoableActions from "./utils/undo";
@@ -16,22 +17,35 @@ export type RootState = {
   app: AppStateWithUndo;
 };
 
-const persistPlugin = createRematchPersist({
+const persistConfig = {
+  key: "root",
+  storage,
   whitelist: ["app"],
-  version: parseInt(process.env.REACT_APP_VERSION || "1", 10)
-});
+  version: parseInt(process.env.REACT_APP_VERSION || "1", 10),
+  stateReconciler: (inboundState: RootState) => {
+    return inboundState;
+  }
+};
+
+const persistPlugin = {
+  onStoreCreated(store: any) {
+    persistStore(store);
+  }
+};
 
 export const store = init({
   models,
   redux: {
     combineReducers: reducers => {
-      console.log(reducers);
-      return combineReducers({
-        ...reducers,
-        app: undoable(reducers.app, {
-          filter: filterUndoableActions,
+      return persistReducer(
+        persistConfig,
+        combineReducers({
+          ...reducers,
+          app: undoable(reducers.app, {
+            filter: filterUndoableActions
+          })
         })
-      });
+      );
     }
   },
   plugins: [persistPlugin]
