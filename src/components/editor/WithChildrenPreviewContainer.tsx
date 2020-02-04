3131dcd2e900e98d9a088b3f05db10dae246564b
Thibault <thibault@premieroctet.com>
2020-02-04 18:59:56 +0100

    Generic components

---
+++ src/components/editor/WithChildrenPreviewContainer.tsx
@@ -0,0 +1,26 @@
+import React, { FunctionComponent, ComponentClass } from 'react'
+import { useInteractive } from '../../hooks/useInteractive'
+import { useDropComponent } from '../../hooks/useDropComponent'
+import ComponentPreview from './ComponentPreview'
+
+const WithChildrenPreviewContainer: React.FC<{
+  component: IComponent
+  type: string | FunctionComponent<any> | ComponentClass<any, any>
+}> = ({ component, type }) => {
+  const { drop, isOver } = useDropComponent(component.id)
+  const { props, ref } = useInteractive(component, true)
+
+  if (isOver) {
+    props.bg = 'teal.50'
+  }
+
+  return React.createElement(
+    type,
+    { ...props, pos: 'relative', ref: drop(ref) },
+    component.children.map((key: string) => (
+      <ComponentPreview componentName={key} />
+    )),
+  )
+}
+
+export default WithChildrenPreviewContainer
