8b78ddd0b451aeda101445e233193d8fe033bb06
Hugo FOYART <hfoyart@premieroctet.com>
2020-02-04 17:56:50 +0100

    Fix console warnings

--- src/components/inspector/controls/SizeControl.tsx
+++ src/components/inspector/controls/SizeControl.tsx
@@ -1,37 +1,37 @@
-import React, { ReactNode } from "react";
-import FormControl from "./FormControl";
-import { Select } from "@chakra-ui/core";
-import { useForm } from "../../../hooks/useForm";
+import React, { ReactNode } from 'react'
+import FormControl from './FormControl'
+import { Select } from '@chakra-ui/core'
+import { useForm } from '../../../hooks/useForm'
 
-export type Size = "2xs" | "xs" | "sm" | "md" | "lg" | "xl" | "2xl";
+export type Size = '2xs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl'
 type SizeControlPropsType = {
-  name?: string;
-  label?: string | ReactNode;
-  value: string;
-  options?: Size[];
-};
+  name?: string
+  label?: string | ReactNode
+  value: string
+  options?: Size[]
+}
 
-const options = ["xs", "sm", "md", "lg"];
+const options = ['xs', 'sm', 'md', 'lg']
 
 const SizeControl = (props: SizeControlPropsType) => {
-  const { setValueFromEvent } = useForm();
-  const choices = props.options || options;
+  const { setValueFromEvent } = useForm()
+  const choices = props.options || options
 
   return (
-    <FormControl label={props.label} htmlFor={props.name || "size"}>
+    <FormControl label={props.label} htmlFor={props.name || 'size'}>
       <Select
         size="sm"
-        id={props.name || "size"}
-        name={props.name || "size"}
-        value={props.value || ""}
+        id={props.name || 'size'}
+        name={props.name || 'size'}
+        value={props.value || ''}
         onChange={setValueFromEvent}
       >
         {choices.map(choice => (
-          <option>{choice}</option>
+          <option key={choice}>{choice}</option>
         ))}
       </Select>
     </FormControl>
-  );
-};
+  )
+}
 
-export default SizeControl;
+export default SizeControl
