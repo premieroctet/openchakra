0faa7fd1d405b1ae165d2269075c200fd94a3c07
shinework <adrien.baptiste@gmail.com>
2020-02-11 21:46:32 +0100

    Fix issue with panel switching

--- src/hooks/usePropsSelector.ts
+++ src/hooks/usePropsSelector.ts
@@ -2,11 +2,13 @@ import { useSelector } from 'react-redux'
 import { RootState } from '../core/store'
 
 const usePropsSelector = (propsName: string) => {
-  return useSelector(
+  const value = useSelector(
     (state: RootState) =>
       state.components.present.components[state.components.present.selectedId]
         .props[propsName],
   )
+
+  return value || ''
 }
 
 export default usePropsSelector
