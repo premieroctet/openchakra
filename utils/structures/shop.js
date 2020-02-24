15708fb2c8ae7dc85ac77849c99c70788d72be20
Edwin <lienard.edwin@gmail.com>
2020-02-24 16:04:17 +0100

    crea utils for validation steppers

--- utils/structures/shop.js
+++ utils/structures/shop.js
@@ -1,14 +1,14 @@
 // Dans le cas de champs non renseignés, bien passer "null" et non chaîne vide.
 
 const Shop={
-  booking_request: false;     // true/false
-  my_alfred_conditions: null; // BASIC/PICTURE/ID_CARD/RECOMMEND
-  welcome_message: "";
-  cancel_mode: "";            // FLEXIBLE/MODERATE/STRICT
+  booking_request: false,     // true/false
+  my_alfred_conditions: null, // BASIC/PICTURE/ID_CARD/RECOMMEND
+  welcome_message: "",
+  cancel_mode: "",         // FLEXIBLE/MODERATE/STRICT
   is_particular: true,        // true/false : particulier.pro
   company: {name:null, creation_date:null, siret:null, naf_ape:null, status:null}, //
   service: "service_id",
-  prestations: [{prestation_id:"id_prestation", price:0, billing_id:"id_billing}],
+  prestations: [{prestation_id:"id_prestation", price:0, billing_id:"id_billing"}],
   equipments: [{equipement_id: "quip_id"}], // Ids des équipements
   location: {alfred:false, client:false, visio:false}, // Lieu(x) de prestation
   travel_tax: 0, // Frais de déplacement
@@ -22,4 +22,4 @@ const Shop={
   certification : [{name:"", year:"", picture:""}],
   address: {address:"", city:"", zip:"", country:""}, // Adresse différente ; null si non spécifiée
   perimeter: 0,
-}
+};
