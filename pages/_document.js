b74cf288bc39b1040943956e447a7d9e06c2f396
Sébastien Auvray <sebastien.auvray@wisper.io>
2020-04-02 13:24:40 +0200

    Infra:mise en place document.js EDW pour accélération

--- pages/_document.js
+++ pages/_document.js
@@ -4,58 +4,9 @@ import Document, { Head, Main, NextScript } from 'next/document';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import flush from 'styled-jsx/server';
 import { ServerStyleSheet } from 'styled-components';
+import { ServerStyleSheets } from '@material-ui/core/styles';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    let pageContext;
-    const page = ctx.renderPage((Component) => {
-      const WrappedComponent = (props) => {
-        // eslint-disable-next-line prefer-destructuring
-        pageContext = props.pageContext;
-        return <Component {...props} />;
-      };
-
-      WrappedComponent.propTypes = {
-        // eslint-disable-next-line react/forbid-prop-types
-        pageContext: PropTypes.object.isRequired,
-      };
-
-      return WrappedComponent;
-    });
-
-    let css;
-    if (pageContext) {
-      css = pageContext.sheetsRegistry.toString();
-    }
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-    try {
-      ctx.renderPage = () => originalRenderPage({
-        enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
-      });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        ...page,
-        pageContext,
-        styles: (
-          <React.Fragment>
-            <style
-              id="jss-server-side"
-                // eslint-disable-next-line react/no-danger
-              dangerouslySetInnerHTML={{ __html: css }}
-            />
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-            {flush() || null}
-          </React.Fragment>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
 
   render() {
     const { pageContext } = this.props;
@@ -87,5 +38,46 @@ class MyDocument extends Document {
     );
   }
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
+
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+  };
+};
 
 export default MyDocument;
