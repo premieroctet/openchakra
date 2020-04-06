3cf12832ac8315411518c2eb437f385ece6ff1e6
shinework <adrien.baptiste@gmail.com>
2020-04-06 15:05:39 +0200

    Use backgroundColor instead of bg props

--- src/components/inspector/panels/components/AvatarBadgePanel.tsx
+++ src/components/inspector/panels/components/AvatarBadgePanel.tsx
@@ -1,9 +1,14 @@
-import React, { memo } from "react";
-import ColorsControl from "../../controls/ColorsControl";
+import React, { memo } from 'react'
+import ColorsControl from '../../controls/ColorsControl'
 
 const AvatarPanel = () => (
   <>
-    <ColorsControl withFullColor label="Color" name="bg" enableHues />
+    <ColorsControl
+      withFullColor
+      label="Color"
+      name="backgroundColor"
+      enableHues
+    />
 
     <ColorsControl
       withFullColor
@@ -12,6 +17,6 @@ const AvatarPanel = () => (
       enableHues
     />
   </>
-);
+)
 
-export default memo(AvatarPanel);
+export default memo(AvatarPanel)
