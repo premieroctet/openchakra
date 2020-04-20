dd6a899a9ce3ed71741467872b6d1780caaa1c41
Sébastien Auvray <sebastien.auvray@wisper.io>
2020-04-20 14:46:00 +0200

    Dashboard : export boutiques

--- pages/dashboard/home.js
+++ pages/dashboard/home.js
@@ -10,6 +10,7 @@ import Router from 'next/router';
 import Layout from '../../hoc/Layout/Layout';
 import axios from "axios";
 import Link from "next/link";
+import { CSVLink} from "react-csv";
 
 
 const jwt = require('jsonwebtoken');
@@ -40,7 +41,8 @@ class home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            is_admin: ''
+            is_admin: '',
+            shopsData: [],
         }
     }
 
@@ -50,63 +52,65 @@ class home extends React.Component {
         if(auth === null) {
             Router.push('/login')
         } else {
-
-
             const token = localStorage.getItem('token').split(' ')[1];
             const decode = jwt.decode(token);
             this.setState({is_admin: decode.is_admin});
         }
+        axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
+        axios.get("/myAlfred/api/admin/shops/extract")
+          .then (res => this.setState({shopsData: res.data}))
+          .catch (err => console.log(err));
     }
-
 
     render() {
         const { classes } = this.props;
         const admin= this.state.is_admin;
-        const list =
-                    <Grid>
-                        <Grid item style={{ display: 'flex', justifyContent: 'center' }}>
-                            <Typography style={{ fontSize: 30 }}>Moniteur</Typography>
-                        </Grid>
-                        <Link href="/dashboard/users"><a>Suivi des inscrits</a></Link><br/>
-                        <Grid item style={{ display: 'flex', justifyContent: 'center' }}>
-                            <Typography style={{ fontSize: 30 }}>Base de données</Typography>
-                        </Grid>
-                        <Link href="/dashboard/category/all"><a>Catégories</a></Link><br/>
-                        <Link href="/dashboard/billing/all"><a>Méthodes de facturation</a></Link><br/>
-                        <Link href="/dashboard/calculating/all"><a>Méthodes de calcul</a></Link><br/>
-                        <Link href="/dashboard/filterPresentation/all"><a>Filtres de présentation</a></Link><br/>
-                        <Link href="/dashboard/job/all"><a>Métiers</a></Link><br/>
-                        <Link href="/dashboard/searchFilter/all"><a>Filtres de recherche</a></Link><br/>
-                        <Link href="/dashboard/tags/all"><a>Tags</a></Link><br/>
-                        <Link href="/dashboard/equipments/all"><a>Equipements</a></Link><br/>
-                        <Link href="/dashboard/shopBanner/all"><a>Photos bannière shop</a></Link><br/>
-                        <Link href="/dashboard/services/all"><a>Services</a></Link><br/>
-                        <Link href="/dashboard/prestations/all"><a>Prestations</a></Link><br/>
-                        <Link href="/dashboard/options/all"><a>Options</a></Link><br/>
-                        <Link href="/dashboard/users/all"><a>Utilisateurs</a></Link><br/>
-                        <Link href="/dashboard/alfred/all"><a>Alfred</a></Link><br/>
-                        <Link href="/dashboard/admin/all"><a>Administrateurs</a></Link><br/>
-                        <Grid item style={{ display: 'flex', justifyContent: 'center' }}>
-                            <Typography style={{ fontSize: 30 }}>Fonctions DEV</Typography>
-                        </Grid>
-                        <Link href="/dashboard/dev/api_calls"><a>Appels API</a></Link><br/>
-                    </Grid>;
-        const refused = <Grid item style={{ display: 'flex', justifyContent: 'center' }}>
-            <Typography style={{ fontSize: 30 }}>Accès refusé</Typography>
-        </Grid>;
 
         return (
-            <Layout>
+          <Layout>
                 <Grid container className={classes.signupContainer}>
                     <Card className={classes.card}>
                         <Grid>
-                            {admin ? list : refused}
+                            {admin ?
+                              <Grid>
+                                  <Grid item style={{ display: 'flex', justifyContent: 'center' }}>
+                                      <Typography style={{ fontSize: 30 }}>Moniteur</Typography>
+                                  </Grid>
+                                  <Link href="/dashboard/users"><a>Suivi des inscrits</a></Link><br/>
+                                  <Grid item style={{ display: 'flex', justifyContent: 'center' }}>
+                                      <Typography style={{ fontSize: 30 }}>Base de données</Typography>
+                                  </Grid>
+                                  <Link href="/dashboard/category/all"><a>Catégories</a></Link><br/>
+                                  <Link href="/dashboard/billing/all"><a>Méthodes de facturation</a></Link><br/>
+                                  <Link href="/dashboard/calculating/all"><a>Méthodes de calcul</a></Link><br/>
+                                  <Link href="/dashboard/filterPresentation/all"><a>Filtres de présentation</a></Link><br/>
+                                  <Link href="/dashboard/job/all"><a>Métiers</a></Link><br/>
+                                  <Link href="/dashboard/searchFilter/all"><a>Filtres de recherche</a></Link><br/>
+                                  <Link href="/dashboard/tags/all"><a>Tags</a></Link><br/>
+                                  <Link href="/dashboard/equipments/all"><a>Equipements</a></Link><br/>
+                                  <Link href="/dashboard/shopBanner/all"><a>Photos bannière shop</a></Link><br/>
+                                  <Link href="/dashboard/services/all"><a>Services</a></Link><br/>
+                                  <Link href="/dashboard/prestations/all"><a>Prestations</a></Link><br/>
+                                  <Link href="/dashboard/options/all"><a>Options</a></Link><br/>
+                                  <Link href="/dashboard/users/all"><a>Utilisateurs</a></Link><br/>
+                                  <Link href="/dashboard/alfred/all"><a>Alfred</a></Link><br/>
+                                  <Link href="/dashboard/admin/all"><a>Administrateurs</a></Link><br/>
+                                  <CSVLink asyncOnClick={true} data={this.state.shopsData} target="_blank">Export boutiques</CSVLink>
+                                  <Grid item style={{ display: 'flex', justifyContent: 'center' }}>
+                                      <Typography style={{ fontSize: 30 }}>Fonctions DEV</Typography>
+                                  </Grid>
+                                  <Link href="/dashboard/dev/api_calls"><a>Appels API</a></Link><br/>
+                              </Grid>
+                              :
+                              <Grid item style={{ display: 'flex', justifyContent: 'center' }}>
+                                  <Typography style={{ fontSize: 30 }}>Accès refusé</Typography>
+                              </Grid>
+                            }
 
                         </Grid>
                     </Card>
                 </Grid>
-            </Layout>
-
+                </Layout>
         );
     };
 }
