648113d0d57f6dd22bd41e2f45f7a5982e5b1ec1
Edwin <lienard.edwin@gmail.com>
2020-05-07 16:07:09 +0200

    bg position signup fix

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,4 @@
-import React, { Fragment } from 'react';
-
+import React from 'react';
 import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
 import { Typography } from '@material-ui/core';
@@ -11,82 +10,15 @@ import Router from 'next/router';
 import Layout from '../hoc/Layout/Layout';
 import axios from "axios";
 import Checkbox from "@material-ui/core/Checkbox";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
 import AlgoliaPlaces from "algolia-places-react";
-import DatePicker, {registerLocale,setDefaultLocale} from "react-datepicker";
+import DatePicker, {registerLocale} from "react-datepicker";
 import fr from 'date-fns/locale/fr';
-import Footer from '../hoc/Layout/Footer/Footer';
 import { toast } from 'react-toastify';
 import {Helmet} from 'react-helmet';
+import styles from './signup/signupStyle'
 
 registerLocale('fr', fr);
 
-const styles = theme => ({
-  fullContainer: {
-    backgroundImage: 'url(../static/background/connexion_inscription.png)',
-    backgroundPosition: 'center',
-    backgroundSize: 'contain',
-    backgroundRepeat:'no-repeat',
-    alignItems: 'center',
-    height: '180vh',
-    justifyContent: 'top',
-    flexDirection: 'column',
-  },
-  signupContainer: {
-    backgroundColor: 'rgba(0,0,0, 0.15)',
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-    alignItems: 'center',
-    height: '180vh',
-    flexDirection: 'column',
-    position: 'absolute',
-    top: '82%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    zIndex: '2',
-  },
-  card: {
-    //padding: '1.5rem 3rem',
-    maxWidth: 600,
-    marginTop: 170,
-    boxShadow: '0px 2px 66px -37px rgba(10,10,10,0.65)',
-    [theme.breakpoints.down("xs")]:{
-      marginTop: 250
-    }
-  },
-  datenaissance: {
-    marginTop: 20,
-    width: '100%'
-  },
-  banner: {
-    marginBottom: 25,
-    backgroundColor: '#2FBCD3',
-    height: 80,
-
-  },
-  newContainer: {
-    padding: 10,
-  },
-  title: {
-    color: 'white',
-    display: 'flex',
-    justifyContent: 'center',
-    marginTop: 0,
-    paddingTop: 22,
-    letterSpacing: 1,
-  },
-  country: {
-    width: '100%'
-  },
-
-  birthday:{
-    height:40,
-    fontSize: '0.9rem'
-  }
-
-
-
-});
 
 class signup extends React.Component {
       constructor(props) {
