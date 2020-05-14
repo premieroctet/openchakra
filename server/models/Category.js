f962d1ee248827ea5de630949663e3843fa190d5
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2020-05-14 18:41:02 +0200

    Seartch : implémentation metaphone

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+var metaphone = require('metaphone')
 
 const CategorySchema = new Schema({
     label: {
@@ -15,9 +16,24 @@ const CategorySchema = new Schema({
     tags: [{
     type: Schema.Types.ObjectId,
     ref: 'tag'
-    }]
+  }],
+  s_label: {
+    type: String,
+    default: function() {
+      return metaphone(this.label)
+    }
+  },
 });
 
 CategorySchema.index({label:'text'});
 
-module.exports = Category = mongoose.model('category',CategorySchema);
+const Category = mongoose.model('category',CategorySchema);
+
+// To update s_label
+Category.find({})
+  .then (categories => {
+    categories.forEach( c => c.save());
+  }
+)
+
+module.exports = Category;
