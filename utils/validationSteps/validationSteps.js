8e5979543e56cfc101c1e701c25dc8a4329f9593
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2020-05-27 14:44:43 +0200

    Prestations :vérification prix à 0

--- utils/validationSteps/validationSteps.js
+++ utils/validationSteps/validationSteps.js
@@ -9,10 +9,11 @@ const selectService = (shop) =>{
 };
 
 const selectPrestation = (shop) =>{
+  console.debug(`Validating prestations : ${JSON.stringify(shop.prestations)}`)
   if (Object.keys(shop.prestations).length===0) return "disabled";
   return !Object.values(shop.prestations)
     .every(v => {
-      return !(v.price === 0 || v.billing == null || Object.keys(v.billing).length === 0);
+      return !(v.price == 0 || v.billing == null || Object.keys(v.billing).length === 0);
     });
 };
 
