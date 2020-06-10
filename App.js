ef3985768b03581e1c4fe2485db9b993a258ec03
Edwin <lienard.edwin@gmail.com>
2020-06-10 16:42:48 +0200

    add margin

--- App.js
+++ App.js
@@ -11,67 +11,27 @@ import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
-  View,
-  Text,
-  StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import { WebView } from 'react-native-webview';
 
 const App: () => React$Node = () => {
   return (
-    <>
-      <WebView
-        source={{ uri: 'https://my-alfred.io/' }}
-      />
-    </>
+      <>
+        <SafeAreaView style={styles.flexContainer}>
+            <WebView
+                source={{ uri: 'https://my-alfred.io/' }}
+                style={{ marginTop: 20 }}
+            />
+        </SafeAreaView>
+      </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  flexContainer: {
+    flex: 1
+  }
 });
 
 export default App;
