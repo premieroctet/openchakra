900735052bd6bac0e97c9f82dba758ce6b8517d5
Edwin <lienard.edwin@gmail.com>
2020-06-15 17:02:57 +0200

    add icon ios

--- App.js
+++ App.js
@@ -1,87 +1,89 @@
-import React, { Fragment } from "react";
+import React, { Component } from "react";
 import {
-    SafeAreaView,
     StyleSheet,
-    ScrollView,
     View,
     Text,
-    BackHandler,
-    StatusBar
+    ScrollView,
+    TouchableOpacity,
+    ActivityIndicator,
+    StatusBar,
 } from "react-native";
-import { WebView } from "react-native-webview";
-import {
-    Header,
-    LearnMoreLinks,
-    Colors,
-    DebugInstructions,
-    ReloadInstructions
-} from "react-native/Libraries/NewAppScreen";
+import {WebView} from 'react-native-webview';
+import {Icon} from 'native-base'
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.startingUrl =
-            "https://my-alfred.io/";
-        this.handleBackButton = this.handleBackButton.bind(this);
-    }
-
-    componentDidMount() {
-        BackHandler.addEventListener("hardwareBackPress", this.handleBackButton);
-    }
-
-    componentWillUnmount() {
-        BackHandler.removeEventListener("hardwareBackPress", this.handleBackButton);
-    }
-
-    handleBackButton = () => {
-        console.log(this.state);
-        const { canGoBack } = this.state;
-        if (canGoBack) {
-            this.webView.goBack();
-            return true;
-        } else {
-            return false;
-        }
+    state = {
+        WEBVIEW_REF: "weViewRed",
+        loading: false
+    };
+    goBack = () => {
+        this.refs[this.state.WEBVIEW_REF].goBack();
+    };
+    goForward = () => {
+        this.refs[this.state.WEBVIEW_REF].goForward();
     };
 
     render() {
         return (
-            <Fragment>
+            <View style={{ flex: 1 }}>
                 <WebView
-                    source={{ uri: this.startingUrl }}
-                    style={{ marginTop: 20 }}
-                    ref={webView => (this.webView = webView)}
-                    injectedJavaScript={`
-                      (function() {
-                        function wrap(fn) {
-                          return function wrapper() {
-                            var res = fn.apply(this, arguments);
-                            window.ReactNativeWebView.postMessage('navigationStateChange');
-                            return res;
-                          }
-                        }
-            
-                        history.pushState = wrap(history.pushState);
-                        history.replaceState = wrap(history.replaceState);
-                        window.addEventListener('popstate', function() {
-                          window.ReactNativeWebView.postMessage('navigationStateChange');
-                        });
-                      })();
-            
-                      true;
-                    `}
-                    onMessage={({ nativeEvent: state }) => {
-                        if (state.data === "navigationStateChange") {
-                            // Navigation state updated, can check state.canGoBack, etc.
-                            this.setState({
-                                canGoBack: state.canGoBack
-                            });
-                        }
-                    }}
+                    source={{ uri: "https://my-alfred.io/" }}
+                    ref={this.state.WEBVIEW_REF}
+                    startInLoadingState={true}
+                    renderLoading={() => (
+                        <ActivityIndicator
+                            color='black'
+                            size='large'
+                            style={styles.flexContainer}
+                        />
+                    )}
                 />
-            </Fragment>
+                <View style={styles.footer}>
+                    <TouchableOpacity
+                        onPress={() => this.goBack()}
+                        disabled={this.state.canGoBack}
+                    >
+                        <Icon type={"Ionicons"} name={'arrow-back-outline'}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text style={styles.icon}>⭐️</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => this.goForward()}>
+                        <Icon type={"Ionicons"} name={'arrow-forward'}/>
+                    </TouchableOpacity>
+                </View>
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    flexContainer: {
+        flex: 1
+    },
+    header: {
+        paddingTop: 40,
+        paddingBottom: 10,
+        backgroundColor: "#0c084c"
+    },
+    title: {
+        color: "#fff",
+        fontSize: 20,
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    icon: {
+        color: "#fff",
+        fontSize: 20,
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    footer: {
+        padding: 20,
+        flexDirection: "row",
+        justifyContent: "space-around",
+        backgroundColor: "#2FBCD3"
+    }
+});
 
 export default App;
