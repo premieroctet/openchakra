968ffe9b3ca063e1b95233790b1b193f35c14ae9
Mateo Martinez <mateo.martinez.7630@gmail.com>
2020-06-26 16:49:58 +0200

    Authentification : création du cookie

--- server/routes/api/authentication.js
+++ server/routes/api/authentication.js
@@ -1,17 +1,21 @@
 const router = require("express").Router();
 const passport = require("passport");
+const jwt = require('jsonwebtoken');
 
-const googleAuth = passport.authenticate('google', { scope: ['profile email https://www.googleapis.com/auth/user.addresses.read https://www.googleapis.com/auth/user.birthday.read'] })
+
+const googleAuth = passport.authenticate('google', { session:false, scope: ['profile email https://www.googleapis.com/auth/user.addresses.read https://www.googleapis.com/auth/user.birthday.read'] })
 
 router.get('/google', googleAuth)
 
 // @Route GET /myAlfred/api/authentication/google_hook
 // Get all availability for one user
-router.get("/google_hook",
-    passport.authenticate("google", {
-        successRedirect: "/",
-        failureRedirect: "/login"
-    })
+router.get("/google_hook", googleAuth, (req,res) => {
+        const payload = {id: "5ebbfe46bed2fb7e1ba6a052", name: "Martinez", firstname: "Mateo", is_admin: false, is_alfred: false}; // Create JWT payload
+
+        jwt.sign(payload, "secret", (err, token) => {
+            res.send({success: true, token: 'Bearer ' + token});
+        });
+    }
 )
 
 module.exports = router;
