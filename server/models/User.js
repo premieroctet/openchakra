b1a0461b0c859865a67d3336755c813ea3100ed2
Mateo Martinez <mateo.martinez.7630@gmail.com>
2020-07-05 20:42:46 +0200

    Authentification: Google - Ajout d'un utilisateur inexistant à la base de données

--- server/models/User.js
+++ server/models/User.js
@@ -24,7 +24,6 @@ const UserSchema = new Schema({
     birthday: {
         type: Date,
         max: `${year}-01-01`,
-        required: true
     },
     phone: {
         type: String
--- server/routes/api/authentication.js
+++ server/routes/api/authentication.js
@@ -17,16 +17,16 @@ router.get("/google_hook", googleAuth, (req,res) => {
     console.log("réponse___: ",req.user,"___fin")
     User.findOne({email: req.user.emails[0].value})
         .then (user => {
-            if (user.external_auth.provider === "google") {
-                console.log("utilisateur trouvé")
-                sendCookie(res, user)
-            } else if (user.email) {
-                console.error("mail présent dans la base")
-                res.status(403).redirect('/login?error=existingEmail')
-            } else {
+            if(!user){
                 console.log("utilisateur inexistant, création...")
                 const user = addUserDB(req.user)
                 sendCookie(res, user)
+            } else if (user.external_auth.provider === "google") {
+                console.log("utilisateur trouvé")
+                sendCookie(res, user)
+            } else {
+                console.error("mail présent dans la base")
+                res.status(403).redirect('/login?error=existingEmail')
             }
         })
 })
@@ -43,7 +43,33 @@ const sendCookie = (res,user) => {
 }
 
 const addUserDB = (user) => {
-
+    console.log(user)
+    const newUser = new User({
+        name: user.name.familyName,
+        //gender:
+        firstname: user.name.givenName,
+        email: user.emails[0].value,
+        //birthday:
+        //phone:
+        picture: user.picture,
+        /*billing_address:{
+            address: this.state.address,
+            zip_code: this.state.zip_code,
+            city: this.state.city,
+            country: this.state.country,
+            lat: this.state.lat,
+            lng: this.state.lng,
+        }*/
+        external_auth: {
+            provider: user.provider,
+            id: user.id
+        }
+    })
+    console.log(newUser)
+    newUser.save()
+        .then(user => {
+            return(user) //TODO: fixer le retour de l'utilisateur enregistré
+        }).catch(err => console.error(err))
 }
 
 module.exports = router;
