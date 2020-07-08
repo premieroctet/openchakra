ade407759b512300c2a29e696884b1d7ee148774
Edwin <lienard.edwin@gmail.com>
2020-07-08 16:37:28 +0200

    remove commentary

--- components/Information/Information.js
+++ components/Information/Information.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
-import Grid from '@material-ui/core/Grid';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
