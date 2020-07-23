b4bb96cdbd3b3b728f6e67090e61a70163b5a16b
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2020-07-23 14:44:17 +0200

    Login : fix /login => redirect to /

---
+++ pages/login.js
@@ -0,0 +1,15 @@
+import {Component} from 'react';
+import Router from "next/router";
+
+class login extends Component {
+
+  componentDidMount() {
+      Router.push('/')
+  }
+
+  render() {
+    return null
+  }
+}
+
+export default login;
