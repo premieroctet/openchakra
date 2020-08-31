b1cc0f88eee632d95a888ff3151fea2190e6f8a0
Hugo FOYART <hfoyart@premieroctet.com>
2020-08-31 16:01:39 +0200

    Fix background grid

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -13,7 +13,7 @@ export const gridStyles = {
   backgroundImage:
     'linear-gradient(to right, #d9e2e9 1px, transparent 1px),linear-gradient(to bottom, #d9e2e9 1px, transparent 1px);',
   backgroundSize: '20px 20px',
-  bg: '#edf2f6',
+  bgColor: '#edf2f6',
   p: 10,
 }
 
