66b4ffef2769b8d557a44b772db5e1c6fd548b48
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2020-09-17 10:21:38 +0200

    Calendar : test

--- web/pages/test/calendar.js
+++ web/pages/test/calendar.js
@@ -12,33 +12,32 @@ class calendarTest extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      current_date: new Date(),
+      date: new Date(),
     };
   }
 
   prevMonth= () => {
-    var date = this.state.current_date
+    var date = this.state.date
     date.setMonth(date.getMonth()-1)
-    this.setState({ current_date: date})
+    this.setState({ date: date})
   }
 
   nextMonth= () => {
-    var date = this.state.current_date
+    var date = this.state.date
     date.setMonth(date.getMonth()+1)
-    this.setState({ current_date: date})
+    this.setState({ date: date})
   }
 
   render() {
     const {classes} = this.props
-    const {current_date} = this.state
+    const {date} = this.state
 
-    //const offsets=[-1,0,1]
-    const offsets=[0,1]
+    //const offsets=[-1, 0, 1]
+    const offsets=[-1,0,1]
     const dates= offsets.map( off => {
-      var dt=new Date(current_date)
-      dt.setMonth(current_date.getMonth()+off)
-      console.log(`${current_date} => ${dt}`)
-      return new Date(dt)
+      var dt=new Date(date)
+      dt.setMonth(date.getMonth()+off)
+      return dt
     })
 
     console.log(`Dates:${dates}`)
@@ -47,40 +46,26 @@ class calendarTest extends React.Component {
       <Grid>
       <Button onClick={this.prevMonth}>Prev</Button>
       <Button onClick={this.nextMonth}>Next</Button>
-      <h1>Calendriers multiples</h1>
-      <>
-      {dates.map( dt => (
-         <Calendar
-          culture={'fr-FR'}
+      <h1>Calendriers multiples avec map => NOK</h1>
+      {dates.map( dt => {
+        return <Calendar
+          events={[]}
+          localizer={localizer}
+          views={[Views.MONTH]}
+          defaultView={Views.MONTH}
+          defaultDate={new Date(dt)}
+          />
+        })}
+      <h1>Calendrier unique avec map => NOK</h1>
+      {[date].map( dt => {
+        return <Calendar
           events={[]}
           localizer={localizer}
           views={[Views.MONTH]}
           defaultView={Views.MONTH}
           defaultDate={dt}
-          dayLayoutAlgorithm={'no-overlap'}
           />
-        ))}
-      </>
-        <h1>Calendrier seul avec map()</h1>
-        {[current_date].map( dt => {
-          return <Calendar
-            culture={'fr-FR'}
-            events={[]}
-            localizer={localizer}
-            views={[Views.MONTH]}
-            defaultView={Views.MONTH}
-            defaultDate={dt}
-            />
-          })}
-        <h1>Calendrier seul sans map()</h1>
-        <Calendar
-          culture={'fr-FR'}
-          events={[]}
-          localizer={localizer}
-          views={[Views.MONTH]}
-          defaultView={Views.MONTH}
-          defaultDate={current_date}
-        />
+        })}
       </Grid>
     );
   }
