5447de0374fa9e6fdddc45611705d287735ce87b
Edwin <lienard.edwin@gmail.com>
2020-10-28 12:11:52 +0100

    confirmpayment - add - style success payment

--- web/pages/paymentSuccess.js
+++ web/pages/paymentSuccess.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
+import Typography from "@material-ui/core/Typography";
 import Router from 'next/router';
 import {withStyles} from '@material-ui/core/styles';
 import io from 'socket.io-client';
 import cookie from 'react-cookies';
 import LayoutPayment from "../hoc/Layout/LayoutPayment";
-import styles from ''
+import styles from '../static/css/pages/paymentSuccess/paymentSuccess'
 
 class paymentSuccess extends React.Component {
   constructor(props) {
@@ -65,7 +66,29 @@ class paymentSuccess extends React.Component {
     return (
       <React.Fragment>
         <LayoutPayment>
+          <Grid style={{display: 'flex', backgroundColor: 'rgba(249,249,249, 1)', width: '100%', justifyContent: 'center', padding: '10%', minHeight: '80vh'}}>
+            <Grid style={{display: 'flex', justifyContent: 'center', width: '50%', backgroundColor: 'white', borderRadius: 27, border: '1px solid rgba(210, 210, 210, 0.5)', paddingLeft: '10%', paddingTop: '5%', paddingBottom: '5%', paddingRight: '10%', textAlign: 'center'}}>
+              <Grid style={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                <Grid style={{display: 'flex', flexDirection: 'column'}}>
+                  <Grid>
+                    <h2>Votre service a été réservé avec succès !</h2>
+                  </Grid>
+                  <Grid>
+                    <Typography>Merci de nous faire confiance.</Typography>
+                  </Grid>
+                </Grid>
+                <Grid>
+                  <Grid>
+                    <Typography>Vous allez être redirigé vers votre page Mes Réservations.</Typography>
+                  </Grid>
+                  <Grid>
+                    <Typography>Si la redirection ne fonctionne pas <a href={'#'}>cliquez ici</a></Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
 
+            </Grid>
+          </Grid>
         </LayoutPayment>
       </React.Fragment>
     );
