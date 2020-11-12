642584a911f6323f6244ec2ae0732419d954abef
Armand_D <armand.deshais@mail.com>
2020-11-12 16:37:48 +0100

    ajout lien footer

--- web/hoc/Layout/Footer/Footer.js
+++ web/hoc/Layout/Footer/Footer.js
@@ -15,106 +15,136 @@ import Divider from "@material-ui/core/Divider";
 import Paper from "@material-ui/core/Paper";
 
 class Footer extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    const {style} = this.props;
-    return (
-      <Grid className={style.footerMainStyle}>
-        <Grid>
-          <Grid className={style.footerMainContainer}>
-            <Grid className={style.footerSection}>
-              <Grid>
-                <h3 className={style.footerTitileSection}>À propos</h3>
-              </Grid>
-              <Grid>
-                <p className={style.footerLink}>Pourquoi My Alfred ?</p>
-              </Grid>
-              <Grid>
-                <p className={style.footerLink}>Notre histoire</p>
-              </Grid>
-              <Grid>
-                <p className={style.footerLink}>Nous contacter</p>
-              </Grid>
-            </Grid>
-            <Grid className={style.footerSection}>
-              <Grid>
-                <h3 className={style.footerTitileSection}>Communauté</h3>
-              </Grid>
-              <Grid>
-                <p className={style.footerLink}>Notre communauté</p>
-              </Grid>
-              <Grid>
-                <p className={style.footerLink}>Inviter un ami</p>
-              </Grid>
-            </Grid>
-            <Grid className={style.footerSection}>
-              <Grid>
-                <h3 className={style.footerTitileSection}>Alfred</h3>
-              </Grid>
-              <Grid>
-                <p className={style.footerLink}>Devenir Alfred</p>
-              </Grid>
-              <Grid>
-                <p className={style.footerLink}>Centre de ressources</p>
-              </Grid>
-            </Grid>
-            <Grid className={style.footerSection}>
-              <Grid>
-                <h3 className={style.footerTitileSection}>Assistance</h3>
-              </Grid>
-              <Grid>
-                <p className={style.footerLink}>Les bases</p>
-              </Grid>
-              <Grid>
-                <p className={style.footerLink}>FAQ</p>
-              </Grid>
-              <Grid>
-                <p className={style.footerLink}>Parler à un humain</p>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid className={style.footerSocialSection}>
-            <Grid>
-              <FacebookIcon/>
-            </Grid>
-            <Grid>
-              <InstagramIcon/>
-            </Grid>
-            <Grid>
-              <LinkedInIcon/>
-            </Grid>
-            <Grid>
-              <TwitterIcon/>
-            </Grid>
-          </Grid>
-          <Grid className={style.footerDividerContainer}>
-            <Divider className={style.footerDivider}/>
-          </Grid>
-          <Grid className={style.footerBrandContainer}>
-            <Grid className={style.footerBrandStyle}>
-              <Grid>
-                <p className={style.footerText}>© 2020 MY ALFRED Corporation. Tous droits réservés</p>
-              </Grid>
-              <Grid className={style.footerRgpdButtons}>
+    render() {
+        const {style} = this.props;
+        return (
+            <Grid className={style.footerMainStyle}>
                 <Grid>
-                  <p className={style.footerLink}>Sécurité</p>
+                    <Grid className={style.footerMainContainer}>
+                        <Grid className={style.footerSection}>
+                            <Grid>
+                                <h3 className={style.footerTitileSection}>À propos</h3>
+                            </Grid>
+                            <Link href={'/footer/apropos'}>
+                                <Grid>
+                                    <p className={style.footerLink}>My Alfred ?</p>
+                                </Grid>
+                            </Link>
+                            <Link href={'/footer/ourTeam'}>
+                                <Grid>
+                                    <p className={style.footerLink}>Notre équipe</p>
+                                </Grid>
+                            </Link>
+                            <Link href={'/footer/contact'}>
+                                <Grid>
+                                    <p className={style.footerLink}>Nous contacter</p>
+                                </Grid>
+                            </Link>
+                        </Grid>
+                        <Grid className={style.footerSection}>
+                            <Grid>
+                                <h3 className={style.footerTitileSection}>Communauté</h3>
+                            </Grid>
+                            <Link href={'/footer/ourCommunity'}>
+                                <Grid>
+                                    <p className={style.footerLink}>Notre communauté</p>
+                                </Grid>
+                            </Link>
+                            <Link href={'/'}>
+                                <Grid>
+                                    <p className={style.footerLink}>Le blog</p>
+                                </Grid>
+                            </Link>
+                            <Link href={'/'}>
+                                <Grid>
+                                    <p className={style.footerLink}>Inviter un ami</p>
+                                </Grid>
+                            </Link>
+                        </Grid>
+                        <Grid className={style.footerSection}>
+                            <Grid>
+                                <h3 className={style.footerTitileSection}>Alfred</h3>
+                            </Grid>
+                            <Link href={'/footer/becomeAlfred'}>
+                                <Grid>
+                                    <p className={style.footerLink}>Devenir Alfred</p>
+                                </Grid>
+                            </Link>
+                            <Link href={'/faq/home'}>
+                                <Grid>
+                                    <p className={style.footerLink}>FAQ des Alfred</p>
+                                </Grid>
+                            </Link>
+                            <Link href={'/'}>
+                                <Grid>
+                                    <p className={style.footerLink}>Centre de ressources</p>
+                                </Grid>
+                            </Link>
+                        </Grid>
+                        <Grid className={style.footerSection}>
+                            <Grid>
+                                <h3 className={style.footerTitileSection}>Assistance</h3>
+                            </Grid>
+                            <Link href={'/footer/addService'}>
+                                <Grid>
+                                    <p className={style.footerLink}>Réserver un service</p>
+                                </Grid>
+                            </Link>
+                            <Link href={'/faq/home'}>
+                                <Grid>
+                                    <p className={style.footerLink}>FAQ des services</p>
+                                </Grid>
+                            </Link>
+                            <Link href={'/'}>
+                                <Grid>
+                                    <p className={style.footerLink}>Parler à un humain</p>
+                                </Grid>
+                            </Link>
+                        </Grid>
+                    </Grid>
+                    <Grid className={style.footerSocialSection}>
+                        <Grid>
+                            <FacebookIcon/>
+                        </Grid>
+                        <Grid>
+                            <InstagramIcon/>
+                        </Grid>
+                        <Grid>
+                            <LinkedInIcon/>
+                        </Grid>
+                        <Grid>
+                            <TwitterIcon/>
+                        </Grid>
+                    </Grid>
+                    <Grid className={style.footerDividerContainer}>
+                        <Divider className={style.footerDivider}/>
+                    </Grid>
+                    <Grid className={style.footerBrandContainer}>
+                        <Grid className={style.footerBrandStyle}>
+                            <Grid>
+                                <p className={style.footerText}>© 2020 MY ALFRED Corporation. Tous droits réservés</p>
+                            </Grid>
+                            <Grid className={style.footerRgpdButtons}>
+                                <Grid>
+                                    <p className={style.footerLink}>Sécurité</p>
+                                </Grid>
+                                <Grid>
+                                    <p className={style.footerLink}>Informations légales</p>
+                                </Grid>
+                                <Grid>
+                                    <p className={style.footerLink}>Confidentialié</p>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
                 </Grid>
-                <Grid>
-                  <p className={style.footerLink}>Informations légales</p>
-                </Grid>
-                <Grid>
-                  <p className={style.footerLink}>Confidentialié</p>
-                </Grid>
-              </Grid>
             </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-    );
-  }
+        );
+    }
 }
 
 export default Footer;
