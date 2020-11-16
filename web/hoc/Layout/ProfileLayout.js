a2910c4da145b3e99c7b9a8a6d76adb09c7abf7f
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2020-11-16 14:03:44 +0100

    ProfileLayout : Avatar => UserAvatar en attente réutilisation ProfileHeader

--- web/hoc/Layout/ProfileLayout.js
+++ web/hoc/Layout/ProfileLayout.js
@@ -4,14 +4,14 @@ import Grid from "@material-ui/core/Grid";
 import ScrollMenu from '../../components/ScrollMenu/ScrollMenu';
 import cookie from 'react-cookies';
 import axios from 'axios'
-const {getLoggedUserId}=require('../../utils/functions');
+const {isEditableUser}=require('../../utils/functions');
 import moment from 'moment'
 import Box from "../../components/Box/Box";
 import styles from '../../static/css/components/Layout/ProfileLayout/ProfileLayout'
 import withStyles from "@material-ui/core/styles/withStyles";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
-
+import UserAvatar from '../../components/Avatar/UserAvatar'
 
 class ProfileLayout extends React.Component {
 
@@ -19,33 +19,21 @@ class ProfileLayout extends React.Component {
     super(props);
     this.state = {
       user: null,
-      items: [
-      {
-        label: 'À propos',
-        url: '/about'
-      },
-      {
-        label: 'Mes services',
-        url: '/services'
-      },
-      {
-        label: 'Mes photos',
-        url: '/pictures'
-      },
-      {
-        label: 'Mes avis',
-        url: '/reviews'
-      },
-      {
-        label: 'Mon calendrier',
-        url: '/calendar'
-      },
-      {
-        label: 'Mes statistiques',
-        url: '/statistics'
-      }
-      ]
     }
+    this.logged_items= [
+      { label: 'À propos', url: '/about' },
+      { label: 'Mes services', url: '/services' },
+      { label: 'Mes photos', url: '/pictures' },
+      { label: 'Mes avis', url: '/reviews' },
+      { label: 'Mon calendrier', url: '/calendar' },
+      { label: 'Mes statistiques', url: '/statistics'}
+    ]
+    this.nonlogged_items= [
+      { label: 'À propos', url: '/about' },
+      { label: 'Services', url: '/services' },
+      { label: 'Photos', url: '/pictures' },
+      { label: 'Avis', url: '/reviews' },
+    ]
   }
 
   componentDidMount = () => {
@@ -61,14 +49,12 @@ class ProfileLayout extends React.Component {
     const {items, user}=this.state;
     const {children, index, classes}=this.props;
 
-
-
-
     if (!user) {
       return null
     }
 
     const url = user.picture.match(/^https?:\/\//) ? user.picture : '/' + user.picture;
+    const menuItems = isEditableUser(this.props.user) ? this.logged_items : this.nonlogged_items
 
     return (
       <Layout user={user}>
@@ -79,7 +65,7 @@ class ProfileLayout extends React.Component {
                 <Grid className={classes.profilLayoutBox}>
                   <Grid className={classes.profilLayoutBannerImg}>
                     <Grid className={classes.profilLayoutAvatar}>
-                      <Avatar alt={user.firstname} src={url} className={classes.cardPreviewLarge} />
+                      <UserAvatar avatr={user} alt={user.firstname} src={url} className={classes.cardPreviewLarge} />
                     </Grid>
                   </Grid>
                   <Grid style={{display: 'flex', justifyContent: 'center', height: '40%', alignItems: 'center'}}>
@@ -93,7 +79,7 @@ class ProfileLayout extends React.Component {
                     </Grid>
                   </Grid>
                   <Grid className={classes.profilLayoutScrollMenu}>
-                    <ScrollMenu categories={items} mode={'profile'} indexCat={index} extraParams={{user: this.props.user}}/>
+                    <ScrollMenu categories={menuItems} mode={'profile'} indexCat={index} extraParams={{user: this.props.user}}/>
                   </Grid>
                 </Grid>
               </Grid>
