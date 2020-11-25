4d7a51a7a2caddebe301957df2efed3d7e6c83b1
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2020-11-25 11:52:51 +0100

    899542 : Affichage des équipements fournis. Aucun affichage si le service n'en prévoit pas. Affichage si le service en prévoit, même si l'Alfred n'en fournit aucun.

--- web/pages/userServicePreview.js
+++ web/pages/userServicePreview.js
@@ -653,7 +653,7 @@ class UserServicesPreview extends React.Component {
                     />
                   </Topic>
                 </Grid>
-                {this.state.equipments.length !== 0 ?
+                {this.state.allDetailEquipments.length !== 0 ?
                   <Grid className={classes.equipmentsContainer}>
                     <Topic
                       titleTopic={'Matériel'}
