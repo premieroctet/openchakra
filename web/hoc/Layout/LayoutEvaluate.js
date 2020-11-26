0e018445d4b8d132eab2328e43ca0291d80a8fe3
Lienard <edwin.lienard@outlook.fr>
2020-11-26 16:03:47 +0100

    refonte evaluate

--- web/hoc/Layout/LayoutEvaluate.js
+++ web/hoc/Layout/LayoutEvaluate.js
@@ -1,50 +1,19 @@
 import React from 'react';
 import Grid from "@material-ui/core/Grid";
-import ScrollMenu from '../../components/ScrollMenu/ScrollMenu';
+import ScrollMenu from "../../components/ScrollMenu/ScrollMenu";
 import Layout from "./Layout";
-import axios from 'axios'
-import cookie from 'react-cookies';
+import axios from "axios";
+import cookie from "react-cookies";
 
 
-class LayoutAccount extends React.Component{
+class LayoutEvaluate extends React.Component{
+
   constructor(props) {
     super(props);
-    this.state= {
-      user: null,
-      items: [
-        {
-          label: 'Notifications',
-          url: '/notifications'
-        },
-        {
-          label: 'Mes Informations',
-          url: '/editProfile'
-        },
-        {
-          label: 'Mode de paiement',
-          url: '/paymentMethod'
-        },
-        {
-          label: 'Mon RIB',
-          url: '/paymentPreference'
-        },
-        {
-          label: 'Mes adresses',
-          url: '/myAddresses'
-        },
-        {
-          label: 'Vérification',
-          url: '/trustAndVerification'
-        },
-        {
-          label: 'Sécurité',
-          url: '/security'
-        }
-      ]
+    this.state={
+      user: {}
     }
   }
-
-
 
   componentDidMount = () => {
     axios.defaults.headers.common['Authorization'] = cookie.load('token');
@@ -56,18 +25,15 @@ class LayoutAccount extends React.Component{
   };
 
   render() {
-    const{items, user}= this.state;
-    const{children, index} = this.props;
+    const{user}= this.state;
+    const{children}= this.props;
 
     return(
       <Layout user={user}>
         <Grid style={{display:'flex', justifyContent:'center'}}>
           <Grid style={{display: 'flex', justifyContent:'center', flexDirection: 'column', alignItems:'center', width: '100%'}}>
             <Grid style={{display: 'flex', justifyContent: 'center'}}>
-              <h2>Mes paramètres</h2>
-            </Grid>
-            <Grid>
-              <ScrollMenu categories={items} mode={'account'} indexCat={index}/>
+              <h2>Evaluation & commentaires</h2>
             </Grid>
             <Grid style={{backgroundColor: 'rgba(249,249,249, 1)', width: '100%'}}>
               <Grid style={{margin:'0 15%', display:'flex', justifyContent:'center', backgroundColor: 'white', borderRadius: 27, border: '1px solid rgba(210, 210, 210, 0.5)', padding: '5% 10%', marginTop : '5vh', marginBottom: '5vh'}}>
@@ -79,6 +45,7 @@ class LayoutAccount extends React.Component{
       </Layout>
     );
   }
+
 }
 
-export default LayoutAccount
+export default LayoutEvaluate
--- web/pages/evaluate.js
+++ web/pages/evaluate.js
@@ -1,113 +1,19 @@
-import React, {Fragment} from 'react';
-import Layout from '../hoc/Layout/Layout';
+import React from 'react';
 import axios from 'axios';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Router from 'next/router';
 import {withStyles} from '@material-ui/core/styles';
-import Footer from '../hoc/Layout/Footer/Footer';
 import StarRatings from 'react-star-ratings';
 import {toast} from 'react-toastify';
 import TextField from '@material-ui/core/TextField';
 import Skills from '../components/Skills/Skills';
 import cookie from 'react-cookies';
-
-const styles = theme => ({
-  bigContainer: {
-    flexGrow: 1,
-
-  },
-  grosHR: {
-    height: '7px',
-    backgroundColor: '#6ec1e4',
-    width: '76%',
-    float: 'left',
-  },
-  fournitureHR: {
-    height: '5px',
-    backgroundColor: '#6ec1e4',
-    width: '85%',
-    float: 'left',
-  },
-  disponibilityHR: {
-    height: '5px',
-    backgroundColor: '#6ec1e4',
-    width: '103%',
-    float: 'left',
-  },
-  conditionsHR: {
-    height: '5px',
-    backgroundColor: '#6ec1e4',
-    width: '189%',
-    float: 'left',
-  },
-  perimeterHR: {
-    height: '5px',
-    backgroundColor: '#6ec1e4',
-    width: '223%',
-    float: 'left',
-  },
-  dispocard: {
-
-    minHeight: '100px',
-    width: '200px',
-    textAlign: 'center',
-
-    boxShadow: '4px 4px 41px -37px rgba(0,0,0,0.0)',
-    border: 'solid 1px #ccc',
-    borderRadius: '10px',
-
-
-  },
-  dispocardin: {
-
-    padding: '1%',
-    fontSize: '17px',
-    fontWeight: 'bold',
-    marginBottom: 10,
-
-
-  },
-
-  prestationlist: {
-
-    padding: '1%',
-
-    marginBottom: 10,
-    border: 'solid 1px #ccc',
-    borderRadius: '5px',
-
-
-  },
-  prestationside: {
-
-    backgroundColor: 'transparent',
-    Border: '0px #ccc solid',
-    borderRadius: '10px',
-    marginRight: '10px',
-    marginLeft: '10px',
-    height: '30px',
-
-
-  },
-
-  dispoheader: {
-
-    height: '2%',
-    color: 'white',
-    width: '100%',
-    padding: '1%',
-
-    fontSize: '15px',
-    textAlign: 'center',
-
-    borderRadius: '0px',
-    backgroundColor: '#F8727F',
-    marginBottom: '20px',
-
-
-  },
-});
+import Typography from "@material-ui/core/Typography";
+import Hidden from "@material-ui/core/Hidden";
+import LayoutMobile from "../hoc/Layout/LayoutMobile";
+import LayoutEvaluate from "../hoc/Layout/LayoutEvaluate";
+import styles from '../static/css/pages/evaluate/evaluate';
 
 
 class Evaluate extends React.Component {
@@ -126,12 +32,6 @@ class Evaluate extends React.Component {
       reactive: false,
       alfred: {},
     };
-    this.changeRating = this.changeRating.bind(this);
-    this.changeRating2 = this.changeRating2.bind(this);
-    this.changeRating3 = this.changeRating3.bind(this);
-    this.onComplimentChanged = this.onComplimentChanged.bind(this);
-
-
   }
 
   static getInitialProps({query: {booking, id}}) {
@@ -153,7 +53,7 @@ class Evaluate extends React.Component {
       .catch(err => {
           if (err.response.status === 401 || err.response.status === 403) {
             cookie.remove('token', {path: '/'});
-            Router.push({pathname: '/login'});
+            Router.push({pathname: '/'});
           }
         },
       );
@@ -172,29 +72,29 @@ class Evaluate extends React.Component {
     this.setState({[name]: !org});
   };
 
-  changeRating(newRating, name) {
+  changeRating = (newRating, name) => {
     this.setState({
       prestation: newRating,
     });
-  }
+  };
 
-  changeRating2(newRating, name) {
+  changeRating2 = (newRating, name) => {
     this.setState({
       price: newRating,
     });
-  }
+  };
 
-  changeRating3(newRating, name) {
+  changeRating3 = (newRating, name)=> {
     this.setState({
       relational: newRating,
     });
-  }
+  };
 
   back() {
     Router.back();
   }
 
-  evaluate() {
+  evaluate = () => {
     const id = this.props.service_id;
     const booking = this.props.booking;
     const service = this.state.service;
@@ -225,194 +125,141 @@ class Evaluate extends React.Component {
         Router.push(`/reservations/detailsReservation?id=${booking}`);
       })
       .catch(err => {
-        toast.error('Une erreur est survenue');
+       console.error(err)
       });
 
-  }
+  };
+
+  content = (classes) =>{
+    return(
+      <Grid container className={classes.bigContainer}>
+        <Grid style={{width: '100%'}}>
+          <Grid style={{width: '100%',display: 'flex', flexDirection: 'column'}}>
+            <Grid item>
+              <h3>Qu’avez-vous pensé de la prestation de votre Alfred ?</h3>
+            </Grid>
+            <Grid container style={{marginTop: '5vh'}} spacing={3}>
+              <Grid item container xl={12} lg={12} md={12} sm={12} xs={12} style={{display: 'flex'}}>
+                <Grid item xl={6} lg={6} md={6} sm={6} xs={6}>
+                  <Typography>Qualité de la prestation</Typography>
+                </Grid>
+                <Grid  item xl={6} lg={6} md={6} sm={6} xs={6}>
+                  <StarRatings
+                    rating={this.state.prestation}
+                    starRatedColor={'rgba(248, 207, 97, 1)'}
+                    changeRating={this.changeRating}
+                    numberOfStars={5}
+                    name='rating'
+                    starDimension={'20px'}
+                    starHoverColor={'rgba(248, 207, 97, 1)'}
+                    starSpacing={'3px'}
+                />
+                </Grid>
+              </Grid>
+              <Grid  item container xl={12} lg={12} md={12} sm={12} xs={12}>
+                <Grid  item xl={6} lg={6} md={6} sm={6} xs={6}>
+                  <Typography>Qualité - Prix</Typography>
+                </Grid>
+                <Grid  item xl={6} lg={6} md={6} sm={6} xs={6}>
+                  <StarRatings
+                    rating={this.state.price}
+                    starRatedColor={'rgba(248, 207, 97, 1)'}
+                    changeRating={this.changeRating2}
+                    numberOfStars={5}
+                    name='rating2'
+                    starDimension={'20px'}
+                    starHoverColor={'rgba(248, 207, 97, 1)'}
+                    starSpacing={'3px'}
+                />
+                </Grid>
+              </Grid>
+              <Grid  item container xl={12} lg={12} md={12} sm={12} xs={12}>
+                <Grid  item xl={6} lg={6} md={6} sm={6} xs={6}>
+                  <Typography>Relationnel</Typography>
+                </Grid>
+                <Grid  item xl={6} lg={6} md={6} sm={6} xs={6} >
+                  <StarRatings
+                    rating={this.state.relational}
+                    starRatedColor={'rgba(248, 207, 97, 1)'}
+                    changeRating={this.changeRating3}
+                    numberOfStars={5}
+                    name='rating3'
+                    starDimension={'20px'}
+                    starHoverColor={'rgba(248, 207, 97, 1)'}
+                    starSpacing={'3px'}
+                  />
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid style={{marginTop: '10vh'}}>
+          <Skills
+            alfred={this.state.alfred}
+            skills={this.state}
+            widthHr={'100%'}
+            hideCount={true}
+            onClick={this.onComplimentChanged}
+          />
+        </Grid>
+        <Grid style={{marginTop: '10vh'}}>
+          <Grid>
+            <h3>Votre commentaire</h3>
+          </Grid>
+          <Grid>
+            <TextField
+              id="outlined-multiline-static"
+              style={{width: '100%'}}
+              multiline
+              rows="6"
+              variant="outlined"
+              onChange={(e) => this.setState({content: e.target.value})}
+            />
+          </Grid>
+        </Grid>
+        <Grid style={{display: 'flex', justifyContent: 'space-between', marginTop: '10vh'}}>
+          <Grid>
+            <Button
+              onClick={this.back}
+              variant={'outlined'}
+              classes={{root: classes.buttonBack}}
+            >
+              Retour
+            </Button>
+          </Grid>
+          <Grid>
+            <Button
+              disabled={this.state.prestation === 0 || this.state.price === 0 || this.state.relational === 0 || !this.state.content.trim()}
+              onClick={this.evaluate}
+              variant={'contained'}
+              classes={{root: classes.buttonSend}}
+              color={'primary'}
+            >
+              Terminé
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    )
+  };
 
   render() {
     const {classes} = this.props;
     const {user} = this.state;
 
     return (
-      <Fragment>
-        <Layout>
-          <Grid container className={classes.bigContainer}>
-
-            {/*Le Header */}
-
-            {/*Le Contenu */}
-            <Grid container>
-              <br></br>
-              {/*Contenu à Gauche*/}
-
-              {/*Petite Description*/}
-              <Grid item md={5} xs={12}
-                    style={{textAlign: 'left', margin: '0 auto', float: 'right', paddingLeft: '3%'}}>
-                <div style={{margin: '20px 11%', marginTop: '5%', width: '90%'}}></div>
-                <Grid container>
-
-                  <Grid item xs={12} style={{marginTop: 50, marginBottom: 30}}>
-                    <h2 style={{
-                      fontSize: '2.5rem',
-                      color: 'rgba(84,89,95,0.95)',
-                      letterSpacing: -1,
-                      fontWeight: '100',
-                      textAlign: 'left',
-                    }}>Evaluation & commentaires</h2>
-
-                  </Grid>
-                </Grid>
-
-
-                <div>
-
-                  <Grid container>
-
-                    <Grid item xs={12} style={{textAlign: 'left'}}>
-
-
-                      <p style={{fontSize: '25px'}}>Qu’avez-vous pensé de la prestation de votre Alfred ?</p>
-
-
-                      <br></br>
-
-                    </Grid>
-                    <Grid item xs={7} style={{textAlign: 'right'}}>
-                      <Grid container>
-                        <Grid item xs={7}><p style={{fontSize: '18px'}}>Qualité de la prestation</p></Grid>
-                        <Grid item xs={5} style={{lineHeight: '4'}}><StarRatings
-                          rating={this.state.prestation}
-                          starRatedColor={'#2FBCD3'}
-                          changeRating={this.changeRating}
-                          numberOfStars={5}
-                          name='rating'
-                          starDimension={'20px'}
-                          starHoverColor={'#2FBCD3'}
-                          starSpacing={'3px'}
-                        /></Grid>
-                      </Grid>
-                      <Grid container>
-                        <Grid item xs={7}>
-                          <p style={{fontSize: '18px'}}>Qualité - Prix</p>
-                        </Grid>
-                        <Grid item xs={5} style={{lineHeight: '4'}}><StarRatings
-                          rating={this.state.price}
-                          starRatedColor={'#2FBCD3'}
-                          changeRating={this.changeRating2}
-                          numberOfStars={5}
-                          name='rating2'
-                          starDimension={'20px'}
-                          starHoverColor={'#2FBCD3'}
-                          starSpacing={'3px'}
-                        /></Grid>
-                      </Grid>
-                      <Grid container>
-                        <Grid item xs={7}>
-                          <p style={{fontSize: '18px'}}>Relationnel</p>
-                        </Grid>
-                        <Grid item xs={5} style={{lineHeight: '4'}}><StarRatings
-                          rating={this.state.relational}
-                          starRatedColor={'#2FBCD3'}
-                          changeRating={this.changeRating3}
-                          numberOfStars={5}
-                          name='rating3'
-                          starDimension={'20px'}
-                          starHoverColor={'#2FBCD3'}
-                          starSpacing={'3px'}
-                        /></Grid>
-                      </Grid>
-
-                    </Grid>
-                  </Grid>
-
-                </div>
-                <Grid className={classes.skillsContentContainer}>
-                  <Skills alfred={this.state.alfred} skills={this.state} widthHr={'100%'} hideCount={true}
-                          onClick={this.onComplimentChanged}/>
-                </Grid>
-                <div>
-                  <br></br>
-                  <p style={{fontSize: '25px'}}>Votre commentaire</p>
-
-                  {/*<form class="example-form">
-
-
-                                        <mat-form-field class="example-full-width">
-                                            <textarea onChange={(e)=>this.setState({content:e.target.value})}
-                                                      style={{width:'100%', height:'150px', borderRadius:'30px'}} matInput placeholder=""></textarea>
-                                        </mat-form-field>
-                                    </form>*/}
-                  <Grid container>
-                    <Grid item xs={12}>
-                      <TextField
-                        id="outlined-multiline-static"
-                        style={{width: '100%'}}
-                        multiline
-                        rows="6"
-                        margin="normal"
-                        variant="outlined"
-                        onChange={(e) => this.setState({content: e.target.value})}
-                      />
-                    </Grid>
-                  </Grid>
-                </div>
-
-
-                {/*cadre avec couleur et checkbox*/}
-
-                <br></br>
-
-                <Grid style={{float: 'left'}} item xs={6}> <Button onClick={() => this.back()} color={'white'}
-                                                                   variant={'contained'} style={{
-                  color: 'gray',
-                  backgroundColor: 'white',
-                  fontSize: '16px',
-                  width: '100%',
-                  paddingLeft: '20px',
-                  paddingRight: '20px',
-                  marginBottom: 50,
-                  marginRight: 20,
-                  borderRadius: '20px',
-                  textTransform: 'capitalize',
-                }}>Retour</Button>
-                </Grid>
-                <Grid style={{float: 'right'}} item xs={6}> <Button
-                  disabled={this.state.prestation === 0 || this.state.price === 0 || this.state.relational === 0 || !this.state.content.trim()}
-                  onClick={() => this.evaluate()} color={'primary'} variant={'contained'} style={{
-                  color: 'white',
-                  fontSize: '16px',
-                  width: '100%',
-                  paddingLeft: '20px',
-                  paddingRight: '20px',
-                  marginBottom: 50,
-                  marginRight: 20,
-                  borderRadius: '20px',
-                  textTransform: 'capitalize',
-                }}>Terminé</Button>
-                </Grid>
-
-
-              </Grid>
-
-              {/*Contenu à droite*/}
-              <Grid item xs={12} md={7} style={{marginTop: '2%', marginBottom: '5%'}}>
-                <Grid container style={{
-                  backgroundImage: `url(../static/assets/icon/creaShopBg.svg)`,
-                  backgroundPosition: 'right',
-                  backgroundRepeat: 'no-repeat',
-                  border: 'thin solid transparent',
-                  maxWidth: '100%',
-                  height: '90vh',
-                  padding: '2%',
-                  position: 'sticky',
-                  top: 100,
-                }}>
-
-                </Grid> </Grid>
-            </Grid> </Grid>
-        </Layout>
-      </Fragment>
+      <React.Fragment>
+        <Hidden only={['xs', 'sm', 'md']}>
+          <LayoutEvaluate user={user}>
+            {this.content(classes)}
+          </LayoutEvaluate>
+        </Hidden>
+        <Hidden only={['lg', 'xl']}>
+          <LayoutMobile currentIndex={null}>
+            {this.content(classes)}
+          </LayoutMobile>
+        </Hidden>
+      </React.Fragment>
     );
   };
 }
