50b13a483422314ea3ad0456b8f239ed46d75749
Pierre <pierre.crbs@gmail.com>
2020-12-04 16:59:47 +0100

    add next export

--- next.config.js
+++ next.config.js
@@ -4,4 +4,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.BUNDLE_VISUALIZE == 1,
 })
 
-module.exports = withPlugins([[withBundleAnalyzer, {}], [withTM]])
+module.exports = withPlugins([
+  [withBundleAnalyzer, {}],
+  [withTM],
+  { target: 'experimental-serverless-trace' },
+])
