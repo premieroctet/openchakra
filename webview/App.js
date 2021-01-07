1146a1b45b88c0f149ae41ff52a86fce463d1245
Edwin <lienard.edwin@gmail.com>
2021-01-07 17:06:48 +0100

    update xcode

--- webview/App.js
+++ webview/App.js
@@ -1,16 +1,10 @@
 import {WebView} from 'react-native-webview';
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useRef, useEffect } from 'react'
 import {
     SafeAreaView,
     StyleSheet,
-    StatusBar,
-    View,
-    TouchableOpacity,
-    Image
 } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
-import AppStatusBar from './components/AppStatusBar/AppStatusBar';
-const THEME_COLOR = 'transparent';
 
 const App = () => {
   useEffect(()=>{
@@ -21,7 +15,6 @@ const App = () => {
 
   return (
     <>
-      <AppStatusBar backgroundColor={THEME_COLOR} barStyle="dark-content"/>
       <SafeAreaView style={styles.flexContainer}  >
         <WebView
           startInLoadingState={true}
@@ -39,10 +32,6 @@ const styles = StyleSheet.create({
     flexContainer: {
         flex: 1
     },
-    statusBar:{
-        backgroundColor: 'transparent',
-        color: '#EDEDED'
-    }
 });
 
 export default App
