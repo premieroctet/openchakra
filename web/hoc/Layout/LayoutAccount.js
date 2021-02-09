c1b83ceafcfa7697d9e78dac8392708d95951dd9
Armand_D <armand.deshais@mail.com>
2021-02-09 14:44:11 +0100

    feature #919422 CB & rib fusion

--- web/hoc/Layout/LayoutAccount.js
+++ web/hoc/Layout/LayoutAccount.js
@@ -3,22 +3,18 @@ import Grid from "@material-ui/core/Grid";
 import ScrollMenu from '../../components/ScrollMenu/ScrollMenu';
 import Layout from "./Layout";
 
-class LayoutAccount extends React.Component{
+class LayoutAccount extends React.Component {
   constructor(props) {
     super(props);
-    this.state= {
+    this.state = {
       items: [
         {
           label: 'Mes Informations',
           url: '/editProfile'
         },
         {
-          label: 'Mode de paiement',
+          label: 'Modes de paiement',
           url: '/paymentMethod'
-        },
-        {
-          label: 'Mon RIB',
-          url: '/paymentPreference'
         },
         {
           label: 'Mes adresses',
@@ -42,13 +38,19 @@ class LayoutAccount extends React.Component{
   }
 
   render() {
-    const{items}= this.state;
-    const{children, index} = this.props;
+    const {items} = this.state;
+    const {children, index} = this.props;
 
-    return(
+    return (
       <Layout>
-        <Grid style={{display:'flex', justifyContent:'center'}}>
-          <Grid style={{display: 'flex', justifyContent:'center', flexDirection: 'column', alignItems:'center', width: '100%'}}>
+        <Grid style={{display: 'flex', justifyContent: 'center'}}>
+          <Grid style={{
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            alignItems: 'center',
+            width: '100%'
+          }}>
             <Grid style={{display: 'flex', justifyContent: 'center'}}>
               <h2>Mes paramètres</h2>
             </Grid>
@@ -56,7 +58,17 @@ class LayoutAccount extends React.Component{
               <ScrollMenu categories={items} mode={'account'} indexCat={index}/>
             </Grid>
             <Grid style={{backgroundColor: 'rgba(249,249,249, 1)', width: '100%'}}>
-              <Grid style={{margin:'0 15%', display:'flex', justifyContent:'center', backgroundColor: 'white', borderRadius: 27, border: '1px solid rgba(210, 210, 210, 0.5)', padding: '5% 10%', marginTop : '5vh', marginBottom: '5vh'}}>
+              <Grid style={{
+                margin: '0 15%',
+                display: 'flex',
+                justifyContent: 'center',
+                backgroundColor: 'white',
+                borderRadius: 27,
+                border: '1px solid rgba(210, 210, 210, 0.5)',
+                padding: '5% 10%',
+                marginTop: '5vh',
+                marginBottom: '5vh'
+              }}>
                 {children}
               </Grid>
             </Grid>
