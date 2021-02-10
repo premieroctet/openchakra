236270452366b538a29d212ff03f04cbaa7d700d
Lienard <edwin.lienard@outlook.fr>
2021-02-10 16:34:11 +0100

    add condition if surcess and message === object

--- web/utils/notifications.js
+++ web/utils/notifications.js
@@ -3,6 +3,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const snackBarSuccess =  (messages) =>{
+  if(typeof messages === "object"){
+    Object.keys(messages).map(res => {
+      messages = Object.values(messages[res])
+    });
+  }
   let mysnacbar = <SnackBar severity={'success'} message={messages} id={messages}/>;
   let body = document.getElementById('__next');
   let div = document.createElement('div');
@@ -18,6 +23,7 @@ const snackBarError=  (errors) =>{
   }
   let mysnacbar = <SnackBar severity={'error'} message={errors} id={errors}/>;
   let div = document.createElement('div');
+  div.id = 'id_snackbar';
   ReactDOM.render(mysnacbar, document.getElementById('__next').appendChild(div));
 };
 
