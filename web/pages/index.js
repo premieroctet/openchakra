209fc13e1f866a28c637bed35ff8fada2ce139d0
Armand Deshais <armand.deshais@gmail.com>
2021-02-11 15:09:15 +0100

    patch

--- web/pages/index.js
+++ web/pages/index.js
@@ -18,6 +18,7 @@ import TrustAndSecurity from "../hoc/Layout/TrustAndSecurity/TrustAndSecurity";
 import {Divider} from "@material-ui/core";
 import ResaService from "../components/HomePage/ResaService/ResaService";
 import {is_b2b_site} from "../utils/context";
+
 const {getLoggedUserId} = require('../utils/functions');
 
 class Home extends React.Component {
@@ -63,155 +64,80 @@ class Home extends React.Component {
           <meta property="description"
                 content="Des milliers de services référencés ! Consultez les offres de service rémunérés de milliers de particuliers avec My Alfred, première application d’offres de services entre particuliers. Rendre service en étant rémunéré autour de chez soi n’a jamais été aussi simple"/>
         </Helmet>
-        {
-          !is_b2b_site() ?
-            <Grid>
-              <Grid>
-                <InfoBar style={classes}/>
+        <Grid>
+          <Grid>
+            <InfoBar style={classes}/>
+          </Grid>
+          <Grid container
+                className={is_b2b_site() ? classes.navbarAndBannerContainerB2b : classes.navbarAndBannerContainer}>
+            <Grid className={classes.navbarAndBannerBackground}>
+              <Grid className={classes.navbarComponentPosition}>
+                <NavBar/>
               </Grid>
-              <Grid container className={classes.navbarAndBannerContainer}>
-                <Grid className={classes.navbarAndBannerBackground}>
-                  <Grid className={classes.navbarComponentPosition}>
-                    <NavBar/>
-                  </Grid>
-                  <Grid className={classes.bannerPresentationContainer}>
-                    <Grid className={classes.bannerSize}>
-                      <BannerPresentation/>
-                    </Grid>
-                  </Grid>
+              <Grid className={classes.bannerPresentationContainer}>
+                <Grid className={classes.bannerSize}>
+                  <BannerPresentation/>
                 </Grid>
               </Grid>
-              <Grid container className={classes.mainContainerStyle}>
-                <Grid className={classes.generalWidthContainer}>
-                  <CategoryTopic category={category}/>
-                </Grid>
-              </Grid>
-              <Grid container className={classes.howItWorksComponent}>
-                <Grid className={classes.generalWidthContainer}>
-                  <HowItWorks/>
-                </Grid>
-              </Grid>
-              <Grid container className={classes.mainContainerStyle}>
-                <Grid className={classes.generalWidthContainer}>
-                  <OurAlfred alfred={alfred}/>
-                </Grid>
-              </Grid>
-              <Grid container className={classes.becomeAlfredComponent}>
-                <Grid className={classes.generalWidthContainer}>
-                  <ResaService/>
-                </Grid>
-              </Grid>
-              <Hidden only={['xs', 'sm']}>
-                <Grid container className={classes.mainNewsLetterStyle}>
-                  <Grid className={classes.generalWidthContainerNewsLtter}>
-                    <NewsLetter/>
-                  </Grid>
-                </Grid>
-              </Hidden>
-              <Grid>
-                <Divider/>
-              </Grid>
-              <Hidden only={['xs', 'sm', 'md']}>
-                <Grid className={classes.trustAndSecurityContainer}>
-                  <Grid className={classes.trustAndSecurityComponent}>
-                    <TrustAndSecurity/>
-                  </Grid>
-                </Grid>
-              </Hidden>
-              <Grid container className={classes.mainContainerStyleFooter}>
-                <Grid className={classes.generalWidthFooter}>
-                  <Footer/>
-                </Grid>
-              </Grid>
-              <Hidden only={['xl', 'lg', 'md', 'sm']}>
-                <Grid style={{
-                  position: 'fixed',
-                  bottom: '3%',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  width: '100%',
-                  zIndex: 1
-                }}>
-                  <Grid style={{width: '100%'}}>
-                    <MobileNavbar currentIndex={0}/>
-                  </Grid>
-                </Grid>
-              </Hidden>
-            </Grid> :
-            <Grid>
-              <Grid>
-                <InfoBar style={classes}/>
-              </Grid>
-              <Grid container className={classes.navbarAndBannerContainerB2b}>
-                <Grid className={classes.navbarAndBannerBackground}>
-                  <Grid className={classes.navbarComponentPosition}>
-                    <NavBar/>
-                  </Grid>
-                  <Grid className={classes.bannerPresentationContainer}>
-                    <Grid className={classes.bannerSize}>
-                      <BannerPresentation/>
-                    </Grid>
-                  </Grid>
-                </Grid>
-              </Grid>
-              <Grid container className={classes.mainContainerStyle}>
-                <Grid className={classes.generalWidthContainer}>
-                  <CategoryTopic category={category}/>
-                </Grid>
-              </Grid>
-              <Grid container className={classes.howItWorksComponentB2b}>
-                <Grid className={classes.generalWidthContainer}>
-                  <HowItWorks/>
-                </Grid>
-              </Grid>
-              <Grid container className={classes.mainContainerStyle}>
-                <Grid className={classes.generalWidthContainer}>
-                  <OurAlfred alfred={alfred}/>
-                </Grid>
-              </Grid>
-              <Grid container className={classes.becomeAlfredComponent}>
-                <Grid className={classes.generalWidthContainer}>
-                  <ResaService/>
-                </Grid>
-              </Grid>
-              <Hidden only={['xs', 'sm']}>
-                <Grid container className={classes.mainNewsLetterStyle}>
-                  <Grid className={classes.generalWidthContainerNewsLtter}>
-                    <NewsLetter/>
-                  </Grid>
-                </Grid>
-              </Hidden>
-              <Grid>
-                <Divider/>
-              </Grid>
-              <Hidden only={['xs', 'sm', 'md']}>
-                <Grid className={classes.trustAndSecurityContainer}>
-                  <Grid className={classes.trustAndSecurityComponent}>
-                    <TrustAndSecurity/>
-                  </Grid>
-                </Grid>
-              </Hidden>
-              <Grid container className={classes.mainContainerStyleFooter}>
-                <Grid className={classes.generalWidthFooter}>
-                  <Footer/>
-                </Grid>
-              </Grid>
-              <Hidden only={['xl', 'lg', 'md', 'sm']}>
-                <Grid style={{
-                  position: 'fixed',
-                  bottom: '3%',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  width: '100%',
-                  zIndex: 1
-                }}>
-                  <Grid style={{width: '100%'}}>
-                    <MobileNavbar currentIndex={0}/>
-                  </Grid>
-                </Grid>
-              </Hidden>
             </Grid>
-        }
+          </Grid>
+          <Grid container className={classes.mainContainerStyle}>
+            <Grid className={classes.generalWidthContainer}>
+              <CategoryTopic category={category}/>
+            </Grid>
+          </Grid>
+          <Grid container className={is_b2b_site() ? classes.howItWorksComponentB2b : classes.howItWorksComponent}>
+            <Grid className={classes.generalWidthContainer}>
+              <HowItWorks/>
+            </Grid>
+          </Grid>
+          <Grid container className={classes.mainContainerStyle}>
+            <Grid className={classes.generalWidthContainer}>
+              <OurAlfred alfred={alfred}/>
+            </Grid>
+          </Grid>
+          <Grid container className={classes.becomeAlfredComponent}>
+            <Grid className={classes.generalWidthContainer}>
+              <ResaService/>
+            </Grid>
+          </Grid>
+          <Hidden only={['xs', 'sm']}>
+            <Grid container className={classes.mainNewsLetterStyle}>
+              <Grid className={classes.generalWidthContainerNewsLtter}>
+                <NewsLetter/>
+              </Grid>
+            </Grid>
+          </Hidden>
+          <Grid>
+            <Divider/>
+          </Grid>
+          <Hidden only={['xs', 'sm', 'md']}>
+            <Grid className={classes.trustAndSecurityContainer}>
+              <Grid className={classes.trustAndSecurityComponent}>
+                <TrustAndSecurity/>
+              </Grid>
+            </Grid>
+          </Hidden>
+          <Grid container className={classes.mainContainerStyleFooter}>
+            <Grid className={classes.generalWidthFooter}>
+              <Footer/>
+            </Grid>
+          </Grid>
+          <Hidden only={['xl', 'lg', 'md', 'sm']}>
+            <Grid style={{
+              position: 'fixed',
+              bottom: '3%',
+              display: 'flex',
+              justifyContent: 'center',
+              width: '100%',
+              zIndex: 1
+            }}>
+              <Grid style={{width: '100%'}}>
+                <MobileNavbar currentIndex={0}/>
+              </Grid>
+            </Grid>
+          </Hidden>
+        </Grid>
       </React.Fragment>
     );
   }
