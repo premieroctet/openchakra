4c52a8140fc9907ed9289e66a78fd733eee697b7
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2021-02-15 18:59:06 +0100

    919684-api-b2b : avancement

--- web/utils/context.js
+++ web/utils/context.js
@@ -1,3 +1,5 @@
+const {getAuthToken} = require('./authentication')
+
 const is_b2b_site = () => {
 
   if (typeof localStorage == 'undefined') {
@@ -14,10 +16,32 @@ const is_b2b_employee = user => {
 }
 
 const is_b2b_admin = user => {
-  const is_admin = user && user.roles && user.roles.includes['ADMIN']
-  return is_b2b_admin
+  const token=getAuthToken()
+  console.log(`is_b2b_admin:user:${JSON.stringify(user)}`)
+  console.log(`is_b2b_admin:user.roles:${JSON.stringify(user ? user.roles.includes('ADMIN') : [])}`)
+  const is_admin = user && user.roles && user.roles.includes('ADMIN')
+  if (!is_admin) {
+    console.log(`is_b2b_admin:false`)
+    return false
+  }
+  return token && token.role=='ADMIN'
 }
 
+<<<<<<< Updated upstream
+||||||| constructed merge base
+const is_mode_company = user => {
+  const _is_company = user && user.is_company
+  return is_mode_company
+}
+=======
+const is_mode_company = user => {
+  /**
+  const _is_company = user && user.is_company
+  return true //is_mode_company
+  */
+  return is_b2b_admin(user)
+}
+>>>>>>> Stashed changes
 
 module.exports={
   is_b2b_site, is_b2b_employee, is_b2b_admin
