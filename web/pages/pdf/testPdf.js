e3b5d713dd61a3692c349c3f31ecfc8dbaf0f03f
Armand_D <armand.deshais@mail.com>
2021-02-23 17:05:36 +0100

    avancement front gen pdf

---
+++ web/pages/pdf/testPdf.js
@@ -0,0 +1,40 @@
+import LayoutPdf from "../../hoc/Layout/Pdf/LayoutPdf";
+import React from "react";
+import {PDFDownloadLink} from "@react-pdf/renderer";
+import NoSSR from 'react-no-ssr';
+import Grid from "@material-ui/core/Grid";
+
+
+class TestPdf extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false
+    }
+  }
+
+  setLoading = () => {
+    this.setState({loading: true})
+
+  }
+
+  render() {
+    const {loading} = this.state;
+    return (
+      // <LayoutPdf/>
+      <NoSSR>{
+        loading ? "Chargement en cours..." :
+          <Grid onClick={this.setLoading}>
+            <PDFDownloadLink
+              document={<LayoutPdf/>}
+              fileName="faceture.pdf">
+              Télécharger mon récépissé
+            </PDFDownloadLink>
+          </Grid>
+      }
+      </NoSSR>
+    );
+  }
+}
+
+export default TestPdf
