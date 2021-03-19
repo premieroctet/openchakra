e4bb2e6f248233c4b4e268f17f34c8bfd63252ae
Lienard <edwin.lienard@outlook.fr>
2021-03-19 10:44:01 +0100

    remove bug from test page

--- web/pages/test/schedule.js
+++ web/pages/test/schedule.js
@@ -6,7 +6,6 @@ import {Calendar, momentLocalizer, Views} from 'react-big-calendar';
 import moment from 'moment';
 const localizer = momentLocalizer(moment);
 import Button from '@material-ui/core/Button';
-import styles from '../myShop/myAvailabilities/myAvailabilitiesStyle';
 import Schedule from '../../components/Schedule/Schedule';
 
 class calendarTest extends React.Component {
@@ -20,10 +19,10 @@ class calendarTest extends React.Component {
     return (
       <Grid>
         <Grid><h1>Calendrier semaine</h1>
-        <Schedule style={classes} nbSchedule={1} mode={'week'}/>
+        <Schedule  nbSchedule={1} mode={'week'}/>
         </Grid>
         <Grid><h1>Calendrier mois</h1>
-        <Schedule style={classes} nbSchedule={3}/>
+        <Schedule  nbSchedule={3}/>
         </Grid>
       </Grid>
     )
@@ -31,4 +30,4 @@ class calendarTest extends React.Component {
 
 }
 
-export default withStyles(styles)(calendarTest);
+export default calendarTest;
