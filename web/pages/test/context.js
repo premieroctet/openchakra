e3eda63a338fc4fc03f75092ac7f83e807eae0fb
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2021-03-31 17:50:56 +0200

    Petites modifs

--- web/pages/test/context.js
+++ web/pages/test/context.js
@@ -1,23 +1,43 @@
 import React from 'react';
+import axios from 'axios'
+
 const {getLoggedUser}=require('../../utils/functions')
-class MobileTest extends React.Component{
+const {is_b2b_admin}=require('../../utils/context')
+const {setAxiosAuthentication} = require('../../utils/authentication')
+
+class ContextTest extends React.Component{
 
   constructor(props) {
     super(props);
+    this.state = {
+      user : null
+    }
   }
 
+  componentDidMount() {
+    setAxiosAuthentication()
+    axios.get('/myAlfred/api/users/current')
+      .then( response => {
+        this.setState({user : response.data})
+      })
+      .catch (err => console.error(err))
+  }
 
   render() {
-    const{classes} = this.props;
+    const{classes} = this.props
+    const {user} = this.state
 
+    console.log(`User:${user}, is_b2b_admin:${is_b2b_admin(user)}`)
     return(
       <>
         <h1>Valeur token</h1>
         <div>{JSON.stringify(getLoggedUser())}</div>
+        <h1>is_b2b_admin</h1>
+        <div>{is_b2b_admin(user) ? 'Oui' : 'Non'}</div>
       </>
     )
   }
 
 }
 
-export default MobileTest
+export default ContextTest
