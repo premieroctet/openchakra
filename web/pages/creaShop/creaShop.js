a1bdcac5bc0b434ff0b454114c28b2a7d83143b5
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2021-04-19 16:19:29 +0200

    924368 : suppression messages "Diplôme créé" et "Certification créée"

--- web/pages/creaShop/creaShop.js
+++ web/pages/creaShop/creaShop.js
@@ -314,7 +314,7 @@ class creaShop extends React.Component {
 
                 axios.post(`/myAlfred/api/serviceUser/addDiploma/${su_id}`, formData)
                   .then( () => {
-                    snackBarSuccess('Diplôme enregistré')
+                    console.log('Diplôme enregistré')
                   })
                   .catch(err => console.error(err));
               }
@@ -331,7 +331,7 @@ class creaShop extends React.Component {
 
                 axios.post(`/myAlfred/api/serviceUser/addCertification/${su_id}`, formData)
                   .then( () => {
-                    snackBarSuccess('Certification enregistrée')
+                    console.log('Certification enregistrée')
                   })
                   .catch(err => console.error(err));
               }
