99ced2a67389f8be9ef99c004d6b218fa5d17c8f
Armand_D <armand.deshais@mail.com>
2021-04-29 11:58:07 +0200

    factorisation function invoices + page test/invoice

--- web/server/utils/booking.js
+++ web/server/utils/booking.js
@@ -1,12 +1,8 @@
-const moment=require("moment")
+const moment = require("moment")
 
 moment.locale('fr');
 
 const getNextNumber = (value) => {
-  // count
-  // type billing
-  // key 4 chiffre année et mois
-  // 1,2,.4
   return value += 1;
 }
 
@@ -14,6 +10,10 @@ const getKeyDate = () => {
   return moment(date).format("YMM")
 }
 
+
+const getField = (t) => {
+  return t + '_number'
+}
 module.exports = {
-  getNextNumber, getKeyDate
+  getNextNumber, getKeyDate, getField
 }
