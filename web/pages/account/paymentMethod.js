5c8182529012cb956b8cb28f6e7947606e2ce64b
Lienard <edwin.lienard@outlook.fr>
2021-06-22 17:38:45 +0200

    remove old code and update by component

--- web/pages/account/paymentMethod.js
+++ web/pages/account/paymentMethod.js
@@ -1,77 +1,26 @@
-import AccountBalanceIcon from '@material-ui/icons/AccountBalance'
 const {setAxiosAuthentication} = require('../../utils/authentication')
 import React from 'react'
 import axios from 'axios'
 import moment from 'moment'
-import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 import Router from 'next/router'
 import {withStyles} from '@material-ui/core/styles'
-import TextField from '@material-ui/core/TextField'
-import NumberFormat from 'react-number-format'
-import Dialog from '@material-ui/core/Dialog'
-import DialogContent from '@material-ui/core/DialogContent'
-import DialogContentText from '@material-ui/core/DialogContentText'
-import DialogActions from '@material-ui/core/DialogActions'
-import {formatCreditCardNumber, formatCVC, formatExpirationDate} from '../../components/utils'
 import {Helmet} from 'react-helmet'
-import IconButton from '@material-ui/core/IconButton'
 import styles from '../../static/css/pages/paymentMethod/paymentMethod'
-const {isB2BAdmin} = require('../../utils/context')
 import LayoutAccount from '../../hoc/Layout/LayoutAccount'
 import Typography from '@material-ui/core/Typography'
 import Divider from '@material-ui/core/Divider'
-import AddCircleIcon from '@material-ui/icons/AddCircle'
-import MuiDialogTitle from '@material-ui/core/DialogTitle'
-import CloseIcon from '@material-ui/icons/Close'
-import SecurityIcon from '@material-ui/icons/Security'
 import LayoutMobile from '../../hoc/Layout/LayoutMobile'
-import {formatIban} from '../../utils/text'
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever'
-import {toast} from 'react-toastify'
 import HandleCB from '../../components/HandleCB/HandleCB'
+import HandleRIB from '../../components/HandleRIB/HandleRIB'
 
 moment.locale('fr')
-
-const DialogTitle = withStyles(styles)(props => {
-  const {children, classes, onClose, ...other} = props
-  return (
-    <MuiDialogTitle disableTypography {...other} className={classes.root}>
-      <Typography variant="h6">{children}</Typography>
-      {onClose ? (
-        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
-          <CloseIcon/>
-        </IconButton>
-      ) : null}
-    </MuiDialogTitle>
-  )
-})
-
 
 class paymentMethod extends React.Component {
   constructor(props) {
     super(props)
-    this.child = React.createRef()
     this.state = {
       user: {},
-      cards: [],
-      card_number: '',
-      expiration_date: '',
-      issuer: '',
-      focused: '',
-      csv: '',
-      showDeleteCard: false,
-      Idtempo: '',
-      showAddCreditCard: false,
-      showAddRib: false,
-      showDeleteRib: false,
-      accounts: [],
-      haveAccount: false,
-      bic: '',
-      iban: '',
-      error: null,
-      errors: {},
-      is_pro: false,
     }
   }
 
@@ -83,229 +32,13 @@ class paymentMethod extends React.Component {
       .then(res => {
         this.setState({
           user: res.data,
-          userName: res.data.full_name,
-          is_pro: isB2BAdmin(),
         })
       })
       .catch(err => {
-        // if (err.response.status === 401 || err.response.status === 403) {
-        //   clearAuthenticationToken()
-        //   Router.push({pathname: '/'});
-        // }
         console.error(err)
       },
       )
-
-    axios.get('/myAlfred/api/payment/cards')
-      .then(response => {
-        let cards = response.data
-        this.setState({cards: cards})
-      }).catch(err => console.error(err))
-
-    axios.get('/myAlfred/api/payment/activeAccount')
-      .then(response => {
-        let accounts = response.data
-        if (accounts.length) {
-          this.setState({haveAccount: true, accounts: accounts})
-        }
-      })
   }
-
-  handleClick = () => {
-    this.setState({showAddRib: !this.state.showAddRib})
-  };
-
-  handleClick2 = () => {
-    this.setState({showDeleteRib: !this.state.showDeleteRib})
-  };
-
-  handleClose() {
-    this.setState({showDeleteRib: false})
-  }
-
-  refreshCards = () => {
-    this.setState({showDeleteCard: false, showAddCreditCard: false}, () => this.componentDidMount())
-  };
-
-  handleCloseDial = () => {
-    this.setState({showDeleteCard: false})
-  };
-
-  onChange = e => {
-    this.setState({[e.target.name]: e.target.value})
-  };
-
-  handleInputChange = ({target}) => {
-    if (target.name === 'card_number') {
-      target.value = formatCreditCardNumber(target.value)
-    }
-    else if (target.name === 'expiration_date') {
-      target.value = formatExpirationDate(target.value)
-    }
-    else if (target.name === 'csv') {
-      target.value = formatCVC(target.value)
-    }
-
-    this.setState({[target.name]: target.value})
-  };
-
-  deleteAccount(id) {
-    const data = {
-      id_account: id,
-    }
-    axios.put('/myAlfred/api/payment/account', data)
-      .then(() => {
-        toast.error('Compte bancaire supprimé')
-        this.refresh()
-      })
-      .catch(() => {
-        toast.error('Un erreur est survenue')
-      })
-
-  }
-
-  refresh() {
-    this.setState({showDeleteRib: false, haveAccount: false})
-    axios.get('/myAlfred/api/payment/activeAccount')
-      .then(response => {
-        let accounts = response.data
-        if (accounts.length) {
-          this.setState({haveAccount: true, accounts: accounts})
-        }
-      })
-  }
-
-  handleCloseCreditCard = () => {
-    this.setState({showAddCreditCard: false})
-  };
-
-  callAddCreditCard = () => {
-    this.setState({showAddCreditCard: true})
-  };
-
-  callDialogDeletedCard = e => {
-    this.setState({showDeleteCard: true, Idtempo: e})
-  };
-
-  modalAddCreditCard = classes => {
-    return (
-      <Dialog
-        open={this.state.showAddCreditCard}
-        onClose={() => this.handleCloseCreditCard()}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="customized-dialog-title" onClose={this.handleCloseCreditCard}>
-          <Grid style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-            <Grid>
-              <h4>Enregistrer une carte</h4>
-            </Grid>
-            <Grid>
-              <Typography style={{color: 'rgba(39,37,37,35%)'}}>Ajouter une carte en toute sécurité</Typography>
-            </Grid>
-            <Grid>
-              <Typography style={{color: 'red'}}>{this.state.error}</Typography>
-            </Grid>
-          </Grid>
-        </DialogTitle>
-        <DialogContent>
-          <Grid style={{display: 'flex', flexDirection: 'column'}}>
-            <Grid style={{margin: '15px'}}>
-              <NumberFormat
-                customInput={TextField}
-                variant={'outlined'}
-                label="Numéro de carte"
-                name={'card_number'}
-                onChange={this.onChange}
-                value={this.state.card_number}
-                format="#### #### #### ####"
-                placeholder="Votre carte de crédit"
-                style={{width: '100%'}}
-              />
-            </Grid>
-            <Grid style={{margin: '15px'}}>
-              <NumberFormat
-                customInput={TextField}
-                variant={'outlined'}
-                label="Date d'expiration"
-                name={'expiration_date'}
-                onChange={this.onChange}
-                value={this.state.expiration_date}
-                format="##/##"
-                placeholder="MM/YY"
-                style={{width: '100%'}}
-              />
-            </Grid>
-            <Grid style={{margin: '15px'}}>
-              <TextField
-                label="CVV"
-                variant="outlined"
-                value={this.state.csv}
-                onChange={this.onChange}
-                name={'csv'}
-                type="number"
-                pattern="\d{3,4}"
-                style={{width: '100%'}}
-              />
-            </Grid>
-          </Grid>
-          <Grid style={{textAlign: 'center', marginLeft: 15, marginRight: 15, marginTop: '3vh', marginBottom: '3vh'}}>
-            <Button
-              onClick={this.addCard}
-              variant="contained"
-              classes={{root: classes.buttonSave}}
-            >
-              Enregistrer la carte
-            </Button>
-          </Grid>
-          <Grid style={{display: 'flex', alignItems: 'center'}}>
-            <Grid>
-              <Grid>
-                <SecurityIcon style={{color: 'rgba(39,37,37,35%)'}}/>
-              </Grid>
-            </Grid>
-            <Grid>
-              <Grid>
-                <Typography style={{color: 'rgba(39,37,37,35%)'}}>Toutes les données de paiement sur My Alfred sont
-                  cryptées.</Typography>
-              </Grid>
-              <Grid>
-                <Typography style={{color: 'rgba(39,37,37,35%)'}}>Elles sont gérées par mangopay notre partenaire de
-                  confiance.</Typography>
-              </Grid>
-            </Grid>
-          </Grid>
-        </DialogContent>
-      </Dialog>
-    )
-  };
-
-  modalDeleteCreditCard = () => {
-    return (
-      <Dialog
-        open={this.state.showDeleteCard}
-        onClose={this.handleCloseDial}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle
-          id="alert-dialog-title">{'Voulez-vous vraiment supprimer votre carte bancaire ?'}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Si vous supprimez votre carte bancaire vous ne pourrez plus l'utiliser par la suite avec ce compte.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={this.handleCloseDial} color="primary">
-            Annuler
-          </Button>
-          <Button onClick={this.deleteCard} color="secondary" autoFocus>
-            Supprimer
-          </Button>
-        </DialogActions>
-      </Dialog>
-    )
-  };
 
   content = () => {
     return (
@@ -329,81 +62,7 @@ class paymentMethod extends React.Component {
           <Divider style={{height: 2, width: '100%', margin: '5vh 0px'}}/>
         </Grid>
         <Grid>
-          <Grid>
-            <h3>RIB enregistrés</h3>
-          </Grid>
-          <Grid>
-            <Typography
-              style={{color: 'rgba(39,37,37,35%)'}}>{this.state.is_pro ? 'Renseignez un rib pour permettre à vos collaborateurs le paiement par prélèvement bancaire.' : 'Choisissez le versement directement sur votre compte bancaire.'}</Typography>
-          </Grid>
-        </Grid>
-        {this.state.haveAccount ?
-          <Grid container style={{marginTop: '10vh', display: 'flex', alignItems: 'center'}}>
-            <Grid item xl={7} style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-              <Grid item xl={2} style={{display: 'flex'}}>
-                <AccountBalanceIcon/>
-              </Grid>
-              <Grid item xl={6} style={{display: 'flex', flexDirection: 'column'}}>
-                <Grid>
-                  <Grid>
-                    <Typography>{this.state.accounts[0].OwnerName}</Typography>
-                  </Grid>
-                </Grid>
-                <Grid>
-                  <Typography
-                    style={{color: 'rgba(39,37,37,35%)'}}>{formatIban(this.state.accounts[0].IBAN)}</Typography>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item xl={5} style={{display: 'flex', justifyContent: 'center'}}>
-              <IconButton aria-label="delete" onClick={() => this.handleClick2()}>
-                <DeleteForeverIcon/>
-              </IconButton>
-            </Grid>
-          </Grid>
-          :
-          null
-        }
-        <Grid>
-          <Grid>
-            <Divider style={{height: 2, width: '100%', margin: '5vh 0px'}}/>
-          </Grid>
-          <Grid style={{display: 'flex', alignItems: 'center'}}>
-            <Grid>
-              <IconButton aria-label="add" onClick={this.handleClick}>
-                <AddCircleIcon/>
-              </IconButton>
-            </Grid>
-            <Grid>
-              <Typography>Ajouter un rib</Typography>
-            </Grid>
-          </Grid>
-          <Grid>
-            <Divider style={{height: 2, width: '100%', margin: '5vh 0px'}}/>
-          </Grid>
-        </Grid>
-        <Grid style={{
-          marginTop: '10vh',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          marginBottom: '12vh',
-        }}>
-          <Grid style={{marginRight: '2vh'}}>
-            <Grid>
-              <SecurityIcon style={{color: 'rgba(39,37,37,35%)'}}/>
-            </Grid>
-          </Grid>
-          <Grid>
-            <Grid>
-              <Typography style={{color: 'rgba(39,37,37,35%)'}}>Toutes les données de paiement sur My Alfred sont
-                chiffrées.</Typography>
-            </Grid>
-            <Grid>
-              <Typography style={{color: 'rgba(39,37,37,35%)'}}>Elles sont gérées par mangopay notre partenaire de
-                confiance.</Typography>
-            </Grid>
-          </Grid>
+          <HandleRIB/>
         </Grid>
       </Grid>
 
--- web/static/css/pages/paymentMethod/paymentMethod.js
+++ web/static/css/pages/paymentMethod/paymentMethod.js
@@ -1,20 +1,4 @@
 export default theme => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(2),
-  },
-  closeButton: {
-    position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
-  },
-  buttonSave: {
-    textTransform: 'initial',
-    fontWeight: 'bold',
-    color: 'white',
-    backgroundColor: 'black',
-  },
   layoutAccountContainer: {
     [theme.breakpoints.only('xs')]: {
       display: 'none',
