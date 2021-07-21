403cc0036670845df6db1035aa031e5bc2f48564
Lienard <edwin.lienard@outlook.fr>
2021-07-21 11:54:17 +0200

    add new useffect

--- web/components/AlfredConditions/AlfredConditions.js
+++ web/components/AlfredConditions/AlfredConditions.js
@@ -3,15 +3,13 @@ import React, {useEffect, useState} from 'react'
 import {withStyles} from '@material-ui/core/styles'
 import styles from '../../static/css/components/AlfredConditions/AlfredConditions'
 import axios from 'axios'
-import {toast} from 'react-toastify'
 import {SHOP} from '../../utils/i18n'
 import ButtonSwitch from '../ButtonSwitch/ButtonSwitch'
 import moment from 'moment'
 import {ALF_CONDS, CANCEL_MODE} from '../../utils/consts'
 import {snackBarSuccess} from '../../utils/notifications'
 
-const AlfredConditions = ({classes, shop}) => {
-  console.log(shop.booking_request, 'shop')
+function AlfredConditions({classes, shop}) {
   const[idCard, setIdCard] = useState(shop.identity_card)
   const[myAlfredConditions, setMyAlfredConditions] = useState(shop.my_alfred_conditions)
   const[profilePicture, setProfilePicture] = useState(shop.profile_picture)
@@ -22,9 +20,24 @@ const AlfredConditions = ({classes, shop}) => {
   const[bookingRequest, setBookingRequest]= useState(shop.booking_request)
   const[noBookingRequest, setNoBookingRequest]= useState(shop.no_booking_request)
   const[welcomeMessage, setWelcomeMessage]= useState(shop.welcome_message)
+  const[alfredShop, setAlfredShop] = useState(shop)
 
   useEffect(() => {
-    const data = {
+    setAlfredShop(shop)
+    setIdCard(shop.identity_card)
+    setMyAlfredConditions(shop.my_alfred_conditions)
+    setProfilePicture(shop.profile_picture)
+    setAlfredRecommandations(shop.recommandations)
+    setFlexible(shop.flexible_cancel)
+    setModerate(shop.moderate_cancel)
+    setStrict(shop.strict_cancel)
+    setBookingRequest(shop.booking_request)
+    setNoBookingRequest(shop.no_booking_request)
+    setWelcomeMessage(shop.welcome_message)
+  }, [shop])
+
+  useEffect(() => {
+    axios.put('/myAlfred/api/shop/editParameters', {
       my_alfred_conditions: myAlfredConditions,
       profile_picture: profilePicture,
       identity_card: idCard,
@@ -35,15 +48,8 @@ const AlfredConditions = ({classes, shop}) => {
       booking_request: bookingRequest,
       no_booking_request: noBookingRequest,
       welcome_message: welcomeMessage,
-    }
-    axios.put('/myAlfred/api/shop/editParameters', {data})
-      .then((res) => {
-        console.log(res.data)
-      })
-      .catch(err => {
-        console.error(err)
-      })
-  }, [])
+    }).catch(err => { console.error(err) })
+  }, [profilePicture, idCard, isFlexible, isModerate, isStrict, alfredRecommandations, bookingRequest, noBookingRequest, welcomeMessage, myAlfredConditions])
 
   return (
     <Grid container spacing={3} style={{margin: 0, width: '100%'}}>
@@ -105,7 +111,11 @@ const AlfredConditions = ({classes, shop}) => {
             id={CANCEL_MODE.FLEXIBLE}
             checked={isFlexible}
             label={SHOP.bookingCondition.condition_flexible}
-            onChange={() => setFlexible(!isFlexible)}
+            onChange={() => {
+              setFlexible(!isFlexible)
+              setModerate(false)
+              setStrict(false)
+            }}
           />
         </Grid>
         <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
@@ -114,7 +124,11 @@ const AlfredConditions = ({classes, shop}) => {
             id={CANCEL_MODE.MODERATE}
             checked={isModerate}
             label={SHOP.bookingCondition.condition_moderate}
-            onChange={() => setModerate(!isModerate)}
+            onChange={() => {
+              setModerate(!isModerate)
+              setFlexible(false)
+              setStrict(false)
+            }}
           />
         </Grid>
         <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
@@ -123,7 +137,11 @@ const AlfredConditions = ({classes, shop}) => {
             id={CANCEL_MODE.STRICT}
             checked={isStrict}
             label={SHOP.bookingCondition.condition_strict}
-            onChange={() => setStrict(!isStrict)}
+            onChange={() => {
+              setModerate(false)
+              setFlexible(false)
+              setStrict(!isStrict)
+            }}
           />
         </Grid>
       </Grid>
