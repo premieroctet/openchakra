46dd2d08892c3e396738bc8c21188d1645165ab0
Lienard <edwin.lienard@outlook.fr>
2021-07-22 17:34:37 +0200

    avancement

--- web/pages/test/configStyle/importConfigStyle.js
+++ web/pages/test/configStyle/importConfigStyle.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
-import {useTheme} from '@material-ui/core/styles'
-import { useStyles } from './style'
+import {useStyles} from './style'
 
 function importConfigStyle() {
   const classes = useStyles()
   return(
     <Grid>
-      <p className={classes.mySelector }>Bonjour</p>
+      <p className={classes.mySelector}>Bonjour</p>
     </Grid>
   )
 }
