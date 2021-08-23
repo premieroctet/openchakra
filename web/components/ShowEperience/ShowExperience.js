5594b8dd20e00ac9a5a7735a71dee414765ab35e
Lienard <edwin.lienard@outlook.fr>
2021-08-23 11:31:16 +0200

    responsive about.js

--- web/components/ShowEperience/ShowExperience.js
+++ web/components/ShowEperience/ShowExperience.js
@@ -50,10 +50,10 @@ class ShowExperience extends React.Component {
               services.map(res => {
                 return(
                   <Grid container item spacing={2} xl={12} lg={12} md={12} sm={12} xs={12} style={{margin: 0, width: '100%'}}>
-                    <Grid item xl={2} lg={2} className={classes.containerExpPics}>
+                    <Grid item xl={2} lg={2} md={4} sm={12} xs={12} className={classes.containerExpPics}>
                       <Grid style={{backgroundImage: `url("/${res.service.picture}")`}} className={classes.expPics}/>
                     </Grid>
-                    <Grid container item xl={10} lg={10} style={{margin: 0, width: '100%', display: 'flex', alignItems: 'center'}}>
+                    <Grid container item xl={10} lg={10} md={8} sm={12} xs={12} style={{margin: 0, width: '100%', display: 'flex', alignItems: 'center'}}>
                       <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
                         <Typography style={{fontSize: '16px', fontWeight: 'bold'}}>{res.experience_title.charAt(0).toUpperCase() + res.experience_title.slice(1)}</Typography>
                       </Grid>
