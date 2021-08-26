8c43482ac308562ae488caedf48c2f84e135bf3b
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2021-08-26 11:07:22 +0200

    fix eslint

--- web/hoc/Layout/LayoutMessages.js
+++ web/hoc/Layout/LayoutMessages.js
@@ -1,37 +1,36 @@
 const {setAxiosAuthentication}=require('../../utils/authentication')
-import React from "react";
-import withStyles from "@material-ui/core/styles/withStyles";
+import React from 'react'
+import withStyles from '@material-ui/core/styles/withStyles'
 import styles from '../../static/css/components/Layout/LayoutMessages/LayoutMessages'
-import Grid from "@material-ui/core/Grid";
-import axios from "axios";
+import Grid from '@material-ui/core/Grid'
 
-import Layout from "./Layout";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
+import Layout from './Layout'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
 
-class LayoutMessages extends React.Component{
+class LayoutMessages extends React.Component {
 
   constructor(props) {
-    super(props);
+    super(props)
     this.state={
     }
   }
 
   componentDidMount = () => {
     setAxiosAuthentication()
-  };
+  }
 
   handleChange = (event, newValue) => {
     this.setState({tabIndex: newValue}, () => this.props.handleChange())
-  };
+  }
 
   render() {
-    const{children, tabIndex, classes, userInfo}= this.props;
+    const{children, tabIndex, classes, userInfo}= this.props
 
     return(
       <Layout user={userInfo}>
-        <Grid style={{display:'flex', justifyContent:'center'}}>
-          <Grid style={{display: 'flex', justifyContent:'center', flexDirection: 'column', alignItems:'center', width: '100%'}}>
+        <Grid style={{display: 'flex', justifyContent: 'center'}}>
+          <Grid style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', width: '100%'}}>
             <Grid style={{display: 'flex', justifyContent: 'center'}}>
               <h2>Mes Messages</h2>
             </Grid>
@@ -52,16 +51,16 @@ class LayoutMessages extends React.Component{
               </Tabs>
             </Grid>
             <Grid style={{backgroundColor: 'rgba(249,249,249, 1)', width: '100%'}}>
-              <Grid style={{margin:'0 15%', display:'flex', justifyContent:'center', backgroundColor: 'white', borderRadius: 27, border: '1px solid rgba(210, 210, 210, 0.5)', padding: '5% 10%', marginTop : '5vh', marginBottom: '5vh'}}>
+              <Grid style={{margin: '0 15%', display: 'flex', justifyContent: 'center', backgroundColor: 'white', borderRadius: 27, border: '1px solid rgba(210, 210, 210, 0.5)', padding: '5% 10%', marginTop: '5vh', marginBottom: '5vh'}}>
                 {children}
               </Grid>
             </Grid>
           </Grid>
         </Grid>
       </Layout>
-    );
+    )
   }
 
 }
 
-export default withStyles(styles)(LayoutMessages);
+export default withStyles(styles)(LayoutMessages)
