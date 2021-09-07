be9d4e6854a86ab8b66b293b4db116073831d8e0
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2021-09-07 10:53:04 +0200

    931333: suppression getInitialProps

--- web/pages/evaluateClient.js
+++ web/pages/evaluateClient.js
@@ -1,21 +1,24 @@
+import {withStyles} from '@material-ui/core/styles'
 import {withTranslation} from 'react-i18next'
-const {clearAuthenticationToken, setAxiosAuthentication}=require('../utils/authentication')
-import React from 'react';
-import axios from 'axios';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import Router from 'next/router';
-import {withStyles} from '@material-ui/core/styles';
-import StarRatings from 'react-star-ratings';
-import TextField from '@material-ui/core/TextField';
-import styles from '../static/css/pages/evaluateClient/evaluateClient';
-import Typography from "@material-ui/core/Typography";
-import LayoutEvaluate from "../hoc/Layout/LayoutEvaluate";
-import LayoutMobile from "../hoc/Layout/LayoutMobile";
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import React from 'react'
+import Router from 'next/router'
+import StarRatings from 'react-star-ratings'
+import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
+import axios from 'axios'
 
-class EvaluateClient extends React.Component {
+import BasePage from './basePage'
+import LayoutEvaluate from '../hoc/Layout/LayoutEvaluate'
+import LayoutMobile from '../hoc/Layout/LayoutMobile'
+import styles from '../static/css/pages/evaluateClient/evaluateClient'
+
+const {clearAuthenticationToken, setAxiosAuthentication}=require('../utils/authentication')
+
+class EvaluateClient extends BasePage {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       user: {},
       service: {},
@@ -23,75 +26,73 @@ class EvaluateClient extends React.Component {
       accuracy: 0,
       relational: 0,
       content: '',
-    };
+    }
   }
 
   static getInitialProps({query: {booking, id, client}}) {
-    return {booking: booking, service_id: id, client: client};
+    return {booking: booking, service_id: id, client: client}
 
   }
 
   componentDidMount() {
-    const id = this.props.service_id;
-    localStorage.setItem('path', Router.pathname);
+    const id = this.getURLProps().id
+    localStorage.setItem('path', Router.pathname)
     setAxiosAuthentication()
     axios
       .get('/myAlfred/api/users/current')
       .then(res => {
-        let user = res.data;
-        this.setState({user: user});
+        let user = res.data
+        this.setState({user: user})
       })
       .catch(err => {
-          if (err.response.status === 401 || err.response.status === 403) {
-            clearAuthenticationToken()
-            Router.push({pathname: '/'});
-          }
-        },
-      );
+        if (err.response.status === 401 || err.response.status === 403) {
+          clearAuthenticationToken()
+          Router.push({pathname: '/'})
+        }
+      },
+      )
 
-    axios.get('/myAlfred/api/serviceUser/' + id)
+    axios.get(`/myAlfred/api/serviceUser/${ id}`)
       .then(res => {
-        let service = res.data;
-        this.setState({service: service});
+        let service = res.data
+        this.setState({service: service})
       })
-      .catch(err => console.error(err));
+      .catch(err => console.error(err))
   }
 
-  onComplimentChanged = (name) => {
-    const org = this.state[name];
-    this.setState({[name]: !org});
-  };
+  onComplimentChanged = name => {
+    const org = this.state[name]
+    this.setState({[name]: !org})
+  }
 
-  changeRating = (newRating) => {
+  changeRating = newRating => {
     this.setState({
       accueil: newRating,
-    });
-  };
+    })
+  }
 
-  changeRating2 = (newRating) => {
+  changeRating2 = newRating => {
     this.setState({
       accuracy: newRating,
-    });
-  };
+    })
+  }
 
-  changeRating3 = (newRating) => {
+  changeRating3 = newRating => {
     this.setState({
       relational: newRating,
-    });
-  };
+    })
+  }
 
   back = () => {
-    Router.back();
-  };
+    Router.back()
+  }
 
   evaluate =() => {
-    const id = this.props.service_id;
-    const booking = this.props.booking;
-    const client = this.props.client;
-    const content = this.state.content;
-    const accueil = this.state.accueil;
-    const accuracy = this.state.accuracy;
-    const relational = this.state.relational;
+    const {id, booking, client} = this.getURLProps()
+    const content = this.state.content
+    const accueil = this.state.accueil
+    const accuracy = this.state.accuracy
+    const relational = this.state.relational
 
     const obj = {
       booking: booking,
@@ -101,24 +102,24 @@ class EvaluateClient extends React.Component {
       accuracy: accuracy,
       relational: relational,
       content: content,
-    };
+    }
 
     axios.post('/myAlfred/api/reviews/add/client', obj)
       .then(() => {
-        Router.push(`/reservations/reservations`);
+        Router.push('/reservations/reservations')
       })
-      .catch((err) => {
+      .catch(err => {
         console.error(err)
-      });
+      })
 
-  };
+  }
 
-  content = (classes) =>{
+  content = classes => {
     return(
 
       <Grid container className={classes.bigContainer}>
         <Grid style={{width: '100%'}}>
-          <Grid style={{width: '100%',display: 'flex', flexDirection: 'column'}}>
+          <Grid style={{width: '100%', display: 'flex', flexDirection: 'column'}}>
             <Grid item>
               <h3>Qu’avez-vous pensé de votre client ?</h3>
             </Grid>
@@ -127,7 +128,7 @@ class EvaluateClient extends React.Component {
                 <Grid item xl={6} lg={6} md={6} sm={6} xs={6}>
                   <Typography>Accueil</Typography>
                 </Grid>
-                <Grid  item xl={6} lg={6} md={6} sm={6} xs={6}>
+                <Grid item xl={6} lg={6} md={6} sm={6} xs={6}>
                   <StarRatings
                     rating={this.state.accueil}
                     starRatedColor={'rgba(248, 207, 97, 1)'}
@@ -140,11 +141,11 @@ class EvaluateClient extends React.Component {
                   />
                 </Grid>
               </Grid>
-              <Grid  item container xl={12} lg={12} md={12} sm={12} xs={12}>
-                <Grid  item xl={6} lg={6} md={6} sm={6} xs={6}>
+              <Grid item container xl={12} lg={12} md={12} sm={12} xs={12}>
+                <Grid item xl={6} lg={6} md={6} sm={6} xs={6}>
                   <Typography>Précision de la demande</Typography>
                 </Grid>
-                <Grid  item xl={6} lg={6} md={6} sm={6} xs={6}>
+                <Grid item xl={6} lg={6} md={6} sm={6} xs={6}>
                   <StarRatings
                     rating={this.state.accuracy}
                     starRatedColor={'rgba(248, 207, 97, 1)'}
@@ -157,11 +158,11 @@ class EvaluateClient extends React.Component {
                   />
                 </Grid>
               </Grid>
-              <Grid  item container xl={12} lg={12} md={12} sm={12} xs={12}>
-                <Grid  item xl={6} lg={6} md={6} sm={6} xs={6}>
+              <Grid item container xl={12} lg={12} md={12} sm={12} xs={12}>
+                <Grid item xl={6} lg={6} md={6} sm={6} xs={6}>
                   <Typography>Relationnel</Typography>
                 </Grid>
-                <Grid  item xl={6} lg={6} md={6} sm={6} xs={6} >
+                <Grid item xl={6} lg={6} md={6} sm={6} xs={6} >
                   <StarRatings
                     rating={this.state.relational}
                     starRatedColor={'rgba(248, 207, 97, 1)'}
@@ -188,7 +189,7 @@ class EvaluateClient extends React.Component {
               multiline
               rows="6"
               variant="outlined"
-              onChange={(e) => this.setState({content: e.target.value})}
+              onChange={e => this.setState({content: e.target.value})}
             />
           </Grid>
         </Grid>
@@ -216,11 +217,11 @@ class EvaluateClient extends React.Component {
         </Grid>
       </Grid>
     )
-  };
+  }
 
   render() {
-    const {classes} = this.props;
-    const {user} = this.state;
+    const {classes} = this.props
+    const {user} = this.state
     return (
 
       <React.Fragment>
@@ -235,8 +236,8 @@ class EvaluateClient extends React.Component {
           </LayoutMobile>
         </Grid>
       </React.Fragment>
-    );
-  };
+    )
+  }
 }
 
 
