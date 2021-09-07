b12457f118f794691039847a4dcbe4783fde992a
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2021-09-07 12:25:31 +0200

    931333: suppression getInitialProps

--- web/pages/dashboard/category/view.js
+++ web/pages/dashboard/category/view.js
@@ -1,22 +1,22 @@
+import {Typography} from '@material-ui/core'
+import {withStyles} from '@material-ui/core/styles'
 import {withTranslation} from 'react-i18next'
+import Button from '@material-ui/core/Button'
+import Card from '@material-ui/core/Card'
+import FormControl from '@material-ui/core/FormControl'
+import Grid from '@material-ui/core/Grid'
+import React from 'react'
+import Router from 'next/router'
+import Select2 from 'react-select'
+import TextField from '@material-ui/core/TextField'
+import axios from 'axios'
+
+import BasePage from '../../basePage'
+import DocumentEditor from '../../../components/DocumentEditor/DocumentEditor'
+import Layout from '../../../hoc/Layout/Layout'
+
 const {clearAuthenticationToken, setAxiosAuthentication} = require('../../../utils/authentication')
-import React from 'react';
-import Card from '@material-ui/core/Card';
-import Grid from '@material-ui/core/Grid';
-import {Typography} from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
-import {withStyles} from '@material-ui/core/styles';
-import Link from 'next/link';
-import Button from '@material-ui/core/Button';
-import FormControl from '@material-ui/core/FormControl';
-import Layout from '../../../hoc/Layout/Layout';
-import axios from 'axios';
-import Router from 'next/router';
-import Select2 from 'react-select';
-import DocumentEditor from '../../../components/DocumentEditor/DocumentEditor';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-const {snackBarSuccess, snackBarError} = require('../../../utils/notifications');
+const {snackBarSuccess, snackBarError} = require('../../../utils/notifications')
 
 const styles = {
   loginContainer: {
@@ -27,7 +27,6 @@ const styles = {
   },
   card: {
     padding: '1.5rem 3rem',
-    width: 400,
   },
   cardContant: {
     flexDirection: 'column',
@@ -44,23 +43,12 @@ const styles = {
   chip: {
     margin: 2,
   },
-};
+}
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
-
-class view extends React.Component {
+class View extends BasePage {
 
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       category: {particular_label: '', professional_label: ''},
@@ -76,22 +64,17 @@ class view extends React.Component {
       professional_id: null,
       professional_file: null,
       professional_ext: null,
-    };
+    }
 
-    this.handleClick = this.handleClick.bind(this);
-    this.handleChangeTags = this.handleChangeTags.bind(this);
+    this.handleClick = this.handleClick.bind(this)
+    this.handleChangeTags = this.handleChangeTags.bind(this)
     this.onParticularImageChange=this.onParticularImageChange.bind(this)
     this.onProfessionalImageChange=this.onProfessionalImageChange.bind(this)
   }
 
-  static getInitialProps({query: {id}}) {
-    return {category_id: id};
-
-  }
-
   componentDidMount() {
-    localStorage.setItem('path', Router.pathname);
-    const id = this.props.category_id;
+    localStorage.setItem('path', Router.pathname)
+    const id = this.getURLProps().id
     this.setState({
       particular_id: null,
       particular_file: null,
@@ -104,71 +87,71 @@ class view extends React.Component {
     setAxiosAuthentication()
     axios.get(`/myAlfred/api/admin/category/all/${id}`)
       .then(response => {
-        let category = response.data;
-        this.setState({category: category, current_tags: category.tags});
+        let category = response.data
+        this.setState({category: category, current_tags: category.tags})
         this.setState({
           selectedTags: this.state.current_tags.map(b => ({
             label: b.label,
             value: b._id,
           })),
-        });
+        })
 
       })
       .catch(err => {
-        console.error(err);
+        console.error(err)
         if (err.response.status === 401 || err.response.status === 403) {
           clearAuthenticationToken()
-          Router.push({pathname: '/login'});
+          Router.push({pathname: '/login'})
         }
-      });
+      })
 
     axios.get('/myAlfred/api/admin/tags/all')
-      .then((response) => {
-        let tags = response.data;
-        this.setState({all_tags: tags});
-      }).catch((error) => {
-      console.error(error);
-    });
-
+      .then(response => {
+        let tags = response.data
+        this.setState({all_tags: tags})
+      })
+      .catch(error => {
+        console.error(error)
+      })
   }
 
   onChange = e => {
-    const state = this.state.category;
-    state[e.target.name] = e.target.value;
-    this.setState({category: state});
-  };
+    const state = this.state.category
+    state[e.target.name] = e.target.value
+    this.setState({category: state})
+  }
 
   onChangeBool = event => {
     const {name, checked}=event.target
     const state = this.state.category
     state[name]=checked
-    this.setState({category: state});
+    this.setState({category: state})
   }
 
   handleChangeTags = selectedTags => {
-    this.setState({selectedTags});
+    this.setState({selectedTags})
 
-  };
+  }
 
   onParticularImageChange = e => {
     this.setState({
       particular_id: e.target.files[0],
       particular_file: URL.createObjectURL(e.target.files[0]),
-      particular_ext: e.target.files[0].name.split('.').pop()
+      particular_ext: e.target.files[0].name.split('.').pop(),
     })
   }
 
   onProfessionalImageChange = e => {
     this.setState({
-      professional_id : e.target.files[0],
+      professional_id: e.target.files[0],
       professional_file: URL.createObjectURL(e.target.files[0]),
-      professional_ext: e.target.files[0].name.split('.').pop()
+      professional_ext: e.target.files[0].name.split('.').pop(),
     })
   }
 
   onSubmit = e => {
-    e.preventDefault();
-    const {description, particular_label, professional_label} = this.state.category;
+    e.preventDefault()
+    const {description, particular_label, professional_label} = this.state.category
     const {category, particular_id, professional_id} = this.state
 
     if (!category.particular_label) {
@@ -181,15 +164,15 @@ class view extends React.Component {
       return
     }
 
-    let arrayTags = [];
+    let arrayTags = []
     if (this.state.selectedTags != null) {
       this.state.selectedTags.forEach(w => {
-        arrayTags.push(w.value);
-      });
+        arrayTags.push(w.value)
+      })
     }
 
-    const tags = arrayTags;
-    const id = this.props.category_id;
+    const tags = arrayTags
+    const id = this.getURLProps().id
     const data={
       'particular_label': particular_label,
       'professional_label': professional_label,
@@ -200,9 +183,9 @@ class view extends React.Component {
     axios.put(`/myAlfred/api/admin/category/all/${id || ''}`, data)
       .then(res => {
         const newCategory = res.data
-        snackBarSuccess(`Catégorie ${id ? 'modifiée' : 'ajoutée' } avec succès`);
+        snackBarSuccess(`Catégorie ${id ? 'modifiée' : 'ajoutée' } avec succès`)
         if (particular_id || professional_id) {
-          const formData = new FormData();
+          const formData = new FormData()
           if (particular_id) {
             formData.append('particular_picture', particular_id)
           }
@@ -210,8 +193,8 @@ class view extends React.Component {
             formData.append('professional_picture', professional_id)
           }
           axios.put(`/myAlfred/api/admin/category/editPicture/${newCategory._id}`, formData)
-            .then (res => {
-              snackBarSuccess('Illustrations mises à jour');
+            .then(() => {
+              snackBarSuccess('Illustrations mises à jour')
               Router.push(`/dashboard/category/view?id=${newCategory._id}`)
             })
         }
@@ -220,29 +203,30 @@ class view extends React.Component {
         }
       })
       .catch(err => {
-        console.error(err);
-      });
-  };
+        console.error(err)
+      })
+  }
 
   handleClick() {
-    const id = this.props.category_id;
+    const id = this.getURLProps().id
     axios.delete(`/myAlfred/api/admin/category/all/${id}`)
-      .then(res => {
-        alert('Categorie supprimée avec succès');
-        Router.push({pathname: '/dashboard/category/all'});
+      .then(() => {
+        snackBarSuccess('Categorie supprimée avec succès')
+        Router.push({pathname: '/dashboard/category/all'})
       })
       .catch(err => {
-        console.error(err);
-      });
-  };
+        console.error(err)
+        snackBarError(err)
+      })
+  }
 
   render() {
-    const {classes} = this.props;
-    const {category, all_tags, current_tags} = this.state;
+    const {classes} = this.props
+    const {category, all_tags} = this.state
     const optionsTags = all_tags.map(tag => ({
       label: tag.label,
       value: tag._id,
-    }));
+    }))
 
     return (
       <Layout>
@@ -305,29 +289,31 @@ class view extends React.Component {
                     onChange={this.onChange}
                   />
                 </Grid>
-                <Grid item xl={6} lg={6} md={6} sm={6} xs={6}>
-                  <Typography style={{fontSize: 20}}>Illustration particulier</Typography>
-                  <DocumentEditor
-                    key={'particular'}
-                    ext={this.state.ext}
-                    ext_upload={this.state.particular_ext}
-                    db_document={category.particular_picture}
-                    uploaded_file={this.state.particular_file}
-                    onChange={this.onParticularImageChange}
-                    title={'Illu. particulier'}
-                  />
-                </Grid>
-                <Grid item xl={6} lg={6} md={6} sm={6} xs={6}>
-                  <Typography style={{fontSize: 20}}>Illustration professionel</Typography>
-                  <DocumentEditor
-                    key={'professional'}
-                    ext={this.state.ext}
-                    ext_upload={this.state.professional_ext}
-                    db_document={category.professional_picture}
-                    uploaded_file={this.state.professional_file}
-                    onChange={this.onProfessionalImageChange}
-                    title={'Illu. pro'}
-                  />
+                <Grid style= {{display: 'flex', flexDirection: 'vertical'}}>
+                  <Grid item xl={6} lg={6} md={6} sm={6} xs={6}>
+                    <Typography style={{fontSize: 20}}>Illustration particulier</Typography>
+                    <DocumentEditor
+                      key={'particular'}
+                      ext={this.state.ext}
+                      ext_upload={this.state.particular_ext}
+                      db_document={category.particular_picture}
+                      uploaded_file={this.state.particular_file}
+                      onChange={this.onParticularImageChange}
+                      title={'Illu. particulier'}
+                    />
+                  </Grid>
+                  <Grid item xl={6} lg={6} md={6} sm={6} xs={6}>
+                    <Typography style={{fontSize: 20}}>Illustration professionel</Typography>
+                    <DocumentEditor
+                      key={'professional'}
+                      ext={this.state.ext}
+                      ext_upload={this.state.professional_ext}
+                      db_document={category.professional_picture}
+                      uploaded_file={this.state.professional_file}
+                      onChange={this.onProfessionalImageChange}
+                      title={'Illu. pro'}
+                    />
+                  </Grid>
                 </Grid>
                 <Grid item style={{display: 'flex', justifyContent: 'center', marginTop: 30}}>
                   <Button type="submit" variant="contained" color="primary" style={{width: '100%'}}>
@@ -339,9 +325,8 @@ class view extends React.Component {
           </Card>
         </Grid>
       </Layout>
-    );
-  };
+    )
+  }
 }
 
-
-export default withTranslation()(withStyles(styles)(view))
+export default withTranslation()(withStyles(styles)(View))
