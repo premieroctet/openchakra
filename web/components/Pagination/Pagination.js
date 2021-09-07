d20f28423e39568a4014231d09510183447e234f
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2021-09-07 10:52:13 +0200

    Fix withTranslation auto

--- web/components/Pagination/Pagination.js
+++ web/components/Pagination/Pagination.js
@@ -1,35 +1,37 @@
 import {withTranslation} from 'react-i18next'
 import React from 'react'
-import Grid from "@material-ui/core/Grid";
-import { DataGrid } from '@material-ui/data-grid';
+import Grid from '@material-ui/core/Grid'
+import {DataGrid} from '@material-ui/data-grid'
 
 
-export default withTranslation()(class Pagination extends React.Component{)
+class Pagination extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state={
-      page: 1
+      page: 1,
     }
   }
 
   render() {
-    const{style, data} = this.props;
-    const{page} = this.state;
+    const{data} = this.props
+    const{page} = this.state
 
-    let newData = data ? data : '';
+    let newData = data ? data : ''
 
     return(
       <Grid>
         <DataGrid
           page={page}
-          onPageChange={(params) => {
-            this.setState({page: params.page});
+          onPageChange={params => {
+            this.setState({page: params.page})
           }}
           pageSize={5}
           pagination
           {...newData}
         />
       </Grid>
-    );
+    )
   }
 }
+
+export default withTranslation()(Pagination)
