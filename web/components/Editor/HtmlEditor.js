de7015cde9e5f9965cd701001e2bea715162e6de
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2021-09-21 12:23:17 +0200

    931333: HTML editor quill => suneditor

--- web/components/Editor/HtmlEditor.js
+++ web/components/Editor/HtmlEditor.js
@@ -1,14 +1,25 @@
 import {withTranslation} from 'react-i18next'
 import React from 'react'
-import 'react-quill/dist/quill.snow.css'
+import 'suneditor/dist/css/suneditor.min.css'
 import Grid from '@material-ui/core/Grid'
+import SunEditor, {buttonList} from 'suneditor-react'
 
+/**
+TODO : chargement dynamique buttonList
+const SunEditor = dynamic(() => import('suneditor-react'), {
+  ssr: false,
+})
+*/
 
 class HtmlEditor extends React.Component {
   constructor(props) {
     super(props)
-    if (typeof document != 'undefined') {
-      this.quill = require('react-quill')
+    this.options={
+      colorList: [
+        ['#ccc', '#dedede', 'OrangeRed', 'Orange', 'RoyalBlue', 'SaddleBrown'],
+        ['SlateGray', 'BurlyWood', 'DeepPink', 'FireBrick', 'Gold', 'SeaGreen'],
+      ],
+      buttonList: buttonList.complex,
     }
   }
 
@@ -20,56 +31,18 @@ class HtmlEditor extends React.Component {
 
   render() {
 
-    const ReactQuill=this.quill
-    if (!ReactQuill) {
-      return null
-    }
-    let Font = ReactQuill.Quill.import('formats/font')
-    // We do not add Aref Ruqaa since it is the default
-    Font.whitelist = ['Roboto', 'Raleway', 'Montserrat', 'Lato', 'Rubik']
-    ReactQuill.Quill.register(Font, true)
-
     return (
       <Grid>
         <span>{this.props.title}</span>
-        <ReactQuill
-          theme={'snow'}
+        <SunEditor
+          setOptions={this.options}
           onChange={this.onChange}
-          value={this.props.value}
-          modules={HtmlEditor.modules}
-          formats={HtmlEditor.formats}
+          defaultValue={this.props.value}
         />
       </Grid>
     )
   }
 
 }
-
-HtmlEditor.modules = {
-  toolbar: [
-    [{'header': '1'}, {'header': '2'}, {'font': ['Roboto', 'Raleway', 'Montserrat', 'Lato', 'Rubik']}],
-    [{size: []}],
-    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-    [{'list': 'ordered'}, {'list': 'bullet'},
-      {'indent': '-1'}, {'indent': '+1'}],
-    ['link', 'image', 'video'],
-    ['clean'],
-  ],
-  clipboard: {
-    // toggle to add extra line breaks when pasting HTML:
-    matchVisual: false,
-  },
-}
-/*
- * Quill editor formats
- * See https://quilljs.com/docs/formats/
- */
-HtmlEditor.formats = [
-  'header', 'font', 'size',
-  'bold', 'italic', 'underline', 'strike', 'blockquote',
-  'list', 'bullet', 'indent',
-  'link', 'image', 'video',
-]
-
 
 export default withTranslation('custom', {withRef: true})(HtmlEditor)
