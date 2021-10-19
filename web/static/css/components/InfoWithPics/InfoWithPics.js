cf1b4ff2532cbd2484190db7b467de6cd646f58f
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2021-10-19 17:21:22 +0200

    Diminution de marges dans les InfoWithPics ;-)

--- web/static/css/components/InfoWithPics/InfoWithPics.js
+++ web/static/css/components/InfoWithPics/InfoWithPics.js
@@ -1,29 +1,29 @@
 export default theme => ({
-  infoWithPicsMainContainer:{
+  infoWithPicsMainContainer: {
     display: 'flex',
     alignItems: 'center',
-    padding: '5%'
+    padding: '3%',
   },
-  infoWithPicsMarginRight:{
+  infoWithPicsMarginRight: {
     marginRight: '5%',
-    [theme.breakpoints.down('xs')]:{
-      margin: 0
-    }
+    [theme.breakpoints.down('xs')]: {
+      margin: 0,
+    },
   },
-  infoWithPicsColorText:{
-    color:'rgba(39,37,37,35%)'
+  infoWithPicsColorText: {
+    color: 'rgba(39,37,37,35%)',
   },
-  picsSize:{
+  picsSize: {
     width: 80,
     height: 80,
-    [theme.breakpoints.down('xs')]:{
+    [theme.breakpoints.down('xs')]: {
       width: 50,
-      height: 50
-    }
+      height: 50,
+    },
   },
   containerListIcon: {
     [theme.breakpoints.down('xs')]: {
-      marginLeft: 10
+      marginLeft: 10,
     },
-  }
+  },
 })
