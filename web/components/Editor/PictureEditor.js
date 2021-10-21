62f89ce4234bff7153bbfe09cbe3ca68bf7e2676
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2021-10-21 21:10:16 +0200

    931333: ui configuration: fix ajout illustration en modifie une autre

--- web/components/Editor/PictureEditor.js
+++ web/components/Editor/PictureEditor.js
@@ -1,3 +1,4 @@
+import moment from 'moment'
 import {withTranslation} from 'react-i18next'
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
@@ -13,7 +14,6 @@ class PictureEditor extends React.Component {
   }
 
   onChange = ev => {
-    console.log(JSON.stringify(ev.target.value))
     this.setState({uploaded: ev.target.files[0]})
     if (this.props.onChange) {
       this.props.onChange(ev.target.files[0])
@@ -28,7 +28,7 @@ class PictureEditor extends React.Component {
       <Grid style={{display: 'flex'}}>
         <h2>{title}</h2>
         <DocumentEditor
-          title={'Illustration'}
+          title={moment()}
           db_document={value || null}
           uploaded_file={uploaded && URL.createObjectURL(uploaded)}
           onChange={this.onChange}
