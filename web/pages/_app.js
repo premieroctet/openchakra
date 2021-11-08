d6341c0e7ba1123d25d7576c64154b0e28fb2726
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2021-11-08 12:10:07 +0100

    Favicon : add static/custom/favicon.svg

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -77,8 +77,11 @@ class MyApp extends App {
             <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
               integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
               crossOrigin=""/>
-            <link rel="shortcut icon" href="/static/favicon.ico" type="image/x-icon"/>
-            <link rel="icon" href="/static/favicon.ico" type="image/x-icon"/>
+            <link rel="shortcut icon" href="/static/favicon.ico"/>
+            <link rel="icon" href="/static/favicon.ico"/>
+            // Custom favicon
+            <link rel="shortcut icon" href="/static/custom/favicon.svg"/>
+            <link rel="icon" href="/static/custom/favicon.svg"/>
             <link rel="preconnect" href="https://fonts.gstatic.com"/>
             <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
           </Head>
