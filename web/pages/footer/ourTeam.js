ca5bbdb45e65d023117fbf9f95eeb35d1eb94853
Edwin Lienard <lienardedwin@gmail.com>
2021-11-23 15:28:56 +0100

    RENAME variable

--- web/pages/footer/ourTeam.js
+++ web/pages/footer/ourTeam.js
@@ -8,21 +8,21 @@ import CardTeam from '../../components/Card/CardTeam/CardTeam'
 
 function OurTeam(props) {
   const {classes, t, i18n} = props
-  const [array, setArray] = useState([])
+  const [employees, setEmployees] = useState([])
 
   function getEmployees() {
-    let newArray = []
+    let employe = []
     let i = 0
     while(i18n.exists(`EMPLOYEES.${i}.name`) || i18n.exists(`EMPLOYEES.${i}.job`) || i18n.exists(`EMPLOYEES.${i}.description`)) {
-      newArray[i] = {
-        ...newArray[i],
+      employe[i] = {
+        ...employe[i],
         name: t(`EMPLOYEES.${i}.name`),
         job: t(`EMPLOYEES.${i}.job`),
         description: t(`EMPLOYEES.${i}.description`),
       }
       i++
     }
-    setArray(newArray)
+    setEmployees(employe)
   }
 
   useEffect(() => {
@@ -38,9 +38,9 @@ function OurTeam(props) {
           </Grid>
           <Grid container spacing={2} style={{margin: 0, width: '100%'}}>
             {
-              Object.keys(array).map((res, index) => (
+              Object.keys(employees).map((res, index) => (
                 <Grid item lg={3} md={4} sm={6} xs={12} key={index}>
-                  <CardTeam data={array[res]} index={index}/>
+                  <CardTeam data={employees[res]} index={index}/>
                 </Grid>
               ))
             }
