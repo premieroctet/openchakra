14a0decbac6c21d54baa517b29138fca9445ba14
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2021-11-26 11:27:44 +0100

    Mobile: fix titres layout réservations

--- web/hoc/Layout/LayoutMobileReservations.js
+++ web/hoc/Layout/LayoutMobileReservations.js
@@ -47,10 +47,10 @@ class LayoutMobileReservations extends React.Component {
             >
               {
                 userInfo && userInfo.is_alfred ?
-                  <Tab label={ReactHtmlParser(this.props.t('LAYOUT_RESA.messages_alfred'))} className={classes.scrollMenuTab} />
+                  <Tab label={ReactHtmlParser(this.props.t('LAYOUT_RESA.resa_alfred'))} className={classes.scrollMenuTab} />
                   : null
               }
-              <Tab label={ReactHtmlParser(this.props.t('LAYOUT_RESA.messages_user'))} className={classes.scrollMenuTab} />
+              <Tab label={ReactHtmlParser(this.props.t('LAYOUT_RESA.resa_user'))} className={classes.scrollMenuTab} />
             </Tabs>
           </Grid>
         </Grid>
