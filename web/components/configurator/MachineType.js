0418c34f868d06e27b328e2db33c46cc5c71e191
rpasquiou <97103178+rpasquiou@users.noreply.github.com>
2022-02-22 18:20:53 +0100

    UI Configurator wip

--- web/components/configurator/MachineType.js
+++ web/components/configurator/MachineType.js
@@ -1,79 +1,96 @@
-const {Grid, MenuItem, Select, TextField} = require('@material-ui/core')
-const {Autocomplete} = require('@material-ui/lab')
 import React from 'react'
+// import InputLabel from '@mui/material/InputLabel'
+// import MenuItem from '@mui/material/MenuItem'
+// import FormControl from '@mui/material/FormControl'
+// import Select from '@mui/material/Select'
+// import TextField from '@mui/material/TextField'
+// import Autocomplete from '@mui/material/Autocomplete'
+// const {Grid} = require('@material-ui/core')
+
+const {Grid, FormControl, InputLabel, MenuItem, Select, TextField} = require('@material-ui/core')
+const {Autocomplete} = require('@material-ui/lab')
+
 
 function MachineType(props) {
 
   return (
-    <Grid style={{display: 'flex'}}>
-      <Grid item xs={3} style={{display: 'flex', flexDirection: 'column', marginRight: '40px'}}>
-        <h1>Type</h1>
-        <Select name='type' value={props.type} onChange={ev => { props.onTypeChange(ev.target.value) }}>
-          <MenuItem key={''} value={''}>Inconnu</MenuItem>
+    <div className='grid gridcols gap-x-8'>
+      
+      <div>
+        <h2>Quelle machine souhaitez-vous équiper ?</h2>
+    
+        <div className='flex justify-evenly gap-x-4 mb-6'>
           {props.types.map(tp => (
-            <MenuItem key={tp} value={tp}>{tp}</MenuItem>
-          ))
+            <label className='flex flex-col items-center gap-y-1'>
+              <img src='' alt='' width={40} height={40} />
+              <input type="radio" name='type' key={tp} value={tp} onChange={ev => { props.onTypeChange(ev.target.value) }} /> {tp}
+            </label>))
           }
-        </Select>
-      </Grid>
-      <Grid item xs={3} style={{display: 'flex', flexDirection: 'column', marginRight: '40px'}}>
+        </div>
+      </div>
+      
+      <div>
         {!!props.marks.length &&
-          <>
-            <h1>Marque</h1>
-            <Select name='mark' value={props.mark} onChange={ev => { props.onMarkChange(ev.target.value) }}>
-              <MenuItem key={''} value={''}>Inconnu</MenuItem>
-              {props.marks.map(mk => (
-                <MenuItem key={mk} value={mk}>{mk}</MenuItem>
+          
+            <FormControl variant="standard" className='w-full mb-6'>
+              <InputLabel id="machine-brand">Marque de votre machine</InputLabel>
+              <Select labelId="machine-brand" name='mark' value={props.mark} onChange={ev => { props.onMarkChange(ev.target.value) }}>
+                {/* <MenuItem key={''} value={''}>Inconnu</MenuItem> */}
+                {props.marks.map(mk => (
+                  <MenuItem key={mk} value={mk}>{mk}</MenuItem>
+                ))
+                }
+              </Select>
+            </FormControl>
+          
+        }
+      </div>
+      
+      <Grid >
+        {!!props.models.length &&
+            <Autocomplete
+              className='w-full mb-6'
+              options={props.models}
+              value={props.model}
+              filterOptions={(opts, {inputValue}) => { return opts.filter(o => o.toLowerCase().replace(/ /g, '').includes(inputValue.toLowerCase().replace(/ /g, ''))) }}
+              renderInput={params => (<TextField {...params} label='Modèle'/>)}
+              onChange={(ev, value) => props.onModelChange(value)}
+              onInputChange={(ev, value) => props.onModelChange(value)}
+            />
+        }
+      </Grid>
+      <Grid>
+        {!!props.powers.length &&
+        
+          <FormControl variant="standard" className='w-full mb-6' >
+            <InputLabel id="machine-power">Puissance(kW)</InputLabel>
+
+            <Select name='power' labelId='machine-power' disabled={!!props.model} value={props.power} onChange={ev => { props.onPowerChange(ev.target.value) }}>
+              <MenuItem key={null} value={null}>Inconnu</MenuItem>
+              {props.powers.map(pwr => (
+                <MenuItem key={pwr} value={pwr}>{pwr}</MenuItem>
               ))
               }
             </Select>
-          </>
+          </FormControl>
+        
         }
       </Grid>
-      <Grid item xs={3} style={{display: 'flex', flexDirection: 'column', marginRight: '40px'}}>
-        {!!props.models.length &&
-        <>
-          <h1>Modèle</h1>
-          <Autocomplete
-            options={props.models}
-            value={props.model}
-            filterOptions={(opts, {inputValue}) => { return opts.filter(o => o.toLowerCase().replace(/ /g, '').includes(inputValue.toLowerCase().replace(/ /g, ''))) }}
-            renderInput={params => (<TextField {...params}/>)}
-            onChange={(ev, value) => props.onModelChange(value)}
-            onInputChange={(ev, value) => props.onModelChange(value)}
-          />
-        </>
-        }
-      </Grid>
-      <Grid item xs={3} style={{display: 'flex', flexDirection: 'column', marginRight: '40px'}}>
-        {!!props.powers.length &&
-        <>
-          <h1>Puissance(kW)</h1>
-          <Select name='power' disabled={!!props.model} value={props.power} onChange={ev => { props.onPowerChange(ev.target.value) }}>
-            <MenuItem key={null} value={null}>Inconnu</MenuItem>
-            {props.powers.map(pwr => (
-              <MenuItem key={pwr} value={pwr}>{pwr}</MenuItem>
-            ))
-            }
-          </Select>
-        </>
-        }
-      </Grid>
-      <Grid item xs={3} style={{display: 'flex', flexDirection: 'column', marginRight: '40px'}}>
+      <Grid>
         {!!props.weights.length &&
-        <>
-          <h1>Poids(t)</h1>
-          <Select name='weight' disabled={!!props.model} value={props.weight} onChange={ev => props.onWeightChange(ev.target.value)}>
-            <MenuItem key={null} value={null}>Inconnu</MenuItem>
-            {props.weights.map(wght => (
-              <MenuItem key={wght} value={wght}>{wght}</MenuItem>
-            ))
-            }
-          </Select>
-        </>
+          <FormControl variant="standard" className='w-full mb-6' >
+            <InputLabel id="machine-weight">Poids(t)</InputLabel>
+            <Select name='weight' labelId='machine-weight' disabled={!!props.model} value={props.weight} onChange={ev => props.onWeightChange(ev.target.value)}>
+              <MenuItem key={null} value={null}>Inconnu</MenuItem>
+              {props.weights.map(wght => (
+                <MenuItem key={wght} value={wght}>{wght}</MenuItem>
+              ))
+              }
+            </Select>
+          </FormControl>
         }
       </Grid>
-    </Grid>
+    </div>
   )
 }
 
--- web/pages/configurator.js
+++ web/pages/configurator.js
@@ -1,18 +1,20 @@
-const {is_development} = require('../config/config')
-const axios = require('axios')
-const {setAxiosAuthentication} = require('../utils/authentication')
-const {Button, Grid} = require('@material-ui/core')
-const {withTranslation} = require('react-i18next')
-const {STEPS} = require('./configurator/configuratorSteps')
 import React from 'react'
-const ProgressBar=require('../components/ProgressBar/ProgressBar')
-const lodash=require('lodash')
+
+import '../static/feurst.css'
+
+const { is_development } = require('../config/config')
+const axios = require('axios')
+const { setAxiosAuthentication } = require('../utils/authentication')
+const { Button, Grid } = require('@material-ui/core')
+const { withTranslation } = require('react-i18next')
+const { STEPS } = require('./configurator/configuratorSteps')
+const ProgressBar = require('../components/ProgressBar/ProgressBar')
+const lodash = require('lodash')
 
 class Configurator extends React.Component {
-
   constructor(props) {
     super(props)
-    this.state={
+    this.state = {
       step: 0,
       machines: [],
       type: '',
@@ -30,23 +32,37 @@ class Configurator extends React.Component {
       ground: null,
       grounds: [],
       fixType: null,
-      fixTypes: [['PIN', 'À claveter'], ['SOLD', 'À souder']],
+      fixTypes: [
+        ['PIN', 'À claveter'],
+        ['SOLD', 'À souder'],
+      ],
     }
 
     if (is_development()) {
-      this.state={...this.state, step: 3,
-        type: 'excavatrice', mark: 'CATERPILLAR', fixType: 'PIN',
-        model: '374D L', weight: 75.5, power: 355,
-        ground: 'GRAVIER', bladeShape: 'delta', bladeThickness: 70,
-        name: 'Gérard Robert', company: 'COLAS', email: 'sebastien.auvray@my-alfred.io',
-      }
+      // this.state = {
+      //   ...this.state,
+      //   step: 0,
+      //   type: 'excavatrice',
+      //   mark: 'CATERPILLAR',
+      //   fixType: 'PIN',
+      //   model: '374D L',
+      //   weight: 75.5,
+      //   power: 355,
+      //   ground: 'GRAVIER',
+      //   bladeShape: 'delta',
+      //   bladeThickness: 70,
+      //   name: 'Gérard Robert',
+      //   company: 'COLAS',
+      //   email: 'sebastien.auvray@my-alfred.io',
+      // }
     }
   }
 
   componentDidMount = () => {
     setAxiosAuthentication()
-    axios.get('/feurst/api/database')
-      .then(res => {
+    axios
+      .get('/feurst/api/database')
+      .then((res) => {
         this.setState({
           machines: res.data.machines,
           thicknesses: res.data.thicknesses,
@@ -54,7 +70,7 @@ class Configurator extends React.Component {
         })
         this.onMachinesChange(res.data.machines)
       })
-      .catch(err => console.error(err))
+      .catch((err) => console.error(err))
 
     if (is_development()) {
       this.getPrecos()
@@ -63,21 +79,28 @@ class Configurator extends React.Component {
 
   getPrecos = () => {
     setAxiosAuthentication()
-    const data=lodash.pick(this.state, 'type mark model power weight bladeThickness ground fixType'.split(' '))
-    axios.post('/feurst/api/preconisations', data)
-      .then(res => {
+    const data = lodash.pick(
+      this.state,
+      'type mark model power weight bladeThickness ground fixType'.split(' ')
+    )
+    axios
+      .post('/feurst/api/preconisations', data)
+      .then((res) => {
         this.setState({
           precos: res.data,
         })
       })
-      .catch(err => console.error(err))
+      .catch((err) => console.error(err))
   }
 
   getList = (data, attribute) => {
-    return lodash.uniq(data.map(v => v[attribute])).sort((a, b) => (a==null ? -1 : b ==null ? 1 : a-b)).sort()
+    return lodash
+      .uniq(data.map((v) => v[attribute]))
+      .sort((a, b) => (a == null ? -1 : b == null ? 1 : a - b))
+      .sort()
   }
 
-  onMachinesChange = machines => {
+  onMachinesChange = (machines) => {
     this.setState({
       machines: machines,
       types: this.getList(machines, 'type'),
@@ -88,117 +111,178 @@ class Configurator extends React.Component {
     })
   }
 
-  onTypeChange = type => {
-    const {machines}=this.state
-    const isShovel=type=='shovel'
+  onTypeChange = (type) => {
+    const { machines } = this.state
+    const isShovel = type == 'shovel'
     this.setState({
-      type: type, mark: null, model: null, power: null, weight: null,
-      marks: isShovel && [] || this.getList(machines.filter(v => v.type==type), 'mark'),
-      models: isShovel && [] || this.getList(machines.filter(v => v.type==type), 'model'),
-      powers: isShovel && [] || this.getList(machines.filter(v => v.type==type), 'power'),
-      weights: isShovel && [] || this.getList(machines.filter(v => v.type==type), 'weight'),
+      type: type,
+      mark: null,
+      model: null,
+      power: null,
+      weight: null,
+      marks:
+        (isShovel && []) ||
+        this.getList(
+          machines.filter((v) => v.type == type),
+          'mark'
+        ),
+      models:
+        (isShovel && []) ||
+        this.getList(
+          machines.filter((v) => v.type == type),
+          'model'
+        ),
+      powers:
+        (isShovel && []) ||
+        this.getList(
+          machines.filter((v) => v.type == type),
+          'power'
+        ),
+      weights:
+        (isShovel && []) ||
+        this.getList(
+          machines.filter((v) => v.type == type),
+          'weight'
+        ),
     })
   }
 
-  onMarkChange = mark => {
-    const {machines}=this.state
+  onMarkChange = (mark) => {
+    const { machines } = this.state
     this.setState({
-      mark: mark, model: null, power: null, weight: null,
-      models: this.getList(machines.filter(v => !mark || v.mark==mark), 'model'),
+      mark: mark,
+      model: null,
+      power: null,
+      weight: null,
+      models: this.getList(
+        machines.filter((v) => !mark || v.mark == mark),
+        'model'
+      ),
     })
-    let list=this.getList(machines.filter(v => !mark || v.mark==mark), 'type')
-    if (list.length==1) { this.setState({type: list[0]}) }
+    let list = this.getList(
+      machines.filter((v) => !mark || v.mark == mark),
+      'type'
+    )
+    if (list.length == 1) {
+      this.setState({ type: list[0] })
+    }
   }
 
-  onModelChange = model => {
-    const {machines}=this.state
-    this.setState({model: model})
-    let list=this.getList(machines.filter(v => v.model==model), 'mark')
-    if (list.length==1) { this.setState({mark: list[0]}) }
-    list=this.getList(machines.filter(v => v.model==model), 'type')
-    if (list.length==1) { this.setState({type: list[0]}) }
-    list=this.getList(machines.filter(v => v.model==model), 'power')
-    if (list.length==1) { this.setState({power: list[0]}) }
-    list=this.getList(machines.filter(v => v.model==model), 'weight')
-    if (list.length==1) { this.setState({weight: list[0]}) }
+  onModelChange = (model) => {
+    const { machines } = this.state
+    this.setState({ model: model })
+    let list = this.getList(
+      machines.filter((v) => v.model == model),
+      'mark'
+    )
+    if (list.length == 1) {
+      this.setState({ mark: list[0] })
+    }
+    list = this.getList(
+      machines.filter((v) => v.model == model),
+      'type'
+    )
+    if (list.length == 1) {
+      this.setState({ type: list[0] })
+    }
+    list = this.getList(
+      machines.filter((v) => v.model == model),
+      'power'
+    )
+    if (list.length == 1) {
+      this.setState({ power: list[0] })
+    }
+    list = this.getList(
+      machines.filter((v) => v.model == model),
+      'weight'
+    )
+    if (list.length == 1) {
+      this.setState({ weight: list[0] })
+    }
   }
 
-  onPowerChange = power => {
-    this.setState({power: power})
+  onPowerChange = (power) => {
+    this.setState({ power: power })
   }
 
-  onWeightChange = weight => {
-    this.setState({weight: weight})
+  onWeightChange = (weight) => {
+    this.setState({ weight: weight })
   }
 
-  onBladeShapeChange = shape => {
-    this.setState({bladeShape: shape})
+  onBladeShapeChange = (shape) => {
+    this.setState({ bladeShape: shape })
   }
 
-  onBucketWidthChange = width => {
-    this.setState({bucketWidth: width})
+  onBucketWidthChange = (width) => {
+    this.setState({ bucketWidth: width })
   }
 
-  onBladeThicknessChange = thickness => {
-    this.setState({bladeThickness: thickness})
+  onBladeThicknessChange = (thickness) => {
+    this.setState({ bladeThickness: thickness })
   }
 
-  onGroundChange = ground => {
-    this.setState({ground: ground})
+  onGroundChange = (ground) => {
+    this.setState({ ground: ground })
   }
 
-  onFixTypeChange = fixType => {
-    this.setState({fixType: fixType})
+  onFixTypeChange = (fixType) => {
+    this.setState({ fixType: fixType })
   }
 
-  onCompanyChange = company => {
-    this.setState({company: company})
+  onCompanyChange = (company) => {
+    this.setState({ company: company })
   }
 
-  onNameChange = name => {
-    this.setState({name: name})
+  onNameChange = (name) => {
+    this.setState({ name: name })
   }
 
-  onEmailChange = email => {
-    this.setState({email: email})
+  onEmailChange = (email) => {
+    this.setState({ email: email })
   }
 
   nextPage = () => {
-    const {step}=this.state
-    const newStep=Math.min(step+1, STEPS.length-1)
-    if (newStep==3) {
+    const { step } = this.state
+    const newStep = Math.min(step + 1, STEPS.length - 1)
+    if (newStep == 3) {
       this.getPrecos()
     }
-    this.setState({step: newStep})
+    this.setState({ step: newStep })
   }
 
   previousPage = () => {
-    const {step}=this.state
-    const newStep=Math.max(step-1, 0)
-    this.setState({step: newStep})
+    const { step } = this.state
+    const newStep = Math.max(step - 1, 0)
+    this.setState({ step: newStep })
   }
 
   render = () => {
+    const { step, precos } = this.state
 
-    const {step, precos}=this.state
-
-    const {component, validator, menu}=STEPS[step]
+    const { component, validator, menu } = STEPS[step]
 
     return (
-      <Grid style={{width: '80%', position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)'}} className='configurator'>
-        <h6>{/** is_development() && JSON.stringify(lodash.omit(this.state, ['marks', 'machines', 'models', 'powers', 'weights', 'thicknesses', 'grounds']))*/}</h6>
+      <Grid
+        className="configurator relative"
+      >
+        
+        {/** is_development() && JSON.stringify(lodash.omit(this.state, ['marks', 'machines', 'models', 'powers', 'weights', 'thicknesses', 'grounds']))*/}
+        
         <ProgressBar value={step} max={STEPS.length} />
         <h1>{menu}</h1>
-        { component({...this.state, ...this}) }
+        {component({ ...this.state, ...this })}
         {/** JSON.stringify(precos) */}
-        <Grid>
-          <Button disabled={step==0} onClick={this.previousPage}>Précédent</Button>
-          <Button disabled={!validator(this.state)} onClick={this.nextPage}>Suivant</Button>
-        </Grid>
+        <div className='flex sticky justify-between w-full mt-8 bottom-0 bg-white p-4'>
+          <Button className='previous' disabled={step == 0} onClick={this.previousPage}>
+            Précédent
+          </Button>
+          <Button className='next' disabled={!validator(this.state)} onClick={this.nextPage}>
+            Suivant
+          </Button>
+        </div>
       </Grid>
     )
   }
-
 }
 
-module.exports =withTranslation('custom', {withRef: true})(Configurator)
+module.exports = withTranslation('custom', { withRef: true })(Configurator)
