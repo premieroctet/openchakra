365fbdb94b2ceac22e7c7921af3bbf57927d84d5
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2022-03-02 11:12:36 +0100

    940408: Saisie libre sur marque et machine

--- web/components/configurator/MachineType.js
+++ web/components/configurator/MachineType.js
@@ -1,4 +1,4 @@
-const {normalize} = require('../../utils/text')
+
 const {
   CHARGEUSE,
   EXCAVATRICE,
@@ -10,6 +10,7 @@ import {feurstImgPath} from '../../pages/configurator'
 
 const {MenuItem, Select, TextField} = require('@material-ui/core')
 const {Autocomplete} = require('@material-ui/lab')
+const {normalize} = require('../../utils/text')
 
 
 function MachineType(props) {
@@ -39,69 +40,63 @@ function MachineType(props) {
         </div>
       </div>
 
-      {!!props.marks.length &&
-          <div className='machine-brand grid content-between gap-y-4 h-full'>
-            <h2 id="machinebrand">Indiquez la marque de votre machine&nbsp;:</h2>
-            <Select className='w-full' aria-labelledby='machinebrand' name='mark' defaultValue="" value={props.mark || ''} onChange={ev => { props.onMarkChange(ev.target.value) }}>
-              {<MenuItem key={''} value={''}>Inconnu</MenuItem>}
-              {props.marks.map(mk => (
-                <MenuItem key={mk} value={mk}>{mk}</MenuItem>
-              ))
-              }
-            </Select>
-          </div>
-      }
+      <div className='machine-brand grid content-between gap-y-4 h-full'>
+        <h2 id="machinebrand">Indiquez la marque de votre machine&nbsp;:</h2>
+        <Autocomplete
+          className='w-full'
+          options={props.marks}
+          aria-labelledby='machinebrand'
+          value={props.mark}
+          filterOptions={(opts, {inputValue}) => { return opts.filter(o => normalize(o).includes(normalize(props.mark))) }}
+          renderInput={params => (<TextField {...params} />)}
+          onChange={(ev, value) => props.onMarkChange(value)}
+          onInputChange={(ev, value) => props.onMarkChange(value)}
+        />
+      </div>
 
 
-      {!!props.models.length &&
-        <div className='machine-model grid content-between gap-y-4 h-full'>
-          <h2 id="machinemodel">Choisissez le modèle de votre machine&nbsp;:</h2>
-          <Autocomplete
-            className='w-full'
-            options={props.models}
-            aria-labelledby='machinemodel'
-            value={props.model}
-            filterOptions={(opts, {inputValue}) => { return opts.filter(o => o.toLowerCase().replace(/ /g, '').includes(inputValue.toLowerCase().replace(/ /g, ''))) }}
-            renderInput={params => (<TextField {...params} />)}
-            onChange={(ev, value) => props.onModelChange(value)}
-            onInputChange={(ev, value) => props.onModelChange(value)}
-          />
-        </div>
-      }
+      <div className='machine-model grid content-between gap-y-4 h-full'>
+        <h2 id="machinemodel">Choisissez le modèle de votre machine&nbsp;:</h2>
+        <Autocomplete
+          className='w-full'
+          options={props.models}
+          aria-labelledby='machinemodel'
+          value={props.model}
+          filterOptions={(opts, {inputValue}) => { return opts.filter(o => normalize(o).includes(normalize(props.model))) }}
+          renderInput={params => (<TextField {...params} />)}
+          onChange={(ev, value) => props.onModelChange(value)}
+          onInputChange={(ev, value) => props.onModelChange(value)}
+        />
+      </div>
 
-      {!!props.weights.length &&
-        <div className='machine-weight grid content-between gap-y-4 h-full'>
-          <h2 id="machineweight">Indiquez le tonnage de votre machine&nbsp;:</h2>
-          <Select name='weight' aria-labelledby='machineweight' disabled={!!props.model} value={props.weight} onChange={ev => props.onWeightChange(ev.target.value)}>
-            <MenuItem key={null} value={null}>Inconnu</MenuItem>
-            {props.weights.map(wght => (
-              <MenuItem key={wght} value={wght}>{wght}</MenuItem>
-            ))
-            }
-          </Select>
-        </div>
-      }
+      <div className='machine-weight grid content-between gap-y-4 h-full'>
+        <h2 id="machineweight">Indiquez le tonnage de votre machine&nbsp;:</h2>
+        <Select name='weight' aria-labelledby='machineweight' disabled={!!props.model} value={props.weight} onChange={ev => props.onWeightChange(ev.target.value)}>
+          <MenuItem key={null} value={null}>Inconnu</MenuItem>
+          {props.weights.map(wght => (
+            <MenuItem key={wght} value={wght}>{wght}</MenuItem>
+          ))
+          }
+        </Select>
+      </div>
 
-      {!!props.powers.length &&
-        <div className='machine-power grid content-between gap-y-4 h-full'>
-          <h2 id="machinepower">Indiquez la puissance de votre machine&nbsp;:</h2>
-          <Select name='power' aria-labelledby='machinepower' disabled={!!props.model} value={props.power} onChange={ev => { props.onPowerChange(ev.target.value) }}>
-            <MenuItem key={null} value={null}>Inconnu</MenuItem>
-            {props.powers.map(pwr => (
-              <MenuItem key={pwr} value={pwr}>{pwr}</MenuItem>
-            ))
-            }
-          </Select>
-        </div>
-      }
+      <div className='machine-power grid content-between gap-y-4 h-full'>
+        <h2 id="machinepower">Indiquez la puissance de votre machine&nbsp;:</h2>
+        <Select name='power' aria-labelledby='machinepower' disabled={!!props.model} value={props.power} onChange={ev => { props.onPowerChange(ev.target.value) }}>
+          <MenuItem key={null} value={null}>Inconnu</MenuItem>
+          {props.powers.map(pwr => (
+            <MenuItem key={pwr} value={pwr}>{pwr}</MenuItem>
+          ))
+          }
+        </Select>
+      </div>
 
     </div>
   )
 }
 
 const validator= state => {
-  let res=!!state.type && !!state.mark && !!state.power && !!state.weight
-  res = res || state.type==PELLE_BUTTE
+  let res=!!state.type && !!state.mark && !!state.model
   return res
 }
 
