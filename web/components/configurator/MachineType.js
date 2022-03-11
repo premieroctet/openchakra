2bf7dbe5e73018ee07da4a4f9a694d230f81f6b1
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2022-03-11 15:03:59 +0100

    940408: Feurst confugurator: fix saisie tonnage/pouissance bloqués

--- web/components/configurator/MachineType.js
+++ web/components/configurator/MachineType.js
@@ -86,7 +86,7 @@ function MachineType(props) {
             getOptionLabel={option => option.toString() }
             aria-labelledby='machineweight'
             value={props.weight || ''}
-            disabled={props.model?.includes(props.model) && props.weights?.length===1}
+            disabled={props.models?.includes(props.model) && props.weights?.length===1}
             filterOptions={opts => (opts.filter(o => o >= props.weight).sort((a, b) => a - b)) }
             renderInput={params => <TextField {...params} variant="standard" />}
             onChange={(ev, value) => props.onWeightChange(value) }
@@ -104,7 +104,7 @@ function MachineType(props) {
             getOptionLabel={option => option?.toString() }
             aria-labelledby='machinepower'
             value={props.power || ''}
-            disabled={props.model?.includes(props.model) && props.powers?.length===1}
+            disabled={props.models?.includes(props.model) && props.powers?.length===1}
             filterOptions={opts => (opts.filter(o => o >= props.power).sort((a, b) => a - b)) }
             renderInput={params => <TextField {...params} variant="standard" />}
             onChange={(ev, value) => props.onPowerChange(value) }
