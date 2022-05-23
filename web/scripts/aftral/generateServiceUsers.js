d4ce2b5e29a642b56e6917d3ae99f3feee766aed
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2022-05-23 11:03:12 +0200

    Pilote Aftral: Création serviceUsers

--- web/scripts/aftral/generateServiceUsers.js
+++ web/scripts/aftral/generateServiceUsers.js
@@ -1,3 +1,4 @@
+const Billing = require('../../server/models/Billing')
 const ServiceUser = require('../../server/models/ServiceUser')
 const Prestation = require('../../server/models/Prestation')
 const Service = require('../../server/models/Service')
@@ -16,7 +17,13 @@ mongoose.connect(getDatabaseUri(), MONGOOSE_OPTIONS)
   .then(result => {
     user=result
     console.log(result.email)
-    return Service.find({}).populate('prestations')
+    return Billing.findOne({label: /forfait/i})
+      .then(b => {
+        return Prestation.updateMany({}, {billing: [b._id]})
+      })
+      .then(() => {
+        return Service.find({}).populate('prestations')
+      })
   })
   .then(result => {
     const services=result.filter(s => s.prestations?.length>0)
@@ -29,6 +36,7 @@ mongoose.connect(getDatabaseUri(), MONGOOSE_OPTIONS)
         service_address: user.billing_address,
         location: {client: false, alfred: false, visio: true},
         prestations: [{prestation: prestation._id, price: prestation.company_price, billing: prestation.billing[0]}],
+        perimeter: 1000,
       },
       {upsert: true, runValidators: true})
     })
