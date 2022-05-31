e79e400b06bb1e4b69262a9ce7d4531e503b906b
rpasquiou <97103178+rpasquiou@users.noreply.github.com>
2022-05-31 15:02:41 +0200

    942827: EDI login added typing debounce _username

--- web/hoc/withLogin.js
+++ web/hoc/withLogin.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Validator from 'validator'
 import axios from 'axios'
-const {setAuthToken, setAxiosAuthentication}=require('../utils/authentication')
-const {snackBarError}=require('../utils/notifications')
+import debounce from 'lodash/debounce'
+import {setAuthToken, setAxiosAuthentication} from '../utils/authentication'
+import {snackBarError} from '../utils/notifications'
 
 function withLogin(WrappedComponent) {
   
@@ -20,27 +21,33 @@ function withLogin(WrappedComponent) {
         selectedRole: null,
       }
     }
+
+    debouncedCallForUserName = debounce(async(usermail, newState) => {
+
+      if (Validator.isEmail(usermail)) {
+        return await axios.get(`/myAlfred/api/users/roles/${usermail}`)
+          .then(res => {
+            const roles = res.data
+            const selectedRole = roles.length == 1 ? roles[0] : null
+            Object.assign(newState, {roles, selectedRole})
+          })
+          .catch(err => {
+            console.error(err)
+            Object.assign(newState, {selectedRole: null, roles: null})
+            
+          })
+          .finally(() => this.setState(newState))
+      }
+      
+    }, 800)
   
     onChange = async e => {
       const {name, value} = e.target
       const newState = {...this.state, [name]: value}
       if(name === 'username') {
-        const usermail = e.target.value.trim()
+        const usermail = e.target.value
         Object.assign(newState, {username: usermail, roles: null, selectedRole: null})
-        if (Validator.isEmail(usermail)) {
-          await axios.get(`/myAlfred/api/users/roles/${usermail}`)
-            .then(res => {
-              const roles = res.data
-              const selectedRole = roles.length == 1 ? roles[0] : null
-              Object.assign(newState, {roles, selectedRole})
-              this.setState(newState)
-            })
-            .catch(err => {
-              console.error(err)
-              Object.assign(newState, {selectedRole: null, roles: null})
-              this.setState(newState)
-            })
-        }
+        this.debouncedCallForUserName(usermail, newState)
       }
       this.setState(newState)
     }
