1f5fadd4415b31bc88a88ed1f12dbbad118b8e9d
Richard Pasquiou <97103178+rpasquiou@users.noreply.github.com>
2022-06-02 14:54:55 +0200

    942827: EDI design profile page

--- web/pages/edi/profile/profile.js
+++ web/pages/edi/profile/profile.js
@@ -1,7 +1,6 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from 'styled-components'
-import {getLoggedUser} from '../../../utils/context'
-import {BASEPATH_EDI, API_PATH} from '../../../utils/consts'
+import {FEURST_ICON_PATH, BASEPATH_EDI, API_PATH} from '../../../utils/consts'
 import withEdiAuth from '../../../hoc/withEdiAuth'
 import RenewPassword from '../../../components/Password/RenewPassword'
 import {PleasantButton} from '../../../components/Feurst/Button'
@@ -10,7 +9,7 @@ const {ACCOUNT, UPDATE} = require('../../../utils/feurst/consts')
 
 const Profile = () => {
 
-  const loggedUser = getLoggedUser()
+  const [profile, setProfile] = useState()
   const [password, setPassword] = useState('')
   const canSubmitPassword = !!(password?.check1 && password?.check2)
 
@@ -25,71 +24,98 @@ const Profile = () => {
       .catch(err => console.error(err))
   }
 
+  const getUserInfo = async() => {
+    return await client(`${API_PATH}/users/current`)
+      .then(res => {
+        setProfile(res)
+      })
+      .catch(err => console.error(err))
+  }
+
+  useEffect(() => {
+    getUserInfo()
+  }, [])
+
 
   return (<StyledProfile>
-    <SideBar>
-      <ul>
-        <li>Mon compte</li>
-      </ul>
-    </SideBar>
 
-    <Content>
-      <h2>Nom</h2>
-      {loggedUser?.name} {loggedUser?.firstname}
+    {/* <h2><img width={30} height={30} src={`${FEURST_ICON_PATH }/user.icon.svg`} alt="" /><span className='underlined'>Mon profil</span></h2> */}
+    <h3>Nom</h3>
+    <p>{profile?.full_name}</p>
+   
+    <h3>Société</h3>
+    <p>{profile?.company.full_name}</p>
 
-      <h2>Modification du mot de passe</h2>
+    <h3>Email</h3>
+    <p>{profile?.email}</p>
+    
 
-      <form onSubmit={renewPassword}>
-        <RenewPassword setPassword={setPassword} />
-        <PleasantButton
-          disabled={!canSubmitPassword}
-          type='submit'
-          onClick={() => renewPassword}
-        >
+    <h3>Modification du mot de passe</h3>
+
+    <form onSubmit={renewPassword}>
+      <RenewPassword setPassword={setPassword} />
+      <PleasantButton
+        rounded={'full'}
+        disabled={!canSubmitPassword}
+        type='submit'
+        onClick={() => renewPassword}
+      >
             Enregistrer le mot de passe
-        </PleasantButton>
-      </form>
+      </PleasantButton>
+    </form>
 
-    </Content>
+    
   </StyledProfile>)
 }
 
 const StyledProfile = styled.div`
+
   border-top: 1px solid var(--black);
-  background-color: var(--stone-100);
-  display: grid;
-  column-gap: var(--spc-4);
-  grid-template-columns: 10% 1fr;
   height: min(calc(100% - 2rem), 70vh);
+
+  h2, h3 {
+    color: var(--black);
+  }
+
+  h2 {
+    display: inline-flex;
+    column-gap: var(--spc-2);
+    font-size: var(--text-2xl);
+  }
+
+  p {
+    font-size: var(--text-lg);
+  }
+
+  .underlined {
+    border-bottom: 10px solid var(--brand-color);
+  }
+  
+  h3 {
+    font-size: var(--text-2xl);
+    color: var(--black);
+    margin-bottom: 0;
+  }
+
+  form {
+    display: flex; 
+    flex-direction: column;
+    width: min(calc(100% - 2rem), 40vw);
+
+    & > div {
+      margin-bottom: var(--spc-4);
+    }
+  }
+
+  button[type="submit"] {
+    align-self: flex-end;
+  }
+
+  em {
+    margin-bottom: var(--spc-4);
+  }
+
   `
 
-const SideBar = styled.aside`
-  background-color: var(--brand-color);
-  color: var(--white);
-  border-top-right-radius: var(--rounded-2xl);
-  border-bottom-right-radius: var(--rounded-2xl);
-  box-shadow: 1px 0 2px 1px rgba(0, 0, 0, 0.2);
-  width: calc(max-content - 1rem);
-
-  ul {
-    list-style-type: none;
-    margin-left: var(--spc-3);
-    padding: 0;
-  }
-
-  li {
-    width: max-content;
-    box-shadow: 0 0 3px 1px rgba(0, 0, 0, 0.2);
-    padding: var(--spc-4);
-    background-color: var(--white);
-    color: var(--black);
-    border-radius: var(--rounded-7xl);
-  }
-
-`
-
-const Content = styled.div`
-  margin-left: var(--spc-10);
-`
 
 export default withEdiAuth(Profile, {model: ACCOUNT, action: UPDATE, pathAfterFailure: `${BASEPATH_EDI}/login`})
