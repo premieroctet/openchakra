4fb9e1afdc9f74b2bc7d32c1478688edc55bdddc
rpasquiou <97103178+rpasquiou@users.noreply.github.com>
2022-06-21 16:14:12 +0200

    942827: EDI added countdown

--- web/components/Feurst/Header.js
+++ web/components/Feurst/Header.js
@@ -5,6 +5,37 @@ import {screen} from '../../../web/styles/screenWidths'
 import {FEURST_PHONE_NUMBER, BASEPATH_EDI} from '../../utils/feurst/consts'
 import QuickMenu from './QuickMenu'
 
+import Countdown from './Countdown'
+import {StyledCountdown} from './components.styles'
+
+
+const Header = ({accessRights}) => {
+
+  // Set time limit for countdown
+  const hourLimit = new Date()
+  hourLimit.setHours(11)
+  hourLimit.setMinutes(0)
+  hourLimit.setSeconds(0)
+
+  return (
+    <>
+      <InfoBox>
+        <p>Toute commande de moins de 30 articles passée avant {hourLimit.toLocaleTimeString([], {hour: 'numeric', minute: 'numeric'})} sera traitée dans la journée. <StyledCountdown> <Countdown limit={hourLimit} /> </StyledCountdown>Notre secrétariat est ouvert du lundi au vendredi de 9:00 à 17:00.</p>
+      </InfoBox>
+      <HeaderContainer role="banner">
+
+        <a className='phonenumber' href={`tel:${FEURST_PHONE_NUMBER.replace(/\s+/g, '')}`}>Une question ? {FEURST_PHONE_NUMBER}</a>
+
+        <Link href={`${BASEPATH_EDI}`}>
+          <LogoLink aria-label="Accueil" href={`${BASEPATH_EDI}`}>
+            <img className='img-responsive max-w-200' src="https://feurst.fr/wp-content/uploads/2022/01/logo-feurst-01.svg" alt='' width={350} height={104} />
+          </LogoLink>
+        </Link>
+        <QuickMenu accessRights={accessRights} />
+      </HeaderContainer>
+    </>
+  )
+}
 
 const HeaderContainer = styled.header`
 
@@ -52,27 +83,5 @@ const InfoBox = styled.div`
   padding-block: var(--spc-2);
   padding-inline: var(--spc-1);
 `
-
-const Header = ({accessRights}) => {
-
-  return (
-    <>
-      <InfoBox>
-        <p>Toute commande passée avant 11h00 sera traitée dans la journée. Notre secrétariat est ouvert du lundi au vendredi de 9h00 à 17h00.</p>
-      </InfoBox>
-      <HeaderContainer role="banner">
-
-        <a className='phonenumber' href={`tel:${FEURST_PHONE_NUMBER.replace(/\s+/g, '')}`}>Une question ? {FEURST_PHONE_NUMBER}</a>
-
-        <Link href={`${BASEPATH_EDI}`}>
-          <LogoLink aria-label="Accueil" href={`${BASEPATH_EDI}`}>
-            <img className='img-responsive max-w-200' src="https://feurst.fr/wp-content/uploads/2022/01/logo-feurst-01.svg" alt='' width={350} height={104} />
-          </LogoLink>
-        </Link>
-        <QuickMenu accessRights={accessRights} />
-      </HeaderContainer>
-    </>
-  )
-}
 
 export default Header
