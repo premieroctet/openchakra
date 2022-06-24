ef5f8a2c0d787a5f7d9fbde63db156c1caefcd5e
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2022-06-24 16:55:35 +0200

    942827: EDI: improve Excel generation

--- web/server/utils/feurst/generateExcel.js
+++ web/server/utils/feurst/generateExcel.js
@@ -8,19 +8,24 @@ const generateExcel = model => {
   data.push(['Référence', model.reference || 'N/A'])
   data.push(['Date commande', model.creation_date || 'N/A'])
   data.push(['Client', model.company?.name])
+  const address=model.address
+  data.push(['Addresse', address ? `${address.address}, ${address.zip_code} ${address.city} (${address.country})`: 'Non renseignée'])
   data.push(['Délégué', model.sales_representative?.full_name])
   data.push([])
   data.push('Réf. article,Désignation,Quantité,Poids,Prix catalogue,Remise,Votre prix,Total'.split(','))
+  data.push([])
   model.items.forEach(item => {
     const product=item.product
     data.push([product.reference, `${product.description} ${product.description_2}`, item.quantity, item.total_weight,
       item.catalog_price, formatPercent(item.discount), item.net_price, item.total_amount])
   })
-  data.push(`Total,,,${model.total_weight},,,,${model.total_amount}`.split(','))
+  const ship_fee=model.shipping_fee ? model.shipping_fee : model.address? 'Franco' : null
+  if (ship_fee) {
+    data.push(['Livraison', model.shipping_mode==STANDARD_SHIPPING ? 'standard': 'express', '', '', '', '', '', ship_fee])
+  }
   data.push([])
-  data.push([`Livraison ${model.shipping_mode==STANDARD_SHIPPING ? 'standard': 'express'}, environ ${model.shipping_fee}€`])
-  const address=model.address
-  data.push(['Addresse', address ? `${address.address}, ${address.zip_code} ${address.city} (${address.country})`: 'N/A'])
+  data.push(['Total', '', '', model.total_weight, '', '', '', model.total_amount])
+  data.push([])
 
   // Insert empty first column
   data = data.map(d => ['', ...d])
