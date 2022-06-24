4d13fe47375dd4a0271aca3d5c8b2b8b66ca1f63
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2022-06-24 11:44:03 +0200

    942827: EDI: Remove import from administration tabs

--- web/components/Feurst/AccountsList.js
+++ web/components/Feurst/AccountsList.js
@@ -1,9 +1,7 @@
 import React, {useState} from 'react'
 import styled from 'styled-components'
 import dynamic from 'next/dynamic'
-import {ACCOUNT, CREATE, API_PATH, LINK} from '../../utils/feurst/consts'
-import ImportExcelFile from './ImportExcelFile'
-import AccountLink from './AccountLink'
+import {ACCOUNT, CREATE} from '../../utils/feurst/consts'
 import FeurstRegister from './Register'
 import {accountsColumns} from './tablestructures'
 import BaseListTable from './BaseListTable'
@@ -19,16 +17,8 @@ const AccountsList = ({accessRights}) => {
 
   const canAddAccount = accessRights.isActionAllowed(ACCOUNT, CREATE)
 
-
-  // TODO: Import action for FEURST_AD%MIN only
-  // const IMPORTS=[{title: 'Import clients/compagnies/tarification', url: `${API_PATH}/users/import`}]
-  const IMPORTS=[]
   return (
     <>
-      <div>
-        {IMPORTS.map((imp, i) => (<ImportExcelFile key={`imp${i}`} caption={imp.title} importURL={imp.url} templateURL={null} onImport={toggleRefresh}/>))}
-      </div>
-
       {canAddAccount &&
       <div className='container-md mb-8'>
         <NormalButton onClick={() => setIsOpenDialog(true)} rounded={'full'} size={'full-width'}><span>⊕</span> Ajouter un compte</NormalButton>
