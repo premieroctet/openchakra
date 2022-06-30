14bba3d46c5165c7c48c96e05df672866e057eaf
Richard Pasquiou <services@pasquiou.fr>
2022-06-30 23:07:52 +0200

    942827: EDI update cgv OK

--- web/pages/edi/cgv-update.js
+++ web/pages/edi/cgv-update.js
@@ -4,10 +4,12 @@ import styled from 'styled-components'
 import withEdiAuth from '../../hoc/withEdiAuth'
 import {API_PATH, BASEPATH_EDI, ACCOUNT, UPDATE_CGV} from '../../utils/consts'
 import {setAxiosAuthentication} from '../../utils/authentication'
+import {NormalButton} from '../../components/Feurst/Button'
 
 const CGVupdate = () => {
 
   const [filename, setFilename] = useState('')
+  const [cgvUploaded, setCgvUploaded] = useState(false)
   const importURL = `${API_PATH}/users/update-cgv`
 
   const submitFile = async e => {
@@ -23,7 +25,8 @@ const CGVupdate = () => {
     setAxiosAuthentication()
     return await axios.post(importURL, data)
       .then(res => {
-        console.log(res)
+        setCgvUploaded(true)
+        setFilename('')
         return res?.data
       })
       .catch(err => console.error(err))
@@ -31,23 +34,26 @@ const CGVupdate = () => {
 
   return (<div className='container-md mb-8'>
     <h1>Mise à jour des CGV</h1>
+
     <UpdateFile onSubmit={submitFile}>
-      <ol>
-        <li>
+      
+      {cgvUploaded && <p className='success'>Les nouvelles conditions générales ont été mises en place.</p>}
+      
           <label htmlFor='updatecgv'>
-            <span className='button'>Ajouter</span> les nouvelles CGV <span className='italic'>(format .pdf)</span>
+
+           <span role={'img'} alt=''>⬇️</span>
+            <span>Importer vos CGV actuelles</span> 
+            <span className='italic'>(format .pdf)</span>
             <input className='sr-only' id='updatecgv' type={'file'} accept={'.pdf'} onChange={e => setFilename(e.target.files[0])} />
 
-            <span className='data'>{filename?.name || 'non renseigné'}</span>
-
           </label>
-        </li>
+          
+          {filename && <p className='data'>{filename?.name || ''}</p>}
+      
+          <NormalButton disabled={!filename} size={'full-width'} rounded={'full'} type='submit' onSubmit={() => submitFile}>Mettre à jour les CGV</NormalButton>
 
-        <li>
-          <button type='submit' onSubmit={() => submitFile}>Mettre à jour les CGV</button>
-        </li>
-
-      </ol>
+          <p className='text-center'>La mise à jour des CGV impliquera une nouvelle acceptation des CGV pour l'ensemble des comptes.</p>
+  
     </UpdateFile>
   </div>
   )
@@ -55,31 +61,48 @@ const CGVupdate = () => {
 
 const UpdateFile = styled.form`
 
-  /* background-color: var(--yellow-500); */
   padding: var(--spc-4);
   border-radius: var(--rounded-md);
+  font-size: var(--text-lg);
 
-  li {
-    font-size: var(--text-lg);
-    line-height: 3;
-    margin-bottom: var(--spc-4);
+  .success {
+    padding: var(--spc-4);
+    background-color: green;
+    color: white;
   }
 
-
-  button, .button {
-    min-width: 10ch;
-    padding: var(--spc-4);
-    background-color: var(--white);
-    border: 1px solid var(--brand-color);
-    margin-inline: var(--spc-2);
-    margin-block: var(--spc-4);
-    border-radius: var(--rounded-2xl);
+  label {
+    display: flex;
+    flex-direction: column;
+    row-gap: var(--spc-2);
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    margin-inline: auto;
+    padding: var(--spc-8);
+    aspect-ratio: 2 / 1;
+    border: 1px solid var(--stone-400);
+    cursor: pointer;
+    margin-bottom: var(--spc-8);
+    
+    span:first-of-type {
+      font-size: var(--text-4xl);
+      background-color: var(--stone-100);
+      padding: var(--spc-8);
+      border-radius: var(--rounded-full);
+      width: var(--spc-32);
+      aspect-ratio: 1 / 1;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
   }
 
   .data {
-    margin-inline-start: var(--spc-2);
-    border-bottom: 1px solid var(--brand-color);
-    background-color: var(--white);
+    padding-block: var(--spc-4);
+    margin-inline: auto;
+    margin-bottom: var(--spc-4);
+    text-align: center;
   }
 `
 
