f439b57b8a20405376b6714a58774317b9fb976e
Richard Pasquiou <richard.pasquiou@alfredplace.io>
2022-07-26 17:29:57 +0200

    pilote-aftral: hide service address on confirmPayment if visio/elearning

--- web/components/Payment/PaymentChoice/PaymentChoice.js
+++ web/components/Payment/PaymentChoice/PaymentChoice.js
@@ -3,12 +3,12 @@ import {withTranslation} from 'react-i18next'
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
+import withStyles from '@material-ui/core/styles/withStyles'
 import DrawerBookingRecap from '../../Drawer/DrawerBookingRecap/DrawerBookingRecap'
 import Topic from '../../../hoc/Topic/Topic'
 import AddressService from '../../AddressService/AddressService'
 import PaymentMode from '../PaymentMode/PaymentMode'
 import styles from '../../../static/css/components/PaymentChoice/PaymentChoice'
-import withStyles from '@material-ui/core/styles/withStyles'
 import {PAYMENT_CHOICE, PAYMENT_MODE} from '../../../utils/i18n'
 
 class PaymentChoice extends React.Component {
@@ -50,7 +50,7 @@ class PaymentChoice extends React.Component {
                 <a href={'#'}>{ReactHtmlParser(this.props.t('PAYMENT_MODE.link_paid_another_card'))}</a>
               </Grid>
             </Grid>
-            <Grid style={{backgroundColor: 'white', borderRadius: 27, border: '1px solid rgba(210, 210, 210, 0.5)', paddingLeft: '10%', paddingTop: '5%', paddingBottom: '5%', marginTop: '2vh'}}>
+            {(!bookingObj?.location?.elearning || !bookingObj?.location?.visio) ? null : <Grid style={{backgroundColor: 'white', borderRadius: 27, border: '1px solid rgba(210, 210, 210, 0.5)', paddingLeft: '10%', paddingTop: '5%', paddingBottom: '5%', marginTop: '2vh'}}>
               <Topic
                 titleTopic={ReactHtmlParser(this.props.t('PAYMENT_MODE.topic_postal_service'))}
                 underline={false}
@@ -64,6 +64,7 @@ class PaymentChoice extends React.Component {
                 }
               </Topic>
             </Grid>
+            }
           </Grid>
         </Grid>
         <Grid item xl={5} lg={5} md={5} xs={12} sm={12} className={classes.paymentChoiceSecondContainer}>
