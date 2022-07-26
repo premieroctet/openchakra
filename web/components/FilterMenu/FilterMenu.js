18edaf20235b7502f883a42cc5ec8a7f84208d82
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2022-07-26 19:08:17 +0200

    b2b/validation: pages/search to functional

--- web/components/FilterMenu/FilterMenu.js
+++ web/components/FilterMenu/FilterMenu.js
@@ -1,5 +1,3 @@
-import {canAlfredParticularRegister} from '../../config/config'
-import CustomButton from '../CustomButton/CustomButton'
 import ReactHtmlParser from 'react-html-parser'
 import {withTranslation} from 'react-i18next'
 import React from 'react'
@@ -9,14 +7,16 @@ import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Switch from '@material-ui/core/Switch'
 import moment from 'moment'
 import {DateRangePicker} from 'react-dates'
-import styles from '../../static/css/components/FilterMenu/FilterMenu'
 import withStyles from '@material-ui/core/styles/withStyles'
 import Slider from '@material-ui/core/Slider'
 import MultipleSelect from 'react-select'
-const {setAxiosAuthentication}=require('../../utils/authentication')
-const {PRO, PART}=require('../../utils/consts')
 import axios from 'axios'
 import lodash from 'lodash'
+import styles from '../../static/css/components/FilterMenu/FilterMenu'
+import CustomButton from '../CustomButton/CustomButton'
+import {canAlfredParticularRegister} from '../../config/config'
+const {PRO, PART}=require('../../utils/consts')
+const {setAxiosAuthentication}=require('../../utils/authentication')
 
 
 class FilterMenu extends React.Component {
@@ -263,6 +263,9 @@ class FilterMenu extends React.Component {
     }
     else if (results.length === 0) {
       resultMessage = ReactHtmlParser(this.props.t('SEARCHBAR.no_results'))
+    }
+    else {
+      resultMessage=null
     }
 
 
