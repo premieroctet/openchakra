fbac8d3cf5bc73d9a4df88403a3e8166051b772e
Richard Pasquiou <richard.pasquiou@alfredplace.io>
2022-08-05 17:53:15 +0200

    pilote-aftral: Card Preview In Progress

--- web/components/Card/CardPreview/CardPreview.js
+++ web/components/Card/CardPreview/CardPreview.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Link from 'next/link'
 import {withTranslation} from 'react-i18next'
 import Grid from '@material-ui/core/Grid'
-import Box from '@material-ui/core/Box'
 import Rating from '@material-ui/lab/Rating'
 import styled from 'styled-components'
 import Typography from '@material-ui/core/Typography'
@@ -32,20 +31,21 @@ const CardPreview = ({item}) => {
     <Link href={bookingUrl(item._id)} >
       <StyledCardPreview theme={getDataModel()}>
 
-        <div className='customcardpreviewavatar'>
+        <div className='card_avatar customcardpreviewavatar'>
           <UserAvatar user={item.user} />
         </div>
 
-        <Grid className={`customcardpreviewbox`}>
+        <div className={`card_content customcardpreviewbox`}>
+          
+          {image && <div className='card_content-image'><img src={image} alt="" /></div>}
 
-          {image && <img src={image} alt="" />}
+          <div className='card_content-text'>
+          
+            {!isMonoProvider() && <Typography className={`customcardpreviewname`}>{item.user.firstname}</Typography>}
+            <Typography className={`customcardpreviewlabel`}>{item.service.label}</Typography>
+            {city && <Typography className={'customcardpreviewplace'} >{city}</Typography>}
 
-          {!isMonoProvider() && <Typography className={`customcardpreviewname`}>{item.user.firstname}</Typography>}
-          <Typography className={`customcardpreviewlabel`}>{item.service.label}</Typography>
-          {city && <Typography className={'customcardpreviewplace'} >{city}</Typography>}
-
-          <Grid className={'customcardpreviewrating'}>
-            <Box component="fieldset" mb={item.user.score} borderColor="transparent" >
+            <Grid className={'customcardpreviewrating'}>
               <Rating
                 name="simple-controlled"
                 value={item.user.score}
@@ -53,9 +53,9 @@ const CardPreview = ({item}) => {
                 readOnly
               />
               <Typography>({item.user.score})</Typography>
-            </Box>
-          </Grid>
-        </Grid>
+            </Grid>
+          </div>
+        </div>
       </StyledCardPreview>
     </Link>
   )
@@ -63,37 +63,95 @@ const CardPreview = ({item}) => {
 
 const StyledCardPreview = styled.a`
 
-  position: relative;
-  display: flex;
-  flex-direction: column;
+  --card-padding: var(--spc-4);
 
-  row-gap: var(--spc-4);
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 80px 1fr;
+  grid-template-areas:  'card_avatar'
+                        'card_content';
   height: 100%;
-  /* justify-content: center; */
-  align-items: center;
+  min-width: 250px;
+  aspect-ratio: 4 / 5;
   cursor: pointer;
+  transition: transform var(--delayIn) ease-out;
 
-  img {
-    width: 100%;
-    height: auto;
-    border-radius: 5px;
+  &:hover {
+
+    .card_content-image::after {
+      background-color: transparent;
+    }
   }
 
-  & > div {
+  .card_avatar {
+    justify-self: center;
+    grid-area: card_avatar;
+    z-index: 2;
+  }
+  
+  .card_avatar + * {
     margin-top: 40px;
-    min-height: 300px;
   }
 
-  .customcardpreviewavatar {
-    top: -40px;
+  
+  .card_content {
+    display: grid;
+    grid-template-rows: 3fr 2fr;
+    overflow: hidden;
+    border-radius: 1rem;
+    grid-area: card_content;
+    box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.3);
+    grid-row: 1 / -1;
+    grid-column: 1 / -1;
+    margin-bottom: var(--spc-4);
+
+    &> :is(p, div, ):not(.card_content-image) {
+      padding: var(--card-padding);
+    }
+
+    p {
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 3;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+  }
+
+  .card_content > * {
+    transition: var(--delayIn) ease-in;
+  }
+
+  .card_content-image img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+  }
+
+  .card_content-image {
+    position: relative;
+  }
+
+  .card_content-image::after {
+    transition: background-color var(--delayIn) ease-in-out;
+    content: '';
+    width: 100%;
+    height: 100%;
+    display: block;
+    inset: 0;
+    background-color: var(--secondary-color);
+    filter: opacity(0.4) ;
     position: absolute;
   }
 
-  .customcardpreviewavatar + div {
-    padding: 50px 1rem 1rem 1rem;
-    border-radius: 1rem;
-    border: 1px solid #111;
+  .card_content-text {
+    align-self: flex-start;
+    color: var(--black);
+    z-index: 1;
+    height: 100%;
   }
+
 
   ${props => {
   if (props.theme === 'aftral') {
@@ -101,10 +159,6 @@ const StyledCardPreview = styled.a`
 
       p {
         font-weight: bold;
-      }
-
-      img {
-        margin-bottom: var(--spc-4);
       }
 
       `
