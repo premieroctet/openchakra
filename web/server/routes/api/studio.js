16d5efdccacbe4faaf95d859a8504061dafae92f
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2022-09-05 17:46:45 +0200

    studio: API get models

---
+++ web/server/routes/api/studio.js
@@ -0,0 +1,21 @@
+const mongoose=require('mongoose')
+const express = require('express')
+const lodash=require('lodash')
+
+const router = express.Router()
+
+router.get('/models', (req, res) => {
+  const modelNames=lodash.sortBy(mongoose.modelNames())
+  const result=[]
+  modelNames.forEach(name => {
+    result.push({
+      name: name,
+      attributes: Object.keys(mongoose.model(name).schema.paths)
+        .filter(attName => !attName.startsWith('_')),
+    })
+  })
+  // console.log(modelNames.map(m => mongoose.model(m).schema.paths))
+  res.json(result)
+})
+
+module.exports=router
