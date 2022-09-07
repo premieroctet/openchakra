0a1e95a502634b003db78a73a3e1d8a54a962e01
shinework <adrien.baptiste@gmail.com>
2022-09-07 13:56:57 +0200

    Update test stack

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
-const withPlugins = require('next-compose-plugins')
-const withTM = require('next-transpile-modules')(['browser-nativefs'])
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.BUNDLE_VISUALIZE == 1,
-})
-
-module.exports = async phase => {
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-  }
-
-  const defaultConfig = {}
-  return withPlugins([[withBundleAnalyzer], [withTM]], nextConfig)(phase, {
-    defaultConfig,
-  })
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
 }
+
+module.exports = nextConfig
