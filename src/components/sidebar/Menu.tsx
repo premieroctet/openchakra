7f0c7e102b549a60bbb5e5714aee8a3a62080637
Richard Pasquiou <97103178+rpasquiou@users.noreply.github.com>
2022-09-08 16:17:00 +0200

    WIP UI

--- src/components/sidebar/Menu.tsx
+++ src/components/sidebar/Menu.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import Sidebar from './Sidebar'
+import Pages from './Pages'
 
-const menuSections = {
+const menuSections: {
+  [section: string]: {
+    title: string
+    icon: string
+    component?: React.ReactNode
+  }
+} = {
   pages: {
     title: 'Pages',
     icon: '/icons/pages.svg',
+    component: <Pages />,
   },
   components: {
     title: 'Components',
     icon: '/icons/components.svg',
+    component: <Sidebar />,
   },
   medias: {
     title: 'Medias',
@@ -25,25 +35,35 @@ const menuSections = {
 }
 
 const Menu = () => {
+  const [activeSection, setActiveSection] = useState(
+    menuSections.pages.component,
+  )
+
   return (
     <StyledMenu>
       <img
         className="logo"
         src="/images/wappizyLogo.svg"
-        width={224}
+        width={150}
         alt="Logo"
       />
       <ul className="menu">
         {Object.keys(menuSections).map(category => {
           return (
             <li>
-              <img src={menuSections[category]['icon']} alt="" width={70} />
-              <span>{menuSections[category]['title']}</span>
+              <button
+                onClick={() =>
+                  setActiveSection(menuSections[category]['component'])
+                }
+              >
+                <img src={menuSections[category]['icon']} alt="" width={70} />
+                <span>{menuSections[category]['title']}</span>
+              </button>
             </li>
           )
         })}
       </ul>
-      <div className="sidebar">Les beaux composants ici</div>
+      <div className="sidebar">{activeSection}</div>
     </StyledMenu>
   )
 }
@@ -54,9 +74,10 @@ const StyledMenu = styled.div`
     'logo logo'
     'menu sidebar';
   grid-template-rows: auto 1fr;
-  background-color: black;
+  background-color: rgb(20, 19, 37);
   width: max-content;
-  padding: 1rem;
+  padding-block: 1rem;
+  padding-inline: 1rem 0;
 
   .menu {
     grid-area: menu;
@@ -66,7 +87,7 @@ const StyledMenu = styled.div`
     gap: 1rem;
   }
 
-  li {
+  button {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -75,6 +96,8 @@ const StyledMenu = styled.div`
 
   .logo {
     grid-area: logo;
+    justify-self: end;
+    padding: 0.5rem;
   }
 
   .sidebar {
