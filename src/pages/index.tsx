19a9f967ec70a604bc64435328ad17897f067de6
Pierre <pierre.crbs@gmail.com>
2022-09-15 16:37:07 +0200

    fix responsive icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Flex, Box, IconButton } from '@chakra-ui/react'
+import React from 'react'
+import { Flex, Box } from '@chakra-ui/react'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import { Global } from '@emotion/react'
@@ -11,12 +11,13 @@ import EditorErrorBoundary from '~components/errorBoundaries/EditorErrorBoundary
 import Editor from '~components/editor/Editor'
 import { InspectorProvider } from '~contexts/inspector-context'
 import Inspector from '~components/inspector/Inspector'
-import { HiOutlineDesktopComputer } from 'react-icons/hi'
-import { MdOutlineTabletMac } from 'react-icons/md'
-import { ImMobile } from 'react-icons/im'
+import ResponsiveToolBar from '~components/ResponsiveToolBar'
+import { useSelector } from 'react-redux'
+import { getEditorWidth } from '~core/selectors/app'
 
 const App = () => {
-  const [editorWidth, setEditorWidth] = useState<string>('100%')
+  const editorWidth = useSelector(getEditorWidth)
+
   useShortcuts()
 
   return (
@@ -33,45 +34,7 @@ const App = () => {
           <Sidebar />
           <EditorErrorBoundary>
             <Box bg="#edf2f6" flex={1} position="relative">
-              <Flex
-                w="100%"
-                p={4}
-                backgroundSize="20px 20px"
-                bgColor="#edf2f6"
-                align="center"
-                justify="center"
-                position="absolute"
-                zIndex={10}
-                boxShadow="lg"
-              >
-                <IconButton
-                  icon={<HiOutlineDesktopComputer />}
-                  size="lg"
-                  fontSize="30px"
-                  color={editorWidth === '100%' ? 'teal.500' : 'black.500'}
-                  aria-label="Desktop version"
-                  onClick={() => setEditorWidth('100%')}
-                />
-
-                <IconButton
-                  icon={<MdOutlineTabletMac />}
-                  size="lg"
-                  color={editorWidth === '48em' ? 'teal.500' : 'black.500'}
-                  fontSize="30px"
-                  aria-label="Tablet version"
-                  mx={12}
-                  onClick={() => setEditorWidth('48em')}
-                />
-
-                <IconButton
-                  icon={<ImMobile />}
-                  size="lg"
-                  color={editorWidth === '30em' ? 'teal.500' : 'black.500'}
-                  fontSize="30px"
-                  aria-label="Mobile version"
-                  onClick={() => setEditorWidth('30em')}
-                />
-              </Flex>
+              <ResponsiveToolBar />
               <Flex
                 w={editorWidth}
                 transition="all ease 0.5s"
