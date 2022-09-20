6e603405791737b8464a25f5f234f26b36190cc5
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2022-09-20 10:54:13 +0200

    Dynamic data: validate referenced DataProvider exists Add Badge to dynamic text types

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,10 +1,19 @@
-const checkEmptyDataProvider = comp => {
-  if (comp.type == 'DataProvider' && !comp.props?.model) {
-    return Promise.reject(`DataProvider ${comp.id} is unlinked`)
+const checkEmptyDataProvider = (comp, icomponents) => {
+  if (comp.type == 'DataProvider') {
+    if (!comp.props?.model) {
+      return Promise.reject(`DataProvider ${comp.id} is unlinked`)
+    }
+  }
+}
+
+const checkAvailabelDataProvider = (comp, icomponents) => {
+  if (!!comp.props.dataSource && !(comp.props.dataSource in Object.keys(icomponents))) {
+    return Promise.reject(`DataProvider ${comp.props.dataSource} not found`)
   }
 }
 
 export const validate = (icomponents: IComponents) => {
   const components = Object.values(icomponents)
-  return Promise.all(components.map(checkEmptyDataProvider))
+  return Promise.all(components.map(c => checkEmptyDataProvider(c, icomponents)))
+    .then(() => Promise.all(components.map(c => checkAvailabelDataProvider(c, icomponents))))
 }
