dde3a85eda472c0078a1fd0ae038912d03d882d9
Rishabh Raizada <rishabh.raizada@trilogy.com>
2022-09-22 12:49:56 +0400

    Updated oc to uiez && added ts-loader

--- next.config.js
+++ next.config.js
@@ -23,6 +23,20 @@ const nextConfig = {
     //   test: /\.(ts|js)x?$/,
     //   loader: "raw-loader",
     // });
+    config.module.rules.push({
+      test: /\.(ts)x?$/, // Just `tsx?` file only
+      use: [
+        // options.defaultLoaders.babel, I don't think it's necessary to have this loader too
+        {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+            experimentalWatchApi: true,
+            onlyCompileBundledFiles: true,
+          },
+        },
+      ],
+    });
 
     return config;
   },
