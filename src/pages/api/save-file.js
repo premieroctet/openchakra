e3a33132095c0d0da11badea0c942aba9117a772
ayodeji-ti <ayodeji.odesile@trilogy.com>
2022-09-26 17:53:26 +0000

    changed file destination & updated package version

--- src/pages/api/save-file.js
+++ src/pages/api/save-file.js
@@ -2,8 +2,9 @@ import fs from 'fs'
 
 export default function handler(req, res) {
     console.log(req.body);
+    console.log(process.cwd())
     try {
-        fs.writeFileSync('../../components/comp.oc.tsx', req.body.codeBody, (err) => {
+        fs.writeFileSync('../../../../../../remote/tu2k22-chakra/chakra-samples/dummy-login-form/dummy-login-form.oc.tsx', req.body.codeBody, (err) => {
           if (err) throw err;
           else{
              console.log("The file is updated with the given data")
@@ -13,4 +14,4 @@ export default function handler(req, res) {
         console.log(err);
       }
   }
-  // ../../../../../../remote/tu2k22-chakra/chakra-samples/dummy-login-form/dummy-login-form.oc.tsx
+  
