ab8f0b9d40fbb3d8aa174c13d5daa1d62ea22711
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2022-09-30 16:41:33 +0200

    studio: validate icon's icons property

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,13 +1,27 @@
-const checkEmptyDataProvider = (comp, icomponents) => {
+import lodash from 'lodash'
+
+const checkEmptyDataProvider = (comp: IComponent, icomponents: IComponents) => {
   if (comp.type == 'DataProvider') {
-    if (!comp.props ?.model) {
+    if (!comp.props?.model) {
       return Promise.reject(`DataProvider ${comp.id} is unlinked`)
     }
   }
 }
 
-const checkAvailableDataProvider = (comp, icomponents) => {
-  if (!comp.props ?.dataSource) {
+const checkAvailableDataProvider = (
+  comp: IComponent,
+  icomponents: IComponents,
+) => {
+  const ICON_PROPS = ['leftIcon', 'rightIcon', 'icon']
+  return ICON_PROPS.map(i => {
+    if (comp?.props?.[i] == 'Icon') {
+      throw new Error(`Icon ${comp.id} is not defined`)
+    }
+  })
+}
+
+const checkEmptyIcons = (comp: IComponent, icomponents: IComponents) => {
+  if (!comp.props?.dataSource) {
     return
   }
   if (!Object.keys(icomponents).includes(comp.props.dataSource)) {
@@ -17,6 +31,11 @@ const checkAvailableDataProvider = (comp, icomponents) => {
 
 export const validate = (icomponents: IComponents) => {
   const components = Object.values(icomponents)
-  return Promise.all(components.map(c => checkEmptyDataProvider(c, icomponents)))
-    .then(() => Promise.all(components.map(c => checkAvailableDataProvider(c, icomponents))))
+  return Promise.all(
+    components.map(c => checkEmptyDataProvider(c, icomponents)),
+  ).then(() =>
+    Promise.all(
+      components.map(c => checkAvailableDataProvider(c, icomponents)),
+    ),
+  )
 }
