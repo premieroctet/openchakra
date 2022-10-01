7f3f79fe43c15d391437edcf216e3f83cc8e819f
ayodeji-ti <99248255+ayodeji-ti@users.noreply.github.com>
2022-10-01 16:40:55 +0100

    @feature/next.js (#1)
    * Added static build in out directory
    
    * using pnpm instead of npm
    
    * Fixed minor errors for publishing package
    
    * first commit
    
    * updated directory
    
    * changed file destination & updated package version
    
    * addd automatic updates to json file also
    
    Co-authored-by: Rishabh Raizada <rishabh.raizada@trilogy.com>

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 #! /usr/bin/env node
-var shell = require("shelljs");
+var shell = require('shelljs')
 
-shell.exec("echo Starting OpenChakra server..")
-shell.exec("pnpm run dev")
+shell.exec('echo Starting OpenChakra server..')
+shell.exec('cd node_modules/@trilogy-group/tu2k22-openchakra && pnpm start')
---
+++ src/pages/api/save-file.js
@@ -0,0 +1,23 @@
+import fs from 'fs'
+
+export default function handler(req, res) {
+    console.log(req.body);
+    console.log(process.cwd())
+    try {
+        fs.writeFileSync('../../../../../../remote/tu2k22-chakra/chakra-samples/dummy-login-form/dummy-login-form.oc.tsx', req.body.codeBody, (err) => {
+          if (err) throw err;
+          else{
+             console.log("The file is updated with the given data")
+          }
+       });
+        fs.writeFileSync('../../../../../../remote/tu2k22-chakra/chakra-samples/dummy-login-form/dummy-login-form.oc.json', JSON.stringify(req.body.jsonBody), (err) => {
+          if (err) throw err;
+          else{
+            console.log("The file is updated with the given data")
+          }
+      });
+      } catch (err) {
+        console.log(err);
+      }
+  }
+  // 
