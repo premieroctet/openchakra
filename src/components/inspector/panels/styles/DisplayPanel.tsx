c453885946cfa01a44914d1f9f5f33588be3a2ce
Richard Pasquiou <richard.pasquiou@alfredplace.io>
2022-10-07 17:25:32 +0200

    WIP responsive

--- src/components/inspector/panels/styles/DisplayPanel.tsx
+++ src/components/inspector/panels/styles/DisplayPanel.tsx
@@ -15,27 +15,24 @@ function responsiveOptions(data: object) {
 const DisplayPanel = () => {
   const { setValueFromEvent, setValue } = useForm()
   const display = usePropsSelector('display')
+
+
   const displayString = typeof display === 'string'
-  const theme = useTheme()
   const initialBreakpoint: string = 'base'
-  const [customResponsiveProps, setCustomResponsiveProps] = useState(
-    displayString ? { [initialBreakpoint]: display } : display,
-  )
+  const customResponsiveProps = displayString ? { [initialBreakpoint]: null } : display
+  
+  const theme = useTheme()
   const themeBreakpoints: { string: string } = theme.breakpoints
-  const [settledBreakpoints, setSettledBreakpoints] = useState(
-    !displayString ? Object.keys(display) : [initialBreakpoint],
-  )
+  const [settledBreakpoints, setSettledBreakpoints] = useState(Object.keys(customResponsiveProps))
+  
   const availableBreakpoints = Object.keys(themeBreakpoints)
     .filter(bkpt => !Object.keys(customResponsiveProps).includes(bkpt))
     .filter(bkpt => !settledBreakpoints.includes(bkpt))
+  
+  const valueToDisplay = responsiveOptions(customResponsiveProps)
 
-  // console.log('breakpointToAdd', breakpointToAdd, availableBreakpoints)
-
-  const valueToDisplay =
-    typeof display === 'string'
-      ? responsiveOptions({ base: display })
-      : responsiveOptions(display)
-
+  // console.log(customResponsiveProps, settledBreakpoints, availableBreakpoints, valueToDisplay)
+    
   const AddABreakpoint = () => {
     return (
       <form onSubmit={addBreakpoint}>
@@ -58,48 +55,58 @@ const DisplayPanel = () => {
   const handleBreakpoints = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const [breakpoint] = e?.target?.name.split('-')
     const { value } = e.target
+    
+    let newCustomRespProps = {}
+    
+    if (value) {
+      newCustomRespProps = { ...customResponsiveProps, [breakpoint]: value }
+    } else {
+      let prepareRespProps = customResponsiveProps
+      delete prepareRespProps[breakpoint]
+      newCustomRespProps = prepareRespProps
+    }
 
-    setCustomResponsiveProps({ ...customResponsiveProps, [breakpoint]: value })
-    setValue('display', customResponsiveProps)
+    setValue('display', newCustomRespProps)
   }
 
   const addBreakpoint = (e: {
     target: { form: { [x: string]: { value: string } } }
   }) => {
+    // setValue('display', {...customResponsiveProps, [e.target.form['addBreakpoint'].value]: null})
     setSettledBreakpoints(
       uniq([...settledBreakpoints, e.target.form['addBreakpoint'].value]),
     )
   }
 
   /* De base pas de breakpoint */
-  if (displayString && !settledBreakpoints.length) {
-    return (
-      <>
-        <FormControl label="Display">
-          <Select
-            size="sm"
-            value={valueToDisplay || ''}
-            onChange={setValueFromEvent}
-            name="display"
-          >
-            <option></option>
-            <option>block</option>
-            <option>flex</option>
-            <option>inline</option>
-            <option>grid</option>
-            <option>inline-block</option>
-          </Select>
-        </FormControl>
+  // if (displayString && !settledBreakpoints.length) {
+  //   return (
+  //     <>
+  //       <FormControl label="Display">
+  //         <Select
+  //           size="sm"
+  //           value={valueToDisplay || ''}
+  //           onChange={setValueFromEvent}
+  //           name="display"
+  //         >
+  //           <option></option>
+  //           <option>block</option>
+  //           <option>flex</option>
+  //           <option>inline</option>
+  //           <option>grid</option>
+  //           <option>inline-block</option>
+  //         </Select>
+  //       </FormControl>
+  //       </>
+  //   )
+  // }
 
-        <AddABreakpoint />
-      </>
-    )
-  }
 
   return (
     <>
       {settledBreakpoints.map((breakpoint: string, i: number) => {
-        // console.log('alors', customResponsiveProps?.[breakpoint], breakpoint)
+
+        console.log(customResponsiveProps?.[breakpoint])
 
         return (
           <div key={i}>
