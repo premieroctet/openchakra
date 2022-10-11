679791b69e42f4db91a845c025881d90a2cef2eb
Richard Pasquiou <richard.pasquiou@alfredplace.io>
2022-10-11 12:37:24 +0200

    fixed dynamic Date

--- src/custom-components/Date/Date.tsx
+++ src/custom-components/Date/Date.tsx
@@ -1,8 +1,16 @@
-import { Box } from '@chakra-ui/react'
 import React from 'react'
 
-const DateComp = ({date, dataFormat, ...props}:{date: string, dataFormat: string}) => (
-    <Box {...props}>{date}</Box>
-)
+const DateComp = ({"data-value": dataValue, "data-format": dataFormat, ...props}:{'data-value': string, 'data-format': string}) => {
+
+    const date = new Date(dataValue)
+    const dateOptionsToConsider = dataFormat || {}
+
+    // TODO fr-FR locale dynamic
+    const dateTimeFormat = new Intl.DateTimeFormat('fr-FR', dateOptionsToConsider);
+    const dateToDisplay = dateTimeFormat.format(date)
+
+    return (
+    <span {...props}>{dateToDisplay}</span>
+)}
 
 export default DateComp
