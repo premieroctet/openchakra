88ba8d5f28b0c520d58f040c7710806095735e4e
Richard Pasquiou <richard.pasquiou@alfredplace.io>
2022-10-18 12:34:03 +0200

    added upload and post on uploadFile component

--- src/dependencies/custom-components/UploadFile.tsx
+++ src/dependencies/custom-components/UploadFile.tsx
@@ -1,21 +1,47 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Text } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { uploadFile } from '~core/s3'
+import useFetch from 'use-http'
 
-const UploadFile = ({dataSource, backend, children, ...props}: {dataSource: {_id: null} | null, backend: string, children: React.ReactChildren}) => {
+const UploadFile = (
+  {
+    dataSource, 
+    backend, 
+    ressource_id, 
+    children, 
+    ...props
+  }
+  : {
+      dataSource: {_id: null} | null, 
+      backend: string, 
+      ressource_id: string, 
+      children: React.ReactChildren
+    }
+  ) => {
   
-  const handleUpload = (e: React.FormEvent) => {
+  const [uploadInfo, setUploadInfo] = useState('')
+  const {post, response, error} = useFetch(backend)
+
+  const handleUpload = async (e: React.FormEvent) => {
+
+    /* For prod test */ 
+    console.log('uploadTest', dataSource, backend, ressource_id, children, props)
     
     e.preventDefault()
-    const [fileToUpload] = Array.from(e.target?.elements).filter(el => el.type === 'file')
+    const form = e.target as HTMLFormElement
+    const inputFile = form.querySelector('[type="file"]') as HTMLInputElement
+    const fileToUpload = inputFile && inputFile?.files && inputFile?.files[0] as File
     
     if (fileToUpload) {
-      const theFile = fileToUpload?.files && fileToUpload?.files[0]
-
-      /* 
-        TODO
-        1. upload file
-        2. send post (domain/myAlfred/api/action, {action: 'setResourceFile', parent: <resource_id>, url: <s3_url>})
-      */
+      
+      // upload file to S3
+      const uploadedFile = await uploadFile(fileToUpload?.name , fileToUpload)
+        .catch((err) => console.error(err))
+      // Send ressource url 
+      await post('myAlfred/api/action', {action: 'setResourceFile', parent: ressource_id, url: uploadedFile?.Location})
+      if (response.ok) setUploadInfo('Ressource ajoutée')
+      if (error) setUploadInfo('Echec ajout ressource')
+      
     }
     
   }
@@ -25,6 +51,7 @@ const UploadFile = ({dataSource, backend, children, ...props}: {dataSource: {_id
       <form onSubmit={handleUpload}>
         {children}
       </form>
+      {uploadInfo && <Text>{uploadInfo}</Text>} {/*Component status */}
     </Box>
   )
 }
