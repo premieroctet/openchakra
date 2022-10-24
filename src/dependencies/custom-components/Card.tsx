08fdb13fc03c98367657d3c8727d0f9475b36f57
Richard Pasquiou <richard.pasquiou@alfredplace.io>
2022-10-24 16:45:14 +0200

    Card is a simple container

--- src/dependencies/custom-components/Card.tsx
+++ src/dependencies/custom-components/Card.tsx
@@ -1,20 +1,9 @@
-import { Grid, Center, Avatar, Text, Button } from '@chakra-ui/react'
 import React from 'react'
 
-const Card = () => (
-  <Grid gap={6} maxWidth={300} backgroundColor="whatsapp.100">
-    <Center>
-      <Avatar />
-    </Center>
-    <Text textAlign="center" fontSize="lg">
-      Text value
-    </Text>
-    <Text>Text value</Text>
-    <Text>Text value</Text>
-    <Button variant="solid" size="md">
-      Button text
-    </Button>
-  </Grid>
+const Card = ({children, ...props}: {children: React.ReactNode}) => (
+  <div {...props}>
+    {children}
+  </div>
 )
 
 export default Card
