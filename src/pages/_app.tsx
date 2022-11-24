f3608c368a5ba5ba48411737bee635538d9715fa
Richard Pasquiou <97103178+rpasquiou@users.noreply.github.com>
2022-11-24 16:25:03 +0100

    added text input to change font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '@chakra-ui/theme'
+// import theme from '../theme/index'
 import 'react-color-picker/index.css'
 import '@reach/combobox/styles.css'
 
@@ -8,10 +9,12 @@ import { wrapper } from '~core/store'
 import { ErrorBoundary as BugsnagErrorBoundary } from '~utils/bugsnag'
 import AppErrorBoundary from '~components/errorBoundaries/AppErrorBoundary'
 import { AppProps } from 'next/app'
+import Fonts from '~theme/Fonts'
 
 const Main = ({ Component, pageProps }: AppProps) => (
   <BugsnagErrorBoundary>
     <ChakraProvider resetCSS theme={theme}>
+      <Fonts />
       <AppErrorBoundary>
         <Component {...pageProps} />
       </AppErrorBoundary>
