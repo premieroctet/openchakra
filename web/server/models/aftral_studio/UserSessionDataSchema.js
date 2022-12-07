655a3a4dcb14c3e2efa5ec86e620773df9a9b873
Richard Pasquiou <richard.pasquiou@alfredplace.io>
2022-12-07 12:35:10 +0100

    scorm progression WIP

--- web/server/models/aftral_studio/UserSessionDataSchema.js
+++ web/server/models/aftral_studio/UserSessionDataSchema.js
@@ -1,4 +1,4 @@
-const { schemaOptions } = require('../../utils/schemas');
+const {schemaOptions} = require('../../utils/schemas')
 const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
@@ -20,7 +20,29 @@ const UserSessionDataSchema = new Schema({
     spent_time: {
       type: Number,
       required: true,
-    }
+    },
+  }],
+  modules_progress: [{
+    resource: {
+      type: Schema.Types.ObjectId,
+      ref: 'resource',
+      required: true,
+    },
+    module_progress: {
+      type: String,
+      required: true,
+    },
+  }],
+  annotations: [{
+    resource: {
+      type: Schema.Types.ObjectId,
+      ref: 'resource',
+      required: true,
+    },
+    annotation: {
+      type: String,
+      required: false,
+    },
   }],
   finished: [{
     type: Schema.Types.ObjectId,
--- web/server/routes/api/studio.js
+++ web/server/routes/api/studio.js
@@ -20,6 +20,7 @@ const {getModels} =require('../../utils/database')
 const {ACTIONS} = require('../../utils/studio/actions')
 const {buildQuery, addComputedFields} = require('../../utils/database')
 const url=require('url')
+const UserSessionData = require('../../models/UserSessionData')
 
 const router = express.Router()
 
@@ -147,6 +148,43 @@ router.post('/login', (req, res) => {
       console.log(err)
       return res.status(err.status || HTTP_CODES.SYSTEM_ERROR).json(err.message || err)
     })
+})
+
+// router.post('/scormupdate', passport.authenticate('cookie', {session: false}), (req, res) => {
+router.post('/scormupdate', (req, res) => {
+  
+  const value = req.body
+  const idRessource = req?.body?.cmi?.entry
+  const user = req.user
+
+  // console.log(value, idRessource, user)
+  console.log(value)
+
+  // const updateScorm = UserSessionData.findOneAndUpdate({
+  //   user: user._id,
+  // }, {
+  //   user: user._id,
+  // }, {
+  //   upsert: true,
+  //   new: true,
+  // })
+  //   .then(data => {
+  //     const scormProgress = data?.modules_progress?.find(a => a.resource._id.toString() == idRessource.toString())
+  //     if (scormProgress) {
+  //       scormProgress.module_progress = value
+  //     }
+  //     else {
+  //       data.modules_progress.push({
+  //         resource: parent,
+  //         module_progress: value,
+  //       })
+  //     }
+  //     return data.save()
+  //   })
+  //   .catch(e => console.error(e))
+    
+  // return res.json(updateScorm)
+  return res.json({})
 })
 
 router.get('/current-user', passport.authenticate('cookie', {session: false}), (req, res) => {
