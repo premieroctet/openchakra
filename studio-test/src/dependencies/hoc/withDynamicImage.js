d8ab4e2f2d55438aa091d78ebd986ad49e56dfd0
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2022-12-07 18:01:15 +0100

    Updated studio-test dependencies from studio

--- studio-test/src/dependencies/hoc/withDynamicImage.js
+++ studio-test/src/dependencies/hoc/withDynamicImage.js
@@ -4,17 +4,10 @@ import lodash from "lodash";
 const withDynamicImage = Component => {
   const internal = props => {
     const src = lodash.get(props.dataSource, props.attribute);
-    const version = lodash.get(props.dataSource, "version");
     if (!src) {
       return null;
     }
-    return (
-      <Component
-        {...lodash.omit(props, ["children"])}
-        src={src}
-        version={version}
-      />
-    );
+    return <Component {...lodash.omit(props, ["children"])} src={src} />;
   };
 
   return internal;
