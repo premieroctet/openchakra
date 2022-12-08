f37c23aaf73dd771d661f0693fc1b43e6086adae
Richard Pasquiou <97103178+rpasquiou@users.noreply.github.com>
2022-12-08 11:09:55 +0100

    Revert "Updated studio-test dependencies from studio"
    This reverts commit d8ab4e2f2d55438aa091d78ebd986ad49e56dfd0.

--- studio-test/src/dependencies/hoc/withDynamicImage.js
+++ studio-test/src/dependencies/hoc/withDynamicImage.js
@@ -4,10 +4,17 @@ import lodash from "lodash";
 const withDynamicImage = Component => {
   const internal = props => {
     const src = lodash.get(props.dataSource, props.attribute);
+    const version = lodash.get(props.dataSource, "version");
     if (!src) {
       return null;
     }
-    return <Component {...lodash.omit(props, ["children"])} src={src} />;
+    return (
+      <Component
+        {...lodash.omit(props, ["children"])}
+        src={src}
+        version={version}
+      />
+    );
   };
 
   return internal;
