36a4cfaf5560a17f58ea403edae828adce2a28d0
Richard Pasquiou <richard.pasquiou@alfredplace.io>
2022-12-09 12:12:28 +0100

    no more strict mode on studio-test

--- studio-test/src/index.js
+++ studio-test/src/index.js
@@ -5,11 +5,7 @@ import App from "./App";
 //import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
