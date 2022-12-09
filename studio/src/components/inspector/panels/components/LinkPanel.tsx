fb26f1a9c5df06eea982411dec4f928cc5ab0767
Richard Pasquiou <richard.pasquiou@alfredplace.io>
2022-12-09 12:50:37 +0100

    added pagelist on Link component

--- studio/src/components/inspector/panels/components/LinkPanel.tsx
+++ studio/src/components/inspector/panels/components/LinkPanel.tsx
@@ -1,13 +1,37 @@
 import React, { memo } from 'react'
+import { Input } from '@chakra-ui/react'
 import SwitchControl from '~components/inspector/controls/SwitchControl'
-import TextControl from '~components/inspector/controls/TextControl'
-import ChildrenControl from '~components/inspector/controls/ChildrenControl'
+import FormControl from '~components/inspector/controls/FormControl'
+import { useForm } from '~hooks/useForm'
+import usePropsSelector from '~hooks/usePropsSelector'
+import { useSelector } from 'react-redux'
+import { getPages } from '~/core/selectors/components'
 
 const LinkPanel = () => {
+  const pages = useSelector(getPages)
+  const { setValueFromEvent } = useForm()
+  const href = usePropsSelector('href')
+
   return (
     <>
-      {/* <ChildrenControl /> */}
-      <TextControl name="href" label="Href" />
+      <FormControl htmlFor={`href`} label={'Href'}>
+        <Input
+          id={`href`}
+          list={`href-flavors`}
+          size="xs"
+          type="text"
+          name={`href`}
+          value={href || ''}
+          onChange={setValueFromEvent}
+          autoComplete="off"
+        />
+
+        <datalist id={`href-flavors`}>
+          {Object.keys(pages).map(page => (
+            <option key={pages[page].pageId} value={pages[page].pageName} />
+          ))}
+        </datalist>
+      </FormControl>
       <SwitchControl label="External" name="isExternal" />
     </>
   )
