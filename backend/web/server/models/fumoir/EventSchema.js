03435f433b7eecd773cbcaa18a0ba1c017ff4db2
Sébastien Auvray <sebastien.auvray@free.fr>
2022-12-12 16:48:54 +0100

    back: all Event attributes unrequired

--- backend/web/server/models/fumoir/EventSchema.js
+++ backend/web/server/models/fumoir/EventSchema.js
@@ -1,46 +1,53 @@
-const mongoose = require('mongoose')
-const {schemaOptions} = require('../../utils/schemas')
-const {PLACES}=require('../../../utils/fumoir/consts')
+const mongoose = require("mongoose");
+const { schemaOptions } = require("../../utils/schemas");
+const { PLACES } = require("../../../utils/fumoir/consts");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-const EventSchema = new Schema({
-  title: {
-    type: String,
+const EventSchema = new Schema(
+  {
+    title: {
+      type: String
+    },
+    description: {
+      type: String
+    },
+    picture: {
+      type: String
+    },
+    start_date: {
+      type: Date,
+      required: false
+    },
+    end_date: {
+      type: Date,
+      required: false
+    },
+    booking_user: {
+      // User who booked
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: false
+    },
+    members: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user"
+      }
+    ],
+    guests: [
+      {
+        // Guest email addresses
+        type: String
+      }
+    ],
+    place: {
+      type: String,
+      enum: [...Object.keys(PLACES)],
+      required: false
+    }
   },
-  description: {
-    type: String,
-  },
-  picture: {
-    type: String,
-  },
-  start_date: {
-    type: Date,
-    required: true,
-  },
-  end_date: {
-    type: Date,
-    required: true,
-  },
-  booking_user: { // User who booked
-    type: Schema.Types.ObjectId,
-    ref: 'user',
-    required: true,
-  },
-  members: [{
-    type: Schema.Types.ObjectId,
-    ref: 'user',
-  }],
-  guests: [{ // Guest email addresses
-    type: String,
-  }],
-  place: {
-    type: String,
-    enum: [...Object.keys(PLACES)],
-    required: true,
-  },
-  
-}, schemaOptions)
+  schemaOptions
+);
 
-
-module.exports = EventSchema
+module.exports = EventSchema;
