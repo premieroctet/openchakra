e0e5964ec8d7587ce8b654b254a0253d4b4637bd
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2022-12-14 18:14:39 +0100

    backend: last fixed _document page

--- backend/web/pages/_document.js
+++ backend/web/pages/_document.js
@@ -1,9 +1,21 @@
 import React from "react";
-import Document from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
-    return null;
+    return (
+      <Html lang="fr" dir="ltr" xmlns:og="http://ogp.me/ns#">
+        <Head>
+          <meta charSet="utf-8" />
+          {/* PWA primary color */}
+          <meta name="theme-color" content={null} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
 
