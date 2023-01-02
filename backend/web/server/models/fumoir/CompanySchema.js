6c4762670dbb727fda51f49b1dab7794e0a6a4c4
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-01-02 18:32:08 +0100

    fumoir: company model

--- backend/web/server/models/fumoir/CompanySchema.js
+++ backend/web/server/models/fumoir/CompanySchema.js
@@ -1,3 +1,24 @@
-const CompanySchema = require('../others/CompanySchema')
+const mongoose = require('mongoose')
+const {schemaOptions} = require('../../utils/schemas')
+const {hideIllegal} = require('../../../utils/text')
+
+const Schema = mongoose.Schema
+
+const CompanySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Le nom de la société est requis'],
+    },
+    siret: {
+      type: String,
+    },
+    description: {
+      type: String,
+      set: text => hideIllegal(text),
+    },
+  },
+  schemaOptions,
+)
 
 module.exports = CompanySchema
