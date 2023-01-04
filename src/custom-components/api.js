6337717c6599b1c0d4a0f777641e5754b2b8e11b
Rishabh Raizada <rishabh.raizada@trilogy.com>
2023-01-04 22:00:40 +0530

    Fixed deploy API

--- src/custom-components/api.js
+++ src/custom-components/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const API = axios.create({
   baseURL: `${typeof window !== 'undefined' && window.location.href}api`,
-  timeout: 90000,
+  timeout: 120000,
 })
 
 export default API
--- src/pages/api/deploy.js
+++ src/pages/api/deploy.js
@@ -2,7 +2,10 @@ import shell from 'shelljs'
 
 export default async function handler(req, res) {
   try {
-    shell.exec(`bash ../../scripts/release.sh <<< "${req.body.message}"`)
+    shell.exec(
+      `cd ../.. && bash scripts/release.sh <<< "${req.body.message}"`,
+      { shell: '/bin/bash' },
+    )
 
     res.status(200).json(req.body.message)
   } catch (err) {
