febe64ab6ab6ab52188165f3776700723a39d4c6
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-01-22 21:01:04 +0100

    Tests fix

--- backend/web/tests/aftral_studio/virtualEnumsTest.test.js
+++ backend/web/tests/aftral_studio/virtualEnumsTest.test.js
@@ -1,22 +1,31 @@
+const moment=require('moment')
 const mongoose = require('mongoose')
-const {STATUS} = require('../utils/aftral_studio/consts')
-const {MONGOOSE_OPTIONS, getModels} = require('../server/utils/database')
-const {getDatabaseUri} = require('../config/config')
-require('../server/models/Resource')
-require('../server/models/Theme')
+const {forceDataModelAftralStudio}=require('../utils')
+forceDataModelAftralStudio()
+require('../../server/utils/studio/aftral_studio/functions')
+const {STATUS} = require('../../utils/aftral_studio/consts')
+const {MONGOOSE_OPTIONS, getModels} = require('../../server/utils/database')
+require('../../server/models/Resource')
+require('../../server/models/Theme')
+require('../../server/models/TrainingCenter')
 
 describe('Schema virtual enum attributes', () => {
 
-  beforeAll(() => {
-    return mongoose.connect(getDatabaseUri(), MONGOOSE_OPTIONS)
+  beforeAll(async() => {
+    await mongoose.connect(`mongodb://localhost/test${moment().unix()}`, MONGOOSE_OPTIONS)
+  })
+
+  afterAll(async() => {
+    await mongoose.connection.dropDatabase()
+    await mongoose.connection.close()
   })
 
   test('Should return theme status enumValues', async() => {
     const models=await getModels()
     const themeAttributes=models.theme.attributes
-    expect(themeAttributes.status.enumValues).toEqual([null, ...Object.keys(STATUS)])
+    expect(themeAttributes.status.enumValues).toEqual(STATUS)
     const resourcesAttributes=models.resource.attributes
-    return expect(resourcesAttributes.status.enumValues).toEqual([null, ...Object.keys(STATUS)])
+    return expect(resourcesAttributes.status.enumValues).toEqual(STATUS)
   })
 
 })
