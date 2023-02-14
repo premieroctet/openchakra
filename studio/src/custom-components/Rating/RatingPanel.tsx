98dbe8b5df3fd8506a4f79f5a44d3b61d7737764
Richard Pasquiou <richard.pasquiou@alfredplace.io>
2023-02-14 14:19:38 +0100

    wip Rating

--- studio/src/custom-components/Rating/RatingPanel.tsx
+++ studio/src/custom-components/Rating/RatingPanel.tsx
@@ -1,10 +1,73 @@
 import React, { memo } from 'react'
-import ChildrenControl from '~components/inspector/controls/ChildrenControl'
+import { 
+  Input, 
+  Select,
+} from '@chakra-ui/react'
+import { useForm } from '~hooks/useForm'
+import FormControl from '~components/inspector/controls/FormControl'
+import usePropsSelector from '~hooks/usePropsSelector'
 
 const RatingPanel = () => {
+
+  const { setValueFromEvent } = useForm()
+  const illu = usePropsSelector('illu')
+  const size = usePropsSelector('size')
+  const scale = usePropsSelector('scale')
+  const fillColor = usePropsSelector('fillColor')
+  const strokeColor = usePropsSelector('strokeColor')
+
   return (
     <>
-      <ChildrenControl />
+       <FormControl htmlFor="icon" label="Icon">
+        <Select
+          id="icon"
+          value={illu || ''}
+          onChange={setValueFromEvent}
+          name="illu"
+          size="sm"
+        >
+          <option value={'Star'}>Star</option>
+          <option value={'Flame'}>Flame</option>
+        </Select>
+      </FormControl>
+       <FormControl label="Size" htmlFor="ratingsize">
+        <Input
+          value={size || ''}
+          id="ratingsize"
+          type={'number'}
+          size="sm"
+          name="size"
+          onChange={setValueFromEvent}
+        />
+      </FormControl>
+       <FormControl label="Fill color" htmlFor="ratingfillcolor">
+        <Input
+          value={fillColor || ''}
+          id="ratingfillcolor"
+          size="sm"
+          name="fillColor"
+          onChange={setValueFromEvent}
+        />
+      </FormControl>
+       <FormControl label="Stroke color" htmlFor="ratingstrokecolor">
+        <Input
+          value={strokeColor || ''}
+          id="ratingstrokecolor"
+          size="sm"
+          name="strokeColor"
+          onChange={setValueFromEvent}
+        />
+      </FormControl>
+      <FormControl label="Scale" htmlFor="ratingscale">
+        <Input
+          value={scale || ''}
+          id="ratingscale"
+          type={'number'}
+          size="sm"
+          name="scale"
+          onChange={setValueFromEvent}
+        />
+      </FormControl>
     </>
   )
 }
--- studio/src/dependencies/custom-components/Rating.tsx
+++ studio/src/dependencies/custom-components/Rating.tsx
@@ -1,36 +1,81 @@
-import React from 'react'
-import { RadioGroup, Radio, Stack } from '@chakra-ui/react';
+import React, { useState } from "react";
+import { Box, Icon, Stack, Text } from '@chakra-ui/react';
+import * as icons from 'lucide-react'
 
 
+const Rating = React.forwardRef(
+  ({ 
+    value = 0,
+    size = 20, 
+    illu = 'Star', 
+    scale = 5, 
+    fillColor = 'gold', 
+    strokeColor = 'gray' 
+  }: {
+    value: Number
+    size: Number
+    illu: String
+    scale: Number
+    fillColor: String
+    strokeColor: String
+  }, ref) => {
+    const [rating, setRating] = useState(value);
+    const buttons = [];
 
-const Note = (
-  {
-    "data-value": dataValue = 3, 
-    "data-highestnote": highestNote = 5, 
-    ...props
-  }
-  :{
-    'data-value': number, 
-    'data-highestnote': number
-  }) => {
+    const onClick = idx => {
+      if (!isNaN(idx)) {
+        // allow user to click first icon and set rating to zero if rating is already 1
+        if (rating === 1 && idx === 1) {
+          setRating(0);
+        } else {
+          setRating(idx);
+        }
+      }
+    };
 
-  return (
-      <Stack isInline>
-      {Array.from({length: +highestNote}, (el, index) => (
-        <label>
-          <input 
-            type={'radio'} 
-            name={`rating`} 
-            className=""
-            value={index + 1} 
-            {...dataValue === index + 1 && {checked: true}} 
-          />
-        </label>
-        )
-      )}
+    const RatingIcon = ({ fill }) => {
+      return (
+       <Icon
+          as={icons[illu]}
+          name={illu}
+          boxSize={`${size * 1.3}px`}
+          stroke={strokeColor}
+          onClick={onClick}
+          fillOpacity={fill ? "100%" : "0"}
+          fill={fillColor}
+        />
+      );
+    };
+
+    const RatingButton = ({ idx, fill }) => {
+      return (
+        <Box
+          as="button"
+          aria-label={`Rate ${idx}`}
+          height={`${size}px`}
+          width={`${size}px`}
+          mx={1}
+          onClick={() => onClick(idx)}
+          _focus={{ outline: 0 }}
+        >
+          <RatingIcon fill={fill} />
+        </Box>
+      );
+    };
+
+    for (let i = 1; i <= scale; i++) {
+      buttons.push(<RatingButton key={i} idx={i} fill={i <= rating} />);
+    }
+
+    return (
+      <Stack isInline justify="center">
+        <input name="rating" type="hidden" value={rating} ref={ref} />
+        {buttons}
       </Stack>
-  )
+    );
+  }
+);
 
-}
+Rating.displayName = "Rating";
 
-export default Note
+export default Rating;
