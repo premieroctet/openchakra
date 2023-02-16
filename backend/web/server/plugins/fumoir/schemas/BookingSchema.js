35a4da3ecf1e8965140605548c31f7a66cfe9f95
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-02-16 23:40:03 +0100

    Back: RAS

--- backend/web/server/plugins/fumoir/schemas/BookingSchema.js
+++ backend/web/server/plugins/fumoir/schemas/BookingSchema.js
@@ -157,7 +157,7 @@ BookingSchema.virtual('remaining_vat_amount').get(function() {
 })
 
 BookingSchema.virtual('paid').get(function() {
-  if (lodash.isEmpty(this.items.length)) {
+  if (lodash.isEmpty(this.items)) {
     return false
   }
   const already_paid=lodash(this.payments).filter(p => p.status==PAYMENT_SUCCESS).map('amount').sum()
