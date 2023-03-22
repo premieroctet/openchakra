9c577f8371477959fd96cfe270b42f84347be0b1
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-03-22 18:42:03 +0100

    Script to generate enums JS code

---
+++ backend/web/scripts/generateEnums.js
@@ -0,0 +1,14 @@
+const lodash=require('lodash')
+const {normalize}=require('../utils/text')
+
+const typeName=normalize(process.argv[2]).replace(/ /g, '_').toUpperCase()
+const values=process.argv.slice(3)
+const constants=values.map(v => `${typeName}_${normalize(v).replace(/ /g, '_').toUpperCase()}`)
+
+const pairs=lodash.zip(constants, values)
+
+pairs.map(([constant]) => console.log(`const ${constant}="${constant}"`))
+
+console.log(`\nconst ${typeName}={`)
+pairs.map(([constant, value]) => console.log(`\ ${constant}:"${value}",`))
+console.log('}')
