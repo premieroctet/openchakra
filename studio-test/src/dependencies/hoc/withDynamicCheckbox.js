28937f1359eaa6bedd2ef630306ada53ac9144aa
Richard Pasquiou <richard.pasquiou@wappizy.com>
2023-03-31 17:45:07 +0200

    DynamicCheckbox with enum

--- studio-test/src/dependencies/hoc/withDynamicCheckbox.js
+++ studio-test/src/dependencies/hoc/withDynamicCheckbox.js
@@ -1,14 +1,13 @@
-import { ACTIONS } from '../utils/actions'
-
 import React, {useState} from 'react'
 import lodash from 'lodash'
+import { ACTIONS } from '../utils/actions'
 
 const withDynamicCheckbox = Component => {
-  const Internal = ({ dataSource, context, subDataSource, subAttribute, enum: enums, backend, ...props }) => {
+  const Internal = ({ dataSource, context, noautosave, subDataSource, subAttribute, enum: enums, backend, ...props }) => {
 
+    const checkOptions = enums ? Object.entries(JSON.parse(enums)) : []
     const initialValue = lodash.get(dataSource, props.attribute)
-    const [value, setValue]=useState(initialValue)
-    let refValues=null
+    const [retainedvalue, setValue]=useState(initialValue)
 
     const onChange = ev => {
       setValue(!!ev.target.checked)
@@ -20,22 +19,30 @@ const withDynamicCheckbox = Component => {
       }).then(() => props.reload())
     }
 
-    // attribute, dataModel, value
-
-    if (enums) {
-      const checkOptions = JSON.parse(enums)
+    const onEnumChange = ev => {
+      setValue(ev.target.value)
+      if (!noautosave) {
+        ACTIONS.putValue({
+          context: dataSource?._id,
+          value: ev.target.value,
+          props,
+          backend,
+        }).then(() => props.reload())
+      }
     }
 
-
-    console.log({ dataSource, context, subDataSource, subAttribute, enums, backend, ...props })
-    // console.log(checkOptions)
-    console.log(initialValue)
-    console.log('props', props)
-
-    const pr={...props, isChecked: value, value}
-    return (
-      <div {...pr}>
-        <Component {...lodash.omit(props, ['id'])} isChecked={value} onChange={onChange} />
+    const pr={...props, isChecked: retainedvalue, value: retainedvalue}
+    return (enums ? 
+      <div>
+        {checkOptions.map(([key, value]) => {
+          console.log({key, value})
+          return (
+            <Component {...lodash.omit(props, ['id'])} value={key} isChecked={retainedvalue === key} onChange={onEnumChange}>{value}</Component>
+          )
+        })}
+        </div>
+      : <div {...pr}>
+        <Component {...lodash.omit(props, ['id'])} isChecked={retainedvalue} onChange={onChange} />
       </div>
     )
   }
