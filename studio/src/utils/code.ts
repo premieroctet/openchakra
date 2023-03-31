aa1949de9adf74f784889f0c3179e2989279c2f8
Richard Pasquiou <richard.pasquiou@wappizy.com>
2023-03-31 11:21:00 +0200

    keep actual standard using units for breakpoints

--- studio/src/utils/code.ts
+++ studio/src/utils/code.ts
@@ -342,9 +342,7 @@ const buildBlock = ({
           if (propsValueAsObject && Object.keys(propsValue).length >= 1) {
             const gatheredProperties = Object.entries(propsValue)
               .map(([prop, value]) => {
-                return value && /^\d*$/.test(value)
-                  ? ` '${prop}': ${value} `
-                  : ` '${prop}': '${value}' `
+                return ` '${prop}': '${value}' `
               })
               .join(', ')
 
