e4b52fd3ed7343a20e52776369d8c09092e6df29
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-04-13 12:42:33 +0200

    AllE: Fix import

--- backend/web/server/plugins/all-inclusive/schemas/UserSchema.js
+++ backend/web/server/plugins/all-inclusive/schemas/UserSchema.js
@@ -1,8 +1,8 @@
+const { AVAILABILITY, COACHING, COACH_OTHER, ROLES } = require('../consts')
 const mongoose = require("mongoose")
 const bcrypt=require('bcryptjs')
 const { schemaOptions } = require('../../../utils/schemas')
 const lodash=require('lodash')
-const { AVAILABILITY, COACHING, ROLES } = require('../consts')
 const IBANValidator = require('iban-validator-js')
 
 const Schema = mongoose.Schema;
