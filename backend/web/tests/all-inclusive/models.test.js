f751ef4525e640373fafa2917702e3d5114b49af
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-04-17 15:33:52 +0200

    AllE: Quotation status

--- backend/web/tests/all-inclusive/models.test.js
+++ backend/web/tests/all-inclusive/models.test.js
@@ -1,5 +1,8 @@
+const {
+  COACH_ALLE,
+  ROLE_TI
+} = require('../../server/plugins/all-inclusive/consts')
 const Skill = require('../../server/models/Skill')
-const { COACH_ALLE } = require('../../server/plugins/all-inclusive/consts')
 const User = require('../../server/models/User')
 const JobUser = require('../../server/models/JobUser')
 const moment=require('moment')
@@ -16,8 +19,8 @@ describe('Test virtual single ref', () => {
   beforeAll(async() => {
     await mongoose.connect(`mongodb://localhost/test${moment().unix()}`, MONGOOSE_OPTIONS)
     const user=await User.create({
-        firstname: 'Sébastien', name: 'Auvray', birthday: moment(), cguAccepted: true,
-        password: 'prout', email: 's@a.com', coaching: COACH_ALLE})
+        firstname: 'Sébastien', lastname: 'Auvray', birthday: moment(), cguAccepted: true,
+        password: 'prout', email: 's@a.com', coaching: COACH_ALLE, role: ROLE_TI})
     const job=await JobUser.create({name: 'Peintre', user})
     const skill=await Skill.create({name: 'A skill', job})
   })
@@ -31,6 +34,17 @@ describe('Test virtual single ref', () => {
     const skills=await Skill.find()
     const user=await User.findOne().populate({"path":"jobs","populate":"skills"})
     expect(user.jobs[0].skills).toHaveLength(1)
+    await JobUser.update({customer_location: false, foreign_location: false})
+    expect((await JobUser.findOne()).location_str).toEqual('')
+    await JobUser.update({customer_location: true, foreign_location: false})
+    expect((await JobUser.findOne()).location_str).toEqual('Chez le client')
+    await JobUser.update({customer_location: false, foreign_location: true})
+    expect((await JobUser.findOne()).location_str).toEqual('À distance')
+    await JobUser.update({customer_location: true, foreign_location: true})
+    expect((await JobUser.findOne()).location_str).toEqual('Chez le client et à distance')
+  })
+
+  it('must display location_str', async() => {
     await JobUser.update({customer_location: false, foreign_location: false})
     expect((await JobUser.findOne()).location_str).toEqual('')
     await JobUser.update({customer_location: true, foreign_location: false})
