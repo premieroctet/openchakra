adbbcbeb2361f3bfdf0e14e2b8d269f1804acbb6
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-04-19 00:32:20 +0200

    Master: clean code generation WIP

--- studio/src/tests/utils/code.test.ts
+++ studio/src/tests/utils/code.test.ts
@@ -8,7 +8,7 @@ describe('Code generation', () => {
     return Promise.all(PROJECTS.map(p => {
       return Promise.all([import(`./${p}.json`), import(`./${p}_model.json`)])
         .then(([project, models])=> {
-          console.log(project)
+          console.log(project.pages)
           /**
           Object.keys(project.pages).map(pageId => {
             return generateCode(pageId, project.pages, models)
