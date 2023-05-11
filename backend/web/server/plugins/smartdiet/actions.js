c98bdb59a95fe6410df0dc0ec3004163ec4b4d8e
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-05-11 17:22:42 +0200

    Smartdiet: fix join/leave group action

--- backend/web/server/plugins/smartdiet/actions.js
+++ backend/web/server/plugins/smartdiet/actions.js
@@ -3,6 +3,7 @@ const {addAction} = require('../../utils/studio/actions')
 
 const smartdiet_join_group = ({value, join}, user) => {
   return Group.findByIdAndUpdate(value, join ? {$addToSet: {users: user._id}} : {$pull: {users: user._id}})
+    .then(() => Group.findById(value))
 }
 
 addAction('smartdiet_join_group', smartdiet_join_group)
