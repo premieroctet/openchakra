55480356037cecf079b127f76992546d9c7e9331
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-06-13 11:11:21 +0200

    Studio: script to fix extra Lexical values

---
+++ studio/scripts/fix_smartdiet_lexical.js
@@ -0,0 +1,16 @@
+const fs=require('fs')
+const project=require(process.argv[2])
+const outputFile=`${process.argv[2]}.fix`
+
+Object.values(project.pages)
+  .forEach(page => {
+    Object.values(page.components)
+      .filter(comp => comp.type!='Lexical' && /^<p /.test(comp.props.value))
+      .forEach(comp => {
+        console.log(comp.type, comp.props.value)
+        comp.props.value=""
+      })
+  })
+
+fs.writeFileSync(outputFile, JSON.stringify(project, null, 2))
+console.log(`Saved fixed of ${process.argv[2]} to ${outputFile}`)
