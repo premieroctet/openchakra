c2ff7e2aefd129a0319228c10d6cdfdb0246f56e
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-07-03 18:07:40 +0200

    Smartdiet: set keys on user.contents

--- backend/web/server/plugins/smartdiet/functions.js
+++ backend/web/server/plugins/smartdiet/functions.js
@@ -128,7 +128,7 @@ USER_MODELS.forEach(m => {
       options: {ref: 'content'}},
   })
   declareVirtualField({model: m, field: 'contents', instance: 'Array',
-    requires: '_all_contents.comments_count,_all_targets,targets,_all_contents.targets,objective_targets,health_targets,activity_targets,specificity_targets,home_target,_all_contents.search_text',
+    requires: '_all_contents.key,_all_contents.comments_count,_all_targets,targets,_all_contents.targets,objective_targets,health_targets,activity_targets,specificity_targets,home_target,_all_contents.search_text',
     multiple: true,
     caster: {
       instance: 'ObjectID',
