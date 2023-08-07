9d755583acb73d54e7c76bdd82ba89b330cfb63e
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-08-07 17:04:19 +0200

    Smartagenda WIP

--- backend/web/server/plugins/agenda/smartagenda.js
+++ backend/web/server/plugins/agenda/smartagenda.js
@@ -7,17 +7,22 @@ INFOS:
 - diets can be found in the pdo_agenda's only, no references elsewhere (i.e. no account)
 - pdo_events.apilnk_client_id : pdo_client/client id
 - pdo_events.apilnk_equipe_id: 'pdo_agenda/diet_id'
+- appointments start & end dates must be rounded at 1/4h
 */
 const axios = require('axios')
 const config = require('../../../config/config')
 const crypto=require('crypto')
+const moment=require('moment')
+require('moment-round')
 
 const SMARTAGENDA_CONFIG={
   ...config.getSmartAgendaConfig(),
   SMARTAGENDA_SHA1_PASSWORD: crypto.createHash('sha1').update(config.getSmartAgendaConfig().SMARTAGENDA_PASSWORD).digest('hex'),
 }
 
-const ALL_DATA=
+const MAX_RESULTS=1000
+
+const ALL_DATA=//['pdo_type_indispo']
 `pdo_client,pdo_agenda,pdo_groupe,pdo_type_rdv,pdo_events,pdo_events_ouverture,
 pdo_events_supprime,pdo_type_indispo,pdo_agenda_type_rdv,pdo_ressource,pdo_form,
 pdo_form_champ,pdo_form_type_rdv,pdo_surveillance,pdo_envoi,pdo_journal,pdo_groupement`.replace(/\n/g, '').split(',')
@@ -27,6 +32,34 @@ const BASE_URL='https://www.smartagenda.fr/pro/smartdiet/api'
 const ACCOUNT_URL='https://www.smartagenda.fr/pro/smartdiet/api/pdo_client'
 const AGENDAS_URL='https://www.smartagenda.fr/pro/smartdiet/api/pdo_agenda'
 const EVENTS_URL='https://www.smartagenda.fr/pro/smartdiet/api/pdo_events'
+
+// returns moment rounded to the nearest 15 minutes
+const momentToQuarter = m => {
+  if (!m) { return m}
+  const res=moment(m).round(15, 'minutes')
+  return res
+}
+
+const SMARTDIET_DATE_FORMAT='YYYY-MM-DD HH:mm:00'
+// moment => 2000-01-01 12:00:00
+const momentToSmartDate = m => {
+  if (!m) { return m}
+  const res=momentToQuarter(m).format('YYYY-MM-DD HH:mm:00')
+  return res
+}
+
+// moment => 2000-01-01 12:00:00
+const smartDietToMoment = sm => {
+  if (!sm) { return sm}
+  if (!/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/.test(sm)) {
+    throw new Error(`Incorrect format:${sm}`)
+  }
+  const res=moment(sm, SMARTDIET_DATE_FORMAT)
+  if (!res.isValid()) {
+    throw new Error(`Incorrect moment:${sm},moment.invalidatAt:${res.invalidAt()}`)
+  }
+  return res
+}
 
 const getToken = () => {
   const params={
@@ -39,9 +72,14 @@ const getToken = () => {
     .then(res => res.data.token)
 }
 
-const getAccounts = () => {
+const getAccounts = (filter) => {
+  let filters=filter?.email ? {
+    'filter[0][field]': 'mail',
+    'filter[0][comp]': 'LIKE',
+    'filter[0][value]': `%${filter.email}%`,
+  }: {}
   return getToken()
-    .then(token => axios.get(ACCOUNT_URL, {params:{token}}))
+    .then(token => axios.get(ACCOUNT_URL, {params:{token, nbresults: MAX_RESULTS, ...filters}}))
     .then(res => res.data)
 }
 
@@ -61,8 +99,9 @@ const getEvents = () => {
 const getAllData = () => {
   return getToken()
     .then(token => Promise.allSettled(ALL_DATA.map(data =>
-      axios.get(`${BASE_URL}/${data}`,  {params:{nbresults:100000, token}}).then(r => r.data))))
-    .then(res => res.map((r, idx) => [ALL_DATA[idx], r.value]))
+      axios.get(`${BASE_URL}/${data}`,  {params:{nbresults: MAX_RESULTS*100, token}}).then(r => r.data))))
+    //.then(res => Object.entries(res.map((r, idx) => [ALL_DATA[idx], r.value])))
+    .then(res => Object.fromEntries(res.map((r, idx) => [ALL_DATA[idx], r.value])))
 }
 
 
@@ -71,12 +110,55 @@ const getDietAppointments = diet_id => {
     'filter[0][field]':'equipe_id',
     'filter[0][comp]': '=',
     'filter[0][value]': diet_id,
-    'filter[1][field]':'client_id',
-    'filter[1][comp]': '<>',
-    'filter[1][value]': 0,
   }
   return getToken()
-    .then(token => axios.get(EVENTS_URL+'?sortdesc', {params:{token, nbresults:10000, ...filter, sortby: 'start_date'}}))
+    .then(token => axios.get(EVENTS_URL+'?sortdesc', {params:{token, nbresults: MAX_RESULTS, ...filter, sortby: 'start_date'}}))
+    .then(res => res.data)
+}
+
+const getDietUnavailabilities = diet_id => {
+  const filter={
+    'filter[0][field]':'equipe_id',
+    'filter[0][comp]': '=',
+    'filter[0][value]': diet_id,
+    'filter[1][field]':'presta_id',
+    'filter[1][comp]': '=',
+    'filter[1][value]': -1,
+  }
+  return getToken()
+    .then(token => axios.get(EVENTS_URL+'?sortdesc', {params:{token, nbresults: MAX_RESULTS, ...filter, sortby: 'start_date'}}))
+    .then(res => res.data)
+}
+
+const getCustomerAppointments = customer_id => {
+  const filter={
+    'filter[0][field]':'client_id',
+    'filter[0][comp]': '=',
+    'filter[0][value]': customer_id,
+  }
+  return getToken()
+    .then(token => axios.get(EVENTS_URL+'?sortdesc', {params:{token, nbresults: MAX_RESULTS, ...filter, sortby: 'start_date'}}))
+    .then(res => res.data)
+}
+
+const createAppointment = (diet_id, client_id, start_date, end_date) => {
+  const data={
+    equipe_id: diet_id,
+    client_id: client_id,
+    presta_id: '0',
+    text: 'Un rendez-vous',
+    internet: 'App Smartdiet',
+    start_date: momentToSmartDate(start_date),
+    end_date: momentToSmartDate(end_date),}
+
+  return getToken()
+    .then(token => axios.post(`${EVENTS_URL}?token=${token}`, data))
+    .then(res => res.data)
+}
+
+const deleteAppointment = app_id => {
+  return getToken()
+    .then(token => axios.delete(`${EVENTS_URL}?token=${token}&id=${app_id}`))
     .then(res => res.data)
 }
 
@@ -87,4 +169,9 @@ module.exports={
   getEvents,
   getAllData,
   getDietAppointments,
+  createAppointment,
+  getCustomerAppointments,
+  deleteAppointment,
+  getDietUnavailabilities,
+  smartDietToMoment,
 }
--- backend/web/tests/smartdiet/smartagenda.test.js
+++ backend/web/tests/smartdiet/smartagenda.test.js
@@ -1,12 +1,17 @@
-const axios = require('axios')
 const {
+  createAppointment,
+  deleteAppointment,
   getAccounts,
   getAgendas,
   getAllData,
+  getCustomerAppointments,
   getDietAppointments,
+  getDietUnavailabilities,
   getEvents,
   getToken,
+  smartDietToMoment,
 } = require('../../server/plugins/agenda/smartagenda')
+const axios = require('axios')
 const { PERIOD } = require('../../server/plugins/smartdiet/consts')
 const moment = require('moment')
 const lodash=require('lodash')
@@ -17,6 +22,9 @@ forceDataModelSmartdiet()
 const mongoose = require('mongoose')
 
 jest.setTimeout(1000000)
+
+const PAULINE_ID=56
+const SEB_ID=7996
 
 describe('SmartAgenda test ', () => {
 
@@ -31,28 +39,56 @@ describe('SmartAgenda test ', () => {
     expect(token).toBeTruthy()
   })
 
-  it('must get acounts', async() => {
-    const accounts=await getAccounts()
-    console.log(accounts.length)
+  it('must get accounts', async() => {
+    const accounts=await getAccounts({email: 'sebastien.auvray@wappizy.com'})
+    console.log(accounts)
     expect(accounts).toBeTruthy()
   })
 
   it('must get agenda', async() => {
     const agendas=await getAgendas()
-    console.log(agendas.length)
-    expect(agendas).toBeTruthy()
+    console.log(agendas.map(a => a.mail))
+    expect(agendas.length).toBeGreaterThan(0)
   })
 
-  it('must get all data', async() => {
+  it.skip('must get all data', async() => {
     const events=await getAllData()
     console.log(JSON.stringify(events, null, 2))
     expect(events).toBeTruthy()
   })
 
-  it.only('must get diet app', async() => {
-    const events=(await getDietAppointments(8))
-      .map(c => lodash.pick(c, 'start_date,text,client_prenom,client_nom,client_infos'.split(',')))
+  it('must get PAULINE diet appointments', async() => {
+    const events=(await getDietAppointments(PAULINE_ID))
+      .map(c => lodash.omitBy(c, (v, k) => /^(c|montant)/.test(k)))
+    console.log(JSON.stringify(events[0], null, 2))
+  })
+
+  it('must get SEB customer appointments', async() => {
+    const events=(await getCustomerAppointments(SEB_ID))
+    .map(c => lodash.omitBy(c, (v, k) => /^(c|montant)/.test(k)))
     console.log(JSON.stringify(events, null, 2))
+  })
+
+  it('must create then delete an appointment', async() => {
+    const diet_id=PAULINE_ID
+    const customer_id=SEB_ID
+
+    return createAppointment(diet_id, customer_id, moment(), moment().add(4, 'hour'))
+      .then(app => deleteAppointment(app.id))
+      .catch(console.error)
+  })
+
+  it('must find non availabilities', async() => {
+    const events=(await getDietUnavailabilities(PAULINE_ID))
+      .map(c => lodash.omitBy(c, (v, k) => /^(c|montant)/.test(k)))
+      .map(c => lodash.omitBy(c, (v, k) => /^apilnk/.test(k)))
+      .map(c => lodash.omitBy(c, (v, k) => /^date_/.test(k)))
+    console.log(JSON.stringify(events, null, 2))
+  })
+
+  it.only('must convert date', async() => {
+    expect(() => smartDietToMoment('2023-01-09 30:23:15')).toThrow(/Incorrect moment/)
+    expect(smartDietToMoment('2023-01-09 12:23:15').date()).toEqual(9)
   })
 
 })
