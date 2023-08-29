5e2a659f9367463fa2266405eb3b20ec923f4845
Richard Pasquiou <richard.pasquiou@alfredplace.io>
2023-08-29 16:36:40 +0200

    ssh deploy one day

--- studio/src/utils/ssh.ts
+++ studio/src/utils/ssh.ts
@@ -1,13 +1,12 @@
-import config from '../../env.json'
-
 import exec from 'ssh-exec'
 
 const options = {
-  user: config.targetUser,
-  host: config.targetDomain,
-  key: config.privKeyPath,
+  user: process.env.NEXT_PUBLIC_PROJECT_TARGETUSER,
+  host: process.env.NEXT_PUBLIC_PROJECT_TARGETDOMAIN,
+  key: process.env.NEXT_PUBLIC_PROJECT_TARGETKEY,
 }
 
+// TODO : may be used to deploy with SSH
 exec('ls -lh', options).pipe(process.stdout)
 
 export const copyFile = (contents, path) => {}
