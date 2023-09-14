cec357e62836336fe27e73bd22f60707cc178bbd
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-09-14 23:00:28 +0200

    Tracker: fix loggedUser alias

--- backend/web/server/plugins/tracker/functions.js
+++ backend/web/server/plugins/tracker/functions.js
@@ -1,4 +1,4 @@
-const {declareVirtualField, declareEnumField}=require('../../utils/database')
+const {declareVirtualField, declareEnumField, setPreprocessGet}=require('../../utils/database')
 const {ROLES, ISSUE_STATUS}=require('./consts')
 
 declareVirtualField({model: 'loggedUser', field: 'password2', type: 'String'})
@@ -14,3 +14,14 @@ declareVirtualField({model: 'company', field: 'users',
 })
 
 declareEnumField({model: 'issue', field: 'status', enumValues: ISSUE_STATUS})
+
+const preprocessGet = ({model, fields, id, user, params}) => {
+  if (model=='loggedUser') {
+    model='user'
+    id = user?._id || 'INVALIDID'
+  }
+  return Promise.resolve({model, fields, id})
+}
+
+setPreprocessGet(preprocessGet)
+
