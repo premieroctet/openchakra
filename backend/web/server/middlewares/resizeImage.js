be4e1df9c986663a7f308d5b3cb99579fdb12827
Richard Pasquiou <richard.pasquiou@alfredplace.io>
2023-09-15 16:11:08 +0200

    moved consts, method for resizing images on AWS s3

--- backend/web/server/middlewares/resizeImage.js
+++ backend/web/server/middlewares/resizeImage.js
@@ -1,59 +1,8 @@
 const sharp = require('sharp')
-const convert = require('heic-convert')
-const {IMAGES_WIDTHS_FOR_RESIZE, IMAGE_SIZE_MARKER, THUMBNAILS_DIR} = require('../../utils/consts')
 const {sanitizeFilename, generateUUID} = require('../../utils/functions')
+const {IMAGES_WIDTHS_FOR_RESIZE, IMAGE_SIZE_MARKER, THUMBNAILS_DIR} = require('../../utils/consts')
+const {IMAGE_SIZE_SEPARATOR, IMAGE_SETTINGS, switchbuffer} = require('../utils/images')
 
-exports.IMAGE_SIZE_SEPARATOR = '_'
-
-const heicToJpeg = async buffer => {
-  return await convert({
-    buffer: buffer, // the HEIC file buffer
-    format: 'JPEG', // output format
-    quality: 1, // the jpeg compression quality, between 0 and 1
-  })
-}
-
-const RETAINED_QUALITY = 100
-  const JPEG_SETTINGS = {
-    extension: 'jpg',
-    outputFormat: 'jpeg',
-    outputMime: 'image/jpeg',
-    options: {
-      quality: RETAINED_QUALITY,
-      mozjpeg: true,
-    },
-  }
-
-exports.IMAGE_SETTINGS = { // we don't sharp .gif
-  'image/jpg': JPEG_SETTINGS,
-  'image/jpeg': JPEG_SETTINGS,
-  'image/png': {
-    extension: 'png',
-    outputFormat: 'png',
-    outputMime: 'image/png',
-    options: {
-      quality: RETAINED_QUALITY,
-    },
-  },
-  'image/heic': JPEG_SETTINGS,
-  'image/heif': JPEG_SETTINGS,
-  'image/webp': {
-    extension: 'webp',
-    outputFormat: 'webp',
-    outputMime: 'image/webp',
-    options: {
-      quality: RETAINED_QUALITY,
-      lossless: true,
-    },
-  },
-}
-
-exports.switchbuffer = async (buffer, filemimetype) => {
-  if (['image/heic', 'image/heif'].includes(filemimetype)) {
-    return await heicToJpeg(buffer)
-  }
-  return buffer
-}
 
 exports.resizeImage = async(req, res, next) => {
   if (!req.file) { return next() }
