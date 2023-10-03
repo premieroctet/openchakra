2d2ff6e1087a1f4ed6478e3bee0e8c09d4fd95f9
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-10-03 15:15:27 +0200

    Smartdsiet: quizz import script

--- backend/web/scripts/smartdiet/importQuizz.js
+++ backend/web/scripts/smartdiet/importQuizz.js
@@ -1,86 +1,80 @@
+// Expects JSON
+const {
+  QUIZZ_QUESTION_TYPE_ENUM_MULTIPLE,
+  QUIZZ_QUESTION_TYPE_ENUM_SINGLE,
+  QUIZZ_QUESTION_TYPE_NUMERIC,
+  QUIZZ_QUESTION_TYPE_TEXT,
+  QUIZZ_TYPE_HEALTH,
+  QUIZZ_TYPE_PATIENT
+} = require('../../server/plugins/smartdiet/consts')
+const { extractData, guessFileType } = require('../../utils/import')
+const { guessDelimiter } = require('../../utils/text')
+
+const { MONGOOSE_OPTIONS } = require('../../server/utils/database')
+const { getDatabaseUri } = require('../../config/config')
 const mongoose = require('mongoose')
 const lodash=require('lodash')
 const moment=require('moment')
 const fs=require('fs')
-// Expects JSON
-const {
-  QUIZZ_QUESTION_TYPE_ENUM_SINGLE,
-  QUIZZ_TYPE_PATIENT
-} = require('../../server/plugins/smartdiet/consts')
 const Item = require('../../server/models/Item')
 const QuizzQuestion = require('../../server/models/QuizzQuestion')
 const Quizz = require('../../server/models/Quizz')
 
-const upsertQuestion= q => {
-  const params={
-    title: q.question,
-    type: QUIZZ_QUESTION_TYPE_ENUM_SINGLE,
-    origin_id: q.id,
-    origin_quizz_id: q.quiz_id,
-    success_message: `Correct:${q.comments}`,
-    error_message: `Faux:${q.comments}`,
-  }
-  return QuizzQuestion.update(
-    {origin_id: q.id},
-    params,
-    {upsert: true, runValidators: true}
-  )
-  .then(() => QuizzQuestion.findOne({origin_id: q.id}))
-  .then(created => {
-    return Promise.all([q.firstanswer, q.secondanswer].map(answer => {
-      return Item.create({text: answer, quizzQuestion: created._id})
-    }))
-    .then(([item1, item2]) => {
-      const correct_answer=(q.firstanswergood ? item1 : item2)._id
-      return QuizzQuestion.findOneAndUpdate({origin_id: q.id}, {correct_answer})
-    })
-  })
+const TYPE_MAPPING={
+  Single: QUIZZ_QUESTION_TYPE_ENUM_SINGLE,
+  Multiple: QUIZZ_QUESTION_TYPE_ENUM_MULTIPLE,
+  Number: QUIZZ_QUESTION_TYPE_NUMERIC,
+  Text: QUIZZ_QUESTION_TYPE_TEXT,
 }
 
-const importQuizz= ({quizzs , questions}) => {
-  const createQuizzs=quizzs.map((quizz, index) => {
-    const params={
-      name: quizz.name,
-      type: QUIZZ_TYPE_PATIENT,
-      origin_id: quizz.id,
+const QUIZZ_NAME='Bilan nutritionnel initial'
+
+const importQuizz= (data) => {
+  console.log(data.headers)
+  return Promise.all(data.records.map(question => {
+    const type=TYPE_MAPPING[question.type]
+    const title=question.title
+    const answers=question.answers.split('/').map(answer => answer.trim())
+    if (!type) throw new Error(`Unkown type ${question.type} for ${question.title}`)
+    if ([QUIZZ_QUESTION_TYPE_ENUM_SINGLE, QUIZZ_QUESTION_TYPE_ENUM_MULTIPLE].includes(type) && lodash.isEmpty(answers)) {
+      throw new Error(`No anwers for ${question.title} ${question.type}`)
     }
-    return Quizz.update(
-      {origin_id: quizz.id},
-      params,
-      {upsert: true, runValidators: true}
-    )
-      .then(() => Quizz.findOne({origin_id: quizz.id}))
-      .then(createdQuizz => {
-        const quizzQuestions=lodash(questions)
-          .filter(q => q.quiz_id==createdQuizz.origin_id)
-          .sortBy('order')
-          .value()
-        return Promise.all(quizzQuestions.map(q => upsertQuestion(q)))
-        .then(createdQuestions => {
-          createdQuizz.questions=createdQuestions.map(q => q._id)
-          return createdQuizz.save()
-        })
+    return QuizzQuestion.findOneAndUpdate({title}, {type, title}, {upsert: true, runValidators: true})
+      .then(question => {
+        if (answers.length>0) {
+          return Promise.all(answers.map(answer => Item.findOneAndUpdate(
+            {text: answer, quizzQuestion: question._id},
+            {text: answer, quizzQuestion: question._id},
+            {upsert: true}
+          )))
+          .then(() => question)
+        }
+        return question
       })
-  })
-  return Promise.all(createQuizzs)
-    .then(() => Quizz.find({origin_id: {$ne: null}}))
-    .then(quizzs => Promise.all(quizzs.map((q, idx) => {q.creation_date=moment().add(-idx, 'second'); return q.save()})))
+  }))
+  .then(questions => Quizz.findOneAndUpdate(
+    {name: QUIZZ_NAME},
+    {name: QUIZZ_NAME, questions, type: QUIZZ_TYPE_HEALTH},
+    {upsert: true, runValidators: true}
+  ))
 }
 
 if (require.main === module) {
-  const [quizzFile, questionsFile]=process.argv.slice(2, 4)
-  if (!(quizzFile && questionsFile)) {
-    console.log(`Expected quizz_filename questions_filename`)
+  const quizzFile=process.argv[2]
+  if (!quizzFile) {
+    console.log(`Expected quizz_filename`)
     process.exit(1)
   }
-  const [quizzs, questions]=[quizzFile, questionsFile].map(filename => {
-    return JSON.parse(fs.readFileSync(filename))
-  })
-  mongoose.connect('mongodb://localhost/smartdiet')
-    .then(()=> importQuizz({quizzs, questions}))
-    .then(console.log)
-    .catch(console.error)
-    .finally(() => process.exit(0))
+  const quizzs=fs.readFileSync(quizzFile)
+  guessFileType(quizzs)
+    .then(type => extractData(quizzs, {format: type, delimiter: ';'}))
+    .then(data => {
+      mongoose.connect(getDatabaseUri(), MONGOOSE_OPTIONS)
+        .then(()=> importQuizz(data))
+        .then(console.log)
+        .catch(console.error)
+        .finally(() => process.exit(0))
+    })
 }
 else {
   module.exports={
