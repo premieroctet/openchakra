86ec49d03748e6ceef4d464dc2bc420bd9a86420
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-10-09 19:11:04 +0200

    Klesia: fix models

--- backend/web/server/plugins/klesia/schemas/BestPracticesSchema.js
+++ backend/web/server/plugins/klesia/schemas/BestPracticesSchema.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose')
 const {schemaOptions} = require('../../../utils/schemas')
-const StepsContainerSchema=require('./StepsContainerSchema')
 
-// Best practices are exactly steps container
+const Schema = mongoose.Schema
 
-module.exports = StepsContainerSchema
+const BestPracticesSchema = new Schema({
+}, schemaOptions)
+
+/* eslint-disable prefer-arrow-callback */
+BestPracticesSchema.virtual('steps', {
+  ref: 'step', // The Model to use
+  localField: '_id', // Find in Model, where localField
+  foreignField: 'container', // is equal to foreignField
+  options: { sort: { order: 1 } },
+})
+/* eslint-enable prefer-arrow-callback */
+
+module.exports = BestPracticesSchema
