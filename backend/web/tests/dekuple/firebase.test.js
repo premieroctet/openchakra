869c142bf6b54d7ef571caf5bd1937366fffcef0
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-10-24 16:34:49 +0200

    Merge branch 'smartdiet'

--- backend/web/tests/dekuple/firebase.test.js
+++ backend/web/tests/dekuple/firebase.test.js
@@ -15,12 +15,12 @@ describe('Test firebase', () => {
   it('must send user notification', async() => {
     const user_id=`63e4ef709a5bc677cf11a0d6`
     const message=`${moment().format('HH:mm:ss')}=>${user_id}`
-    return sendUserNotification(user_id, message)
+    return sendUserNotification({user:{_id: user_id}}, title: 'Dekuple test', message})
   })
 
   it('must send application notification', async() => {
     const message=`${moment().format('HH:mm:ss')}=>all`
-    return sendAppNotification(message)
+    return sendAppNotification({title: 'Dekuple test braodcast', message})
   })
 
 })
--- backend/web/tests/smartdiet/firebase.test.js
+++ backend/web/tests/smartdiet/firebase.test.js
@@ -15,12 +15,12 @@ describe('Test firebase', () => {
   it('must send user notification', async() => {
     const user_id=`65089ca2b74fb64fcd2d8099`
     const message=`Notification du ${moment().format('HH:mm:ss')} pour ${user_id}`
-    return sendUserNotification(user_id, message)
+    return sendUserNotification({user:{_id: user_id}}, title: 'Smartdiet test', message})
   })
 
   it.skip('must send application notification', async() => {
     const message=`${moment().format('HH:mm:ss')}=>all`
-    return sendAppNotification(message)
+    return sendAppNotification({title: 'Smartdiet test broadcast', message})
   })
 
 })
