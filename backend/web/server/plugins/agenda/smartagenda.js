23a9c823ef8f352f8e5ea5970e6bd694a084a701
Sébastien Auvray <sebastien.auvray@my-alfred.io>
2023-10-24 15:55:11 +0200

    Smartdiet: synchronize smartagenda 3 times to get longer period

--- backend/web/server/plugins/agenda/smartagenda.js
+++ backend/web/server/plugins/agenda/smartagenda.js
@@ -213,7 +213,12 @@ const getAppointmentTypes = () => {
     })
 }
 
-const getAvailabilities = ({diet_id, from, to, appointment_type}) => {
+/**
+Get availabilities for a diet and appontment type (i.e. prestation)
+As smartagenda only returns about one week of availabilities, call twice
+remaining_calls tells the number of recursive calls
+*/
+const getAvailabilities = ({diet_id, from, to, appointment_type, remaining_calls=2}) => {
   if (!(diet_id && from && to && appointment_type)) {
     throw new Error(`diet_id/from/to/appointment_type are required`)
   }
@@ -250,6 +255,15 @@ const getAvailabilities = ({diet_id, from, to, appointment_type}) => {
       const end_date=moment(start_date).add(d.duration, 'minutes')
       return ({start_date, end_date, idpr: d.idpr})
     }))
+    .then(dispos => {
+      const last_date=lodash.max(dispos.map(d => d.start_date))
+      if (remaining_calls==0) {
+        return dispos
+      }
+      return getAvailabilities({diet_id, from:last_date, to, appointment_type,
+        remaining_calls:remaining_calls-1})
+        .then(dispos2 => [...dispos, ...dispos2])
+    })
     .catch(err => {
       if (err?.response?.status==404) {
         return []
@@ -277,27 +291,8 @@ const getAvailabilities = ({diet_id, from, to, appointment_type}) => {
     .then(res => res.length && console.log(`Updated/created ${res.length} appt types`))
 })
 
-const upsertAvailabilities = diet_smartagenda_id => {
-  /**
-  return Range.deleteMany({smartagenda_id: diet_smartagenda_id})
-    .then(() => Promise.all([User.find({smartagenda_id: diet_smartagenda_id}), AppointmentType.find()]))
-    .then(([diets, app_types]) => {
-      const combinations=lodash.product(diets, app_types)
-      return Promise.allSettled(combinations.map(([diet, app_type]) => {
-        return getAvailabilities({diet_id: diet.smartagenda_id, from: start, to: end, appointment_type: app_type.smartagenda_id})
-          .then(avails => {
-            return Promise.all(avails.map(avail => {
-              const params={ user: diet._id, appointment_type: app_type._id, start_date: avail.start_date }
-              return Range.create(params)
-            }))
-          })
-          .catch(err => console.error(`${msg_id}:${err.response.status},${err.response.data.message}`))
-    })
-    */
-}
-
 // Synchronize availabilities every minute
-// DISABLED UNTIL SMARTAGENDA WEBHOOK
+// ENABLED UNTIL SMARTAGENDA WEBHOOK
 !isDevelopment() && cron.schedule('0 * * * * *', () => {
   synchronizeAvailabilities()
 })
@@ -321,11 +316,14 @@ const synchronizeAvailabilities = () => {
   return User.find({role: ROLE_EXTERNAL_DIET, smartagenda_id:{$ne :null}})
     .then(diets => Promise.all(diets.map(d => getDietAppointmentTypes(d)))
       .then(all_app_types => {
+        all_app_types=all_app_types.filter(v => !!v)
         // Compute all (diet, appintment type) combinations
         const combinations=[]
         diets.forEach((diet, index) => {
           all_app_types[index].forEach(app_type => {
-            combinations.push([diet, app_type])
+            if (app_type) {
+              combinations.push([diet, app_type])
+            }
           })
         })
         // Get smartagenda availabilities for eahc diet/appointment type
