34bcf3b5fcaf88d7859ef06ac76c98b61587ab73
Sébastien Auvray <sebastien.auvray@wappizy.com>
2024-04-12 15:35:04 +0200

    Stduio: removed save/create messages

--- studio-test/src/dependencies/utils/messages.js
+++ studio-test/src/dependencies/utils/messages.js
@@ -2,8 +2,6 @@ export const MESSAGES={
   changePassword: 'Votre mot de passe a été modifié',
   register: 'Compte créé avec succès',
   registerToEvent: 'Votre participation a été prise en compte !',
-  save: 'Enregistré !',
-  create: 'Enregistré !',
   smartdiet_fail_event: "C'est pas la fin des haricots !",
   smartdiet_join_team: 'Vous avez intégré cette team !',
   smartdiet_routine_challenge: 'Bravo !',
