a32d6adde2290be812c7045073c6d229e67f10ba
SeghirOumo <seghir.oumohand@wappizy.com>
2024-04-19 10:27:15 +0200

    Studio: [wip] generate PDF action update, needs to be tested with backend

--- studio/src/dependencies/utils/tools.js
+++ studio/src/dependencies/utils/tools.js
@@ -1,8 +1,32 @@
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
 
-const generatePDF = (targetId, fileName="export") => {
+async function loadImage(url) {
+  const corsProxy = "https://thingproxy.freeboard.io/fetch/"; // CORS Proxy to handle cross-origin requests
+  const proxiedUrl = corsProxy + url;
+  return new Promise((resolve, reject) => {
+    let img = new Image();
+    img.onload = () => resolve(img); // Resolve the promise when the image is loaded
+    img.onerror = () => reject(new Error(`Failed to load image at ${url}`)); // Reject the promise if there's an error
+    img.src = proxiedUrl;
+    img.crossOrigin = "Anonymous"; // Set cross-origin to anonymous to prevent CORS issues
+  });
+}
+
+async function generatePDF(targetId, fileName="export"){
+
   const input = document.getElementById(targetId);
+  const imgs= input.querySelectorAll('img')
+  await Promise.all(Array.from(imgs).map(async (img) => {
+    try {
+      const loadedImage = await loadImage(img.src);
+      img.src = loadedImage.src; // Replace the original src with the loaded image src
+      console.log(`Image loaded and replaced for ${img.src}`);
+    } catch (error) {
+      console.error(`Error loading image ${img.src}: ${error}`);
+      console.log("Some images failed to load and will not appear in the PDF.");
+    }
+  }));
   
   return html2canvas(input, { scale: 2, useCORS: true, logging: true, scrollY: -window.scrollY }).then(canvas => {
     const imgData = canvas.toDataURL('image/png');
